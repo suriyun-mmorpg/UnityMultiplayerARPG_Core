cfea61dd6fe5d4aa65458095118cf9b4eac751ee
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-27 00:39:55 +0700

    Add character attribute amount class

--- Scripts/GameData/Character/CharacterAttribute.cs
+++ Scripts/GameData/Character/CharacterAttribute.cs
@@ -8,3 +8,10 @@ public class CharacterAttribute : BaseGameData
     public CharacterStats statsIncreaseEachLevel;
     public CharacterStatsPercentage statsPercentageIncreaseEachLevel;
 }
+
+[System.Serializable]
+public class CharacterAttributeAmount
+{
+    public CharacterAttribute attribute;
+    public int amount;
+}
