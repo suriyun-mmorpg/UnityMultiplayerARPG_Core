8f83e00013c758d24834616a2df5b40bcf4417be
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-29 03:38:34 +0700

    Add attribute level struct which will be synced in gameplay

---
+++ Scripts/Gameplay/CharacterAttributeLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+[System.Serializable]
+public struct CharacterAttributeLevel
+{
+    // Use attributeId as primary key
+    public string attributeId;
+    public int amount;
+    
+    public CharacterAttribute Attribute
+    {
+        get { return GameInstance.CharacterAttributes.ContainsKey(attributeId) ? GameInstance.CharacterAttributes[attributeId] : null; }
+    }
+}
+
+public class SyncListCharacterAttributeLevel : SyncListStruct<CharacterAttributeLevel> { }
