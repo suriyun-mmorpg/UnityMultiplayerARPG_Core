9bade2cdea56fc35d4b4b1e1244687e1705f58b8
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-29 03:39:08 +0700

    Add character item struct which will synced while gameplay

---
+++ Scripts/Gameplay/CharacterItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+[System.Serializable]
+public struct CharacterItem
+{
+    // Use id as primary key
+    public string id;
+    public string itemId;
+    public int level;
+    public int amount;
+    // TODO: I want to add random item bonus
+
+    public Item Item
+    {
+        get { return GameInstance.Items.ContainsKey(itemId) ? GameInstance.Items[itemId] : null; }
+    }
+
+    public EquipmentItem EquipmentItem
+    {
+        get { return Item != null ? Item as EquipmentItem : null; }
+    }
+}
+
+public class SyncListCharacterItem : SyncListStruct<CharacterItem> { }
