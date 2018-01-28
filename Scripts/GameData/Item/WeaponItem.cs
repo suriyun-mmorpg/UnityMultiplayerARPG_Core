fbb6f3fd08a9e3842c3a3256d0055fe3273a2ffe
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-29 04:15:45 +0700

    Add weapon item

---
+++ Scripts/GameData/Item/WeaponItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponItem : EquipmentItem
+{
+    public DamageAmount damage;
+    public DamageEffectivenessAttribute[] effectivenessAttributes;
+    public bool requireTwoHand;
+    public bool canWieldDual;
+
+#if UNITY_EDITOR
+    protected override void OnValidate()
+    {
+        if (requireTwoHand && canWieldDual)
+        {
+            Debug.LogWarning("Weapon which require two hand cannot wield dual");
+            canWieldDual = false;
+        }
+        base.OnValidate();
+    }
+#endif
+}
