c7f64c20c1b797f215b987d2df324cc8766f984b
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-29 04:25:48 +0700

    Add shield item

---
+++ Scripts/GameData/Item/ShieldItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldItem : EquipmentItem
+{
+#if UNITY_EDITOR
+    protected override void OnValidate()
+    {
+        // Shield equipment cannot set custom equip position
+        equipPosition = string.Empty;
+        base.OnValidate();
+    }
+#endif
+}
