719f3cab3c878ae036e187e0f51f277da9a06213
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-29 03:40:19 +0700

    WIP UIs

---
+++ Scripts/UI/UICharacterCreate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICharacterCreate : UIBase
+{
+    public override void Show()
+    {
+        base.Show();
+        // Show list of characters that can be create
+    }
+}
---
+++ Scripts/UI/UICharacterSelection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICharacterSelection : UIBase
+{
+    public override void Show()
+    {
+        base.Show();
+        // Show list of created characters
+    }
+}
