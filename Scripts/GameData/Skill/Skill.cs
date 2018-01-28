85cb72d85fc3d1098318d384755c621ffbdb291e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-29 04:16:39 +0700

    Add skill

---
+++ Scripts/GameData/Skill/Skill.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Skill", menuName = "Create GameData/Skill")]
+public class Skill : BaseGameData
+{
+    public SkillLevel[] requireSkillLevels;
+    public int consumeMp;
+    public float coolDown;
+    [Header("Attack")]
+    public bool isAttack;
+    public DamageAmount damage;
+    [Header("Buffs")]
+    public CharacterStats baseStats;
+    public CharacterStats statsIncreaseEachLevel;
+    public CharacterStatsPercentage statsPercentageIncreaseEachLevel;
+}
+
+[System.Serializable]
+public class SkillLevel
+{
+    public SkillLevel skill;
+    public int level;
+}
