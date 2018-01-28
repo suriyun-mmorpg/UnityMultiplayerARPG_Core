17ee2239252c4677aeed4b610d1e40db4f69baa2
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-29 02:39:27 +0700

    Add UI base

---
+++ Scripts/UI/UIBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBase : MonoBehaviour
+{
+    public bool hideOnAwake = false;
+
+    protected virtual void Awake()
+    {
+        if (hideOnAwake)
+            Hide();
+    }
+
+    public virtual void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public virtual void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
