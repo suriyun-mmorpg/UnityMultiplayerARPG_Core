057f3cd8bb7b5b6b2458aecb1b92df1d16a389f2
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-29 03:40:09 +0700

    Add relates sync structs, stats calculation, character create helper function

--- Scripts/Gameplay/CharacterEntity.cs
+++ Scripts/Gameplay/CharacterEntity.cs
@@ -6,10 +6,13 @@ using UnityEngine.Networking;
 [RequireComponent(typeof(CharacterMovement))]
 public class CharacterEntity : NetworkBehaviour
 {
+    // Use id as primary key
+    [SyncVar]
+    public string id;
     [SyncVar]
     public string characterName;
-    [SyncVar]
-    public string characterClassId;
+    [SyncVar(hook = "OnCharacterClassIdChange")]
+    public string classId;
     [SyncVar]
     public int level;
     [SyncVar]
@@ -24,10 +27,15 @@ public class CharacterEntity : NetworkBehaviour
     public int skillPoint;
     [SyncVar]
     public int gold;
+    [Header("Sync Lists")]
+    public SyncListCharacterAttributeLevel attributeLevels = new SyncListCharacterAttributeLevel();
+    public SyncListCharacterSkillLevel skillLevels = new SyncListCharacterSkillLevel();
+    public SyncListCharacterItem equipments = new SyncListCharacterItem();
+    public SyncListCharacterItem inventory = new SyncListCharacterItem();
 
     public CharacterClass Class
     {
-        get { return GameInstance.CharacterClasses[characterClassId]; }
+        get { return GameInstance.CharacterClasses[classId]; }
     }
 
     public int NextLevelExp
@@ -41,16 +49,76 @@ public class CharacterEntity : NetworkBehaviour
         }
     }
 
+    #region Stats calculation, make saperate stats for buffs calculation
+    public CharacterStats Stats
+    {
+        get
+        {
+            var result = Class.baseStats;
+            result += Class.statsIncreaseEachLevel * level;
+            foreach (var attributeLevel in attributeLevels)
+            {
+                if (attributeLevel.Attribute == null)
+                {
+                    Debug.LogError("Attribute: " + attributeLevel.attributeId + " owned by " + id + " is invalid data");
+                    continue;
+                }
+                result += attributeLevel.Attribute.statsIncreaseEachLevel * level;
+            }
+            foreach (var equipment in equipments)
+            {
+                if (equipment.EquipmentItem == null) {
+                    Debug.LogError("Item: " + equipment.id + " owned by "+ id + " is not equipment");
+                    continue;
+                }
+                result += equipment.EquipmentItem.baseStats;
+                result += equipment.EquipmentItem.statsIncreaseEachLevel * level;
+            }
+            return result;
+        }
+    }
+
+    public CharacterStatsPercentage StatsPercentage
+    {
+        get
+        {
+            var result = Class.statsPercentageIncreaseEachLevel * level;
+            foreach (var attributeLevel in attributeLevels)
+            {
+                if (attributeLevel.Attribute == null)
+                {
+                    Debug.LogError("Attribute: " + attributeLevel.attributeId + " owned by " + id + " is invalid data");
+                    continue;
+                }
+                result += attributeLevel.Attribute.statsPercentageIncreaseEachLevel * level;
+            }
+            foreach (var equipment in equipments)
+            {
+                if (equipment.EquipmentItem == null)
+                {
+                    Debug.LogError("Item: " + equipment.id + " owned by " + id + " is not equipment");
+                    continue;
+                }
+                result += equipment.EquipmentItem.statsPercentageIncreaseEachLevel * level;
+            }
+            return result;
+        }
+    }
+
+    public CharacterStats StatsWithoutBuffs
+    {
+        get { return Stats + StatsPercentage; }
+    }
+    #endregion
+
     public int MaxHp
     {
-        // TODO: Bring data from game instance, equipments
-        get { return 0; }
+        get { return (int)StatsWithoutBuffs.hp; }
     }
 
     public int MaxMp
     {
-        // TODO: Bring data from game instance, equipments
-        get { return 0; }
+        get { return (int)StatsWithoutBuffs.mp; }
     }
 
     private Rigidbody tempRigidbody;
@@ -90,7 +158,14 @@ public class CharacterEntity : NetworkBehaviour
     public override void OnStartClient()
     {
         base.OnStartClient();
-        TempRigidbody.WakeUp();
+        if (!isServer)
+            OnCharacterClassIdChange(classId);
+    }
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        OnCharacterClassIdChange(classId);
     }
 
     public override void OnStartLocalPlayer()
@@ -101,5 +176,36 @@ public class CharacterEntity : NetworkBehaviour
             TempCharacterMovementInput = gameObject.AddComponent<CharacterMovementInput>();
         TempCharacterMovementInput.enabled = true;
         TempCharacterMovement.enabled = true;
+    }
+
+    #region SyncVar Hooks
+    protected virtual void OnCharacterClassIdChange(string classId)
+    { 
+        // TODO: I will use this hook to change character model
+        // Setup model then wake up rigidbody
+        TempRigidbody.WakeUp();
+    }
+    #endregion
+
+    /// <summary>
+    /// Use this function to make an character entity, useful to make temporary data in character create/selection UIs
+    /// </summary>
+    /// <param name="classId"></param>
+    /// <returns></returns>
+    public static CharacterEntity CreateNewCharacter(string characterName, string classId)
+    {
+        var character = Instantiate(GameInstance.Singleton.characterEntityPrefab);
+        character.characterName = characterName;
+        character.classId = classId;
+        foreach (var baseAttribute in character.Class.baseAttributes)
+        {
+            var attributeLevel = new CharacterAttributeLevel();
+            attributeLevel.attributeId = baseAttribute.attribute.Id;
+            attributeLevel.amount = baseAttribute.amount;
+            character.attributeLevels.Add(attributeLevel);
+        }
+        character.currentHp = character.MaxHp;
+        character.currentMp = character.MaxMp;
+        return character;
     }
 }
