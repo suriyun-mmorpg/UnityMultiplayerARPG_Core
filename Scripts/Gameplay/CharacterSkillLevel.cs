159abdb1a0d074cff7569cf5f95ba5d0c2bbe4d5
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-29 03:39:17 +0700

    Add character skill level struct which will synced while gameplay

---
+++ Scripts/Gameplay/CharacterSkillLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+[System.Serializable]
+public struct CharacterSkillLevel
+{
+    public string skillId;
+    public int level;
+}
+
+public class SyncListCharacterSkillLevel : SyncListStruct<CharacterSkillLevel> { }
