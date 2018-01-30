031779c82ed75b843e21ea6f764b28c3bab4a46e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-30 18:14:20 +0700

    Add UIList which will be used to create list of an UIs

---
+++ Scripts/UI/UIList.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIList : MonoBehaviour
+{
+    public GameObject uiPrefab;
+    public Transform uiContainer;
+    protected readonly List<GameObject> uis = new List<GameObject>();
+
+    public void MakeList<T>(List<T> list, System.Action<T, GameObject> onCreate)
+    {
+        var i = 0;
+        for (; i < list.Count; ++i)
+        {
+            GameObject ui;
+            if (i < uis.Count)
+                ui = uis[i];
+            else
+            {
+                ui = Instantiate(uiPrefab);
+                ui.transform.SetParent(uiContainer);
+            }
+            ui.SetActive(true);
+            if (onCreate != null)
+                onCreate(list[i], ui);
+        }
+        for (; i < uis.Count; ++i)
+        {
+            var ui = uis[i];
+            ui.gameObject.SetActive(false);
+        }
+    }
+}
