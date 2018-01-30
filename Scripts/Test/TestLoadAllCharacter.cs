01280d932ea2b2e3de5eda2646bd147e6b8666e5
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-31 05:45:42 +0700

    Test load characters script

---
+++ Scripts/Test/TestLoadAllCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLoadAllCharacter : MonoBehaviour {
+	void Start () {
+        var characters = CharacterDataExtension.LoadAllCharacterData();
+        foreach (var character in characters)
+        {
+            var entity = Instantiate(GameInstance.Singleton.characterEntityPrefab);
+            entity.gameObject.SetActive(false);
+            character.CloneTo(entity);
+            foreach (var attributeLevel in entity.AttributeLevels)
+            {
+                Debug.Log("Found attribute " + attributeLevel.attributeId + " " + attributeLevel.amount);
+            }
+        }
+	}
+}
