7aac600548300fe3e120a248b16370eba35aa75e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-30 18:18:19 +0700

    Show list of an characters that can be created

--- Scripts/UI/UICharacterCreate.cs
+++ Scripts/UI/UICharacterCreate.cs
@@ -1,12 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
+[RequireComponent(typeof(UIList))]
 public class UICharacterCreate : UIBase
 {
+    private UIList tempList;
+    public UIList TempList
+    {
+        get
+        {
+            if (tempList == null)
+                tempList = GetComponent<UIList>();
+            return tempList;
+        }
+    }
+
     public override void Show()
     {
         base.Show();
         // Show list of characters that can be create
+        var creatableClasses = GameInstance.CharacterClasses.Values.Where(a => a.canCreateByPlayer).ToList();
+        TempList.MakeList(creatableClasses, (creatableClass, ui) =>
+        {
+            var characterEntity = CharacterEntity.CreateNewCharacter("", creatableClass.Id);
+            characterEntity.gameObject.SetActive(false);
+            var uiCharacter = ui.GetComponent<UICharacter>();
+            uiCharacter.characterEntity = characterEntity;
+        });
     }
 }
