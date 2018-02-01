8f84fb9f76f45a9c805952e5654cce6a5a069aa4
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-02-02 06:53:03 +0700

    Add scene ui management

---
+++ Scripts/UI/Scenes/UISceneHome.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISceneHome : UIHistory
+{
+    public UICharacterCreate uiCharacterCreate;
+    public UICharacterSelection uiCharacterSelection; 
+
+    public void OnClickSinglePlayer()
+    {
+        Next(uiCharacterSelection);
+    }
+
+    public void OnClickMultiplayer()
+    {
+        // TODO: Show LAN host list
+    }
+
+    public void OnClickCreateCharacter()
+    {
+        Next(uiCharacterCreate);
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+}
