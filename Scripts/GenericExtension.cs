b802df67e4f4be340b8c4826b7b2941c02b09065
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-02-02 06:40:50 +0700

    Add generic extension to use with an Unity's Objects

---
+++ Scripts/GenericExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GenericExtension
+{
+    public static void RemoveChildren(this Transform transform)
+    {
+        while (transform.childCount > 0)
+        {
+            var lastChild = transform.GetChild(transform.childCount - 1);
+            Object.Destroy(lastChild.gameObject);
+        }
+    }
+
+    public static void SetChildrenActive(this Transform transform, bool isActive)
+    {
+        for (var i = 0; i < transform.childCount; ++i)
+        {
+            transform.GetChild(i).gameObject.SetActive(isActive);
+        }
+    }
+}
