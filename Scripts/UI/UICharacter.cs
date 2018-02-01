a361b09bc9ef9c850f400d268249daab14b8aa28
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-02-02 06:42:28 +0700

    Add format support for UI Character

--- Scripts/UI/UICharacter.cs
+++ Scripts/UI/UICharacter.cs
@@ -6,7 +6,17 @@ using UnityEngine.UI;
 
 public class UICharacter : UIBase
 {
-
+    [Header("Display Format")]
+    public string nameFormat = "{0}";
+    public string levelFormat = "Lv: {0}";
+    public string expFormat = "Exp: {0}";
+    public string hpFormat = "Hp: {0}";
+    public string mpFormat = "Mp: {0}";
+    public string statPointFormat = "Stat Points: {0}";
+    public string skillPointFormat = "Skill Points: {0}";
+    public string goldFormat = "Gold: {0}";
+    public string classTitleFormat = "Class: {0}";
+    [Header("UI Elements")]
     public Text textName;
     public Text textLevel;
     public Text textExp;
@@ -27,38 +37,40 @@ public class UICharacter : UIBase
     protected virtual void Update()
     {
         if (textName != null)
-            textName.text = characterData == null ? "" : characterData.CharacterName;
+            textName.text = string.Format(nameFormat, characterData == null ? "" : characterData.CharacterName);
 
         if (textLevel != null)
-            textLevel.text = characterData == null ? "N/A" : characterData.Level.ToString("N0");
+            textLevel.text = string.Format(levelFormat, characterData == null ? "N/A" : characterData.Level.ToString("N0"));
 
         if (textExp != null)
         {
+            var expString = "";
             if (characterData == null)
-                textExp.text = "N/A";
+                expString = "N/A";
             else if (characterData.GetNextLevelExp() > 0)
-                textExp.text = characterData.Exp.ToString("N0") + "/" + characterData.GetNextLevelExp().ToString("N0");
+                expString = characterData.Exp.ToString("N0") + "/" + characterData.GetNextLevelExp().ToString("N0");
             else
-                textExp.text = "Max";
+                expString = "Max";
+            textExp.text = string.Format(expFormat, expString);
         }
 
         if (textHp != null)
-            textHp.text = characterData == null ? "N/A" : characterData.CurrentHp.ToString("N0") + "/" + characterData.GetMaxHp().ToString("N0");
+            textHp.text = string.Format(hpFormat, characterData == null ? "N/A" : characterData.CurrentHp.ToString("N0") + "/" + characterData.GetMaxHp().ToString("N0"));
 
         if (textMp != null)
-            textMp.text = characterData == null ? "N/A" : characterData.CurrentMp.ToString("N0") + "/" + characterData.GetMaxMp().ToString("N0");
+            textMp.text = string.Format(mpFormat, characterData == null ? "N/A" : characterData.CurrentMp.ToString("N0") + "/" + characterData.GetMaxMp().ToString("N0"));
 
         if (textStatPoint != null)
-            textStatPoint.text = characterData == null ? "N/A" : characterData.StatPoint.ToString("N0");
+            textStatPoint.text = string.Format(statPointFormat, characterData == null ? "N/A" : characterData.StatPoint.ToString("N0"));
 
         if (textSkillPoint != null)
-            textSkillPoint.text = characterData == null ? "N/A" : characterData.SkillPoint.ToString("N0");
+            textSkillPoint.text = string.Format(skillPointFormat, characterData == null ? "N/A" : characterData.SkillPoint.ToString("N0"));
 
         if (textGold != null)
-            textGold.text = characterData == null ? "N/A" : characterData.Gold.ToString("N0");
+            textGold.text = string.Format(goldFormat, characterData == null ? "N/A" : characterData.Gold.ToString("N0"));
 
         if (textClassTitle != null)
-            textClassTitle.text = characterData == null ? "N/A" : characterData.GetClass().title;
+            textClassTitle.text = string.Format(classTitleFormat, characterData == null ? "N/A" : characterData.GetClass().title);
 
         if (textClassDescription != null)
             textClassDescription.text = characterData == null ? "N/A" : characterData.GetClass().description;
