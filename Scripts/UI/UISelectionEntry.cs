b38cb13ede28556cb3e831dc2552ba74664a9001
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-02-02 17:08:06 +0700

    Add selection system

---
+++ Scripts/UI/UISelectionEntry.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UISelectionEntry<T> : UIBase
+{
+    [Header("UI Selection Elements")]
+    public GameObject objectSelected;
+    public T data;
+    public UISelectionManager selectionManager;
+    public void OnClickSelect()
+    {
+        if (selectionManager != null)
+            selectionManager.Select(this);
+    }
+
+    public void Select()
+    {
+        if (objectSelected != null)
+            objectSelected.SetActive(true);
+    }
+
+    public void Deselect()
+    {
+        if (objectSelected != null)
+            objectSelected.SetActive(false);
+    }
+}
---
+++ Scripts/UI/UISelectionManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class UISelectionManager : MonoBehaviour
+{
+    public abstract void Select(object ui);
+}
+
+public abstract class UISelectionManager<TData, TUI, TEvent> : UISelectionManager
+    where TUI : UISelectionEntry<TData>
+    where TEvent : UnityEvent<TUI>
+{
+    public TEvent eventOnSelect;
+
+    protected readonly List<TUI> uis = new List<TUI>();
+    public TUI SelectedUI { get; protected set; }
+
+    public void Add(TUI ui)
+    {
+        if (ui == null)
+            return;
+
+        ui.selectionManager = this;
+        // Select first ui
+        if (uis.Count == 0)
+            Select(ui);
+        else
+            ui.Deselect();
+
+        uis.Add(ui);
+    }
+
+    public void Clear()
+    {
+        uis.Clear();
+        SelectedUI = null;
+    }
+
+    public override sealed void Select(object ui)
+    {
+        if (ui == null)
+            return;
+
+        var castedUI = (TUI)ui;
+        castedUI.Select();
+
+        if (eventOnSelect != null)
+            eventOnSelect.Invoke(castedUI);
+
+        SelectedUI = castedUI;
+        foreach (var deselectUI in uis)
+        {
+            if (deselectUI != castedUI)
+                deselectUI.Deselect();
+        }
+    }
+}
