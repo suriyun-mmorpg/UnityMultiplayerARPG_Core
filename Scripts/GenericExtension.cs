455088a47b7dd36611af76434d902934096d9911
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-02-28 00:51:36 +0700

    Add remove components functions

--- Scripts/GenericExtension.cs
+++ Scripts/GenericExtension.cs
@@ -20,4 +20,31 @@ public static class GenericExtension
             transform.GetChild(i).gameObject.SetActive(isActive);
         }
     }
+
+    public static void RemoveComponents<T>(this GameObject gameObject) where T : Component
+    {
+        var components = gameObject.GetComponents<T>();
+        foreach (var component in components)
+        {
+            Object.DestroyImmediate(component);
+        }
+    }
+
+    public static void RemoveComponentsInChildren<T>(this GameObject gameObject, bool includeInactive) where T : Component
+    {
+        var components = gameObject.GetComponentsInChildren<T>(includeInactive);
+        foreach (var component in components)
+        {
+            Object.DestroyImmediate(component);
+        }
+    }
+
+    public static void RemoveComponentsInParent<T>(this GameObject gameObject, bool includeInactive) where T : Component
+    {
+        var components = gameObject.GetComponentsInParent<T>(includeInactive);
+        foreach (var component in components)
+        {
+            Object.DestroyImmediate(component);
+        }
+    }
 }
