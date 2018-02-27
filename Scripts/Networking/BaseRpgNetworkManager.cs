2decff61ff2b0e9df09aa9a946ca708b688fee0c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-02-28 00:28:38 +0700

    Add network manager codes

---
+++ Scripts/Networking/BaseRpgNetworkManager.cs
@@ -0,0 +1,233 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using LiteNetLibHighLevel;
+using LiteNetLib;
+using LiteNetLib.Utils;
+
+[RequireComponent(typeof(LoadGameMaps))]
+public abstract class BaseRpgNetworkManager : LiteNetLibGameManager
+{
+    public enum GameStartType
+    {
+        Client,
+        Server,
+        Host,
+    }
+
+    public class RpgGameMsgTypes
+    {
+        public const short ClientRequestCharacter = GameMsgTypes.Highest + 1;
+        public const short ServerMapResult = GameMsgTypes.Highest + 2;
+    }
+
+    public static GameStartType StartType;
+    public static string ConnectingNetworkAddress;
+    public static int ConnectingNetworkPort;
+
+    public GameObject mapLoadingObject;
+    public Image imageMapLoadingGage;
+    public Text textMapLoadingPercent;
+
+    protected AsyncOperation clientMapLoadOp;
+    protected Scene clientScene;
+    protected GameObject clientScenePhysic;
+    protected readonly Dictionary<long, CharacterData> pendingCharacters = new Dictionary<long, CharacterData>();
+
+    private LoadGameMaps tempLoadGameMaps;
+    public LoadGameMaps TempLoadGameMaps
+    {
+        get
+        {
+            if (tempLoadGameMaps == null)
+                tempLoadGameMaps = GetComponent<LoadGameMaps>();
+            return tempLoadGameMaps;
+        }
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        var gameInstance = GameInstance.Singleton;
+        Assets.RegisterPrefab(gameInstance.characterEntityPrefab.Identity);
+        /*
+        var damages = GameInstance.Damages.Values;
+        foreach (var damage in damages)
+        {
+            Assets.RegisterPrefab(damage)
+        }
+        */
+    }
+
+    protected virtual void Start()
+    {
+        switch (StartType)
+        {
+            case GameStartType.Server:
+            case GameStartType.Host:
+                TempLoadGameMaps.gameMaps.Clear();
+                TempLoadGameMaps.gameMaps.AddRange(GameInstance.GameMaps.Values);
+                TempLoadGameMaps.onLoadedMaps += OnLoadedMaps;
+                TempLoadGameMaps.LoadMaps();
+                StartCoroutine(LoadMapRoutine());
+                break;
+            case GameStartType.Client:
+                networkAddress = ConnectingNetworkAddress;
+                networkPort = ConnectingNetworkPort;
+                StartClient();
+                break;
+        }
+    }
+
+    IEnumerator LoadMapRoutine()
+    {
+        yield return null;
+        if (mapLoadingObject != null)
+            mapLoadingObject.SetActive(true);
+        yield return null;
+        while (!TempLoadGameMaps.IsDone)
+        {
+            yield return null;
+            var progress = TempLoadGameMaps.Progress;
+            if (imageMapLoadingGage != null)
+                imageMapLoadingGage.fillAmount = progress;
+            if (textMapLoadingPercent != null)
+                textMapLoadingPercent.text = (progress * 100).ToString("N0") + "% / 100%";
+        }
+        yield return null;
+        if (mapLoadingObject != null)
+            mapLoadingObject.SetActive(false);
+    }
+
+    protected override void OnDestroy()
+    {
+        switch (StartType)
+        {
+            case GameStartType.Server:
+            case GameStartType.Host:
+                TempLoadGameMaps.onLoadedMaps -= OnLoadedMaps;
+                break;
+        }
+        base.OnDestroy();
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        clientReadyOnConnect = false;
+        Assets.registeringPlayerPrefab = null;
+        Assets.spawnPlayerOnReady = false;
+    }
+#endif
+
+    protected virtual void OnLoadedMaps()
+    {
+        switch (StartType)
+        {
+            case GameStartType.Server:
+                StartServer();
+                break;
+            case GameStartType.Host:
+                StartHost();
+                break;
+        }
+    }
+
+    protected override void RegisterServerMessages()
+    {
+        base.RegisterServerMessages();
+        RegisterServerMessage(RpgGameMsgTypes.ClientRequestCharacter, HandleClientRequestCharacter);
+    }
+
+    protected override void RegisterClientMessages()
+    {
+        base.RegisterClientMessages();
+        RegisterClientMessage(RpgGameMsgTypes.ServerMapResult, HandleServerMapResult);
+    }
+
+    public override void OnPeerDisconnected(NetPeer peer, DisconnectInfo disconnectInfo)
+    {
+        base.OnPeerDisconnected(peer, disconnectInfo);
+        if (pendingCharacters.ContainsKey(peer.ConnectId))
+            pendingCharacters.Remove(peer.ConnectId);
+    }
+
+    public override void OnClientConnected(NetPeer peer)
+    {
+        SendPacket(SendOptions.ReliableUnordered, peer, RpgGameMsgTypes.ClientRequestCharacter, SerializeCharacterRequest);
+    }
+
+    protected void HandleClientRequestCharacter(LiteNetLibMessageHandler messageHandler)
+    {
+        var peer = messageHandler.peer;
+        var reader = messageHandler.reader;
+        var character = DeserializeCharacterRequest(peer, reader);
+        pendingCharacters[peer.ConnectId] = character;
+        SendPacket(SendOptions.ReliableUnordered, peer, RpgGameMsgTypes.ServerMapResult, (writer) =>
+        {
+            var map = TempLoadGameMaps.LoadedMap[character.CurrentMapName];
+            writer.Put(character.CurrentMapName);
+            writer.Put(map.MapOffsets.x);
+            writer.Put(map.MapOffsets.y);
+            writer.Put(map.MapOffsets.z);
+        });
+    }
+
+    protected void HandleServerMapResult(LiteNetLibMessageHandler messageHandler)
+    {
+        var reader = messageHandler.reader;
+        var mapName = reader.GetString();
+        var offset = new Vector3(reader.GetFloat(), reader.GetFloat(), reader.GetFloat());
+        StartCoroutine(LoadMap(mapName, offset));
+    }
+
+    IEnumerator LoadMap(string mapName, Vector3 offset)
+    {
+        yield return null;
+        if (mapLoadingObject != null)
+            mapLoadingObject.SetActive(true);
+        yield return null;
+        clientMapLoadOp = SceneManager.LoadSceneAsync(mapName, LoadSceneMode.Additive);
+        while (!clientMapLoadOp.isDone)
+        {
+            yield return null;
+            var progress = clientMapLoadOp.progress * 100f;
+            if (imageMapLoadingGage != null)
+                imageMapLoadingGage.fillAmount = progress / 100f;
+            if (textMapLoadingPercent != null)
+                textMapLoadingPercent.text = (progress * 100).ToString("N0") + "% / 100%";
+        }
+        clientScene = SceneManager.GetSceneByName(mapName);
+        var rootObjects = clientScene.GetRootGameObjects();
+        foreach (var rootObject in rootObjects)
+        {
+            var position = rootObject.transform.position;
+            rootObject.transform.position = position + offset;
+            // Remove all colliders, we're going to use map data colliders
+            var colliders = rootObject.GetComponentsInChildren<Collider>();
+            foreach (var collider in colliders)
+            {
+                DestroyImmediate(collider);
+            }
+        }
+        // Load collider data for clients
+        if (!IsServer)
+        {
+            var gameMap = GameInstance.GameMaps[mapName];
+            clientScenePhysic = Instantiate(gameMap.physicPrefab);
+            var position = clientScenePhysic.transform.position;
+            clientScenePhysic.transform.position = position + offset;
+            TempLoadGameMaps.TempNavMeshSurface.BuildNavMesh();
+        }
+        yield return null;
+        if (mapLoadingObject != null)
+            mapLoadingObject.SetActive(false);
+        yield return null;
+        SendClientReady();
+    }
+
+    protected abstract void SerializeCharacterRequest(NetDataWriter writer);
+    protected abstract CharacterData DeserializeCharacterRequest(NetPeer peer, NetDataReader reader);
+}
---
+++ Scripts/Networking/LanRpgNetworkManager.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using LiteNetLibHighLevel;
+using LiteNetLib;
+using LiteNetLib.Utils;
+
+public class LanRpgNetworkManager : BaseRpgNetworkManager
+{
+    public static CharacterData SelectedCharacter;
+
+    protected override void SerializeCharacterRequest(NetDataWriter writer)
+    {
+        writer.Put(SelectedCharacter.Id);
+        writer.Put(SelectedCharacter.CharacterName);
+        writer.Put(SelectedCharacter.PrototypeId);
+        writer.Put(SelectedCharacter.Level);
+        writer.Put(SelectedCharacter.Exp);
+        writer.Put(SelectedCharacter.CurrentHp);
+        writer.Put(SelectedCharacter.CurrentMp);
+        writer.Put(SelectedCharacter.StatPoint);
+        writer.Put(SelectedCharacter.SkillPoint);
+        writer.Put(SelectedCharacter.Gold);
+        writer.Put(SelectedCharacter.CurrentMapName);
+        writer.Put(SelectedCharacter.CurrentPosition.x);
+        writer.Put(SelectedCharacter.CurrentPosition.y);
+        writer.Put(SelectedCharacter.CurrentPosition.z);
+        writer.Put(SelectedCharacter.RespawnMapName);
+        writer.Put(SelectedCharacter.RespawnPosition.x);
+        writer.Put(SelectedCharacter.RespawnPosition.y);
+        writer.Put(SelectedCharacter.RespawnPosition.z);
+        writer.Put(SelectedCharacter.LastUpdate);
+        writer.Put(SelectedCharacter.AttributeLevels.Count);
+        foreach (var entry in SelectedCharacter.AttributeLevels)
+        {
+            writer.Put(entry.attributeId);
+            writer.Put(entry.amount);
+        }
+        writer.Put(SelectedCharacter.SkillLevels.Count);
+        foreach (var entry in SelectedCharacter.SkillLevels)
+        {
+            writer.Put(entry.skillId);
+            writer.Put(entry.level);
+        }
+        writer.Put(SelectedCharacter.EquipItems.Count);
+        foreach (var entry in SelectedCharacter.EquipItems)
+        {
+            writer.Put(entry.id);
+            writer.Put(entry.itemId);
+            writer.Put(entry.level);
+            writer.Put(entry.amount);
+        }
+        writer.Put(SelectedCharacter.NonEquipItems.Count);
+        foreach (var entry in SelectedCharacter.NonEquipItems)
+        {
+            writer.Put(entry.id);
+            writer.Put(entry.itemId);
+            writer.Put(entry.level);
+            writer.Put(entry.amount);
+        }
+    }
+
+    protected override CharacterData DeserializeCharacterRequest(NetPeer peer, NetDataReader reader)
+    {
+        var character = new CharacterData();
+        character.Id = reader.GetString();
+        character.CharacterName = reader.GetString();
+        character.PrototypeId = reader.GetString();
+        character.Level = reader.GetInt();
+        character.Exp = reader.GetInt();
+        character.CurrentHp = reader.GetInt();
+        character.CurrentMp = reader.GetInt();
+        character.StatPoint = reader.GetInt();
+        character.SkillPoint = reader.GetInt();
+        character.Gold = reader.GetInt();
+        character.CurrentMapName = reader.GetString();
+        character.CurrentPosition = new Vector3(reader.GetFloat(), reader.GetFloat(), reader.GetFloat());
+        character.RespawnMapName = reader.GetString();
+        character.RespawnPosition = new Vector3(reader.GetFloat(), reader.GetFloat(), reader.GetFloat());
+        character.LastUpdate = reader.GetInt();
+        var count = 0;
+        count = reader.GetInt();
+        for (var i = 0; i < count; ++i)
+        {
+            var entry = new CharacterAttributeLevel();
+            entry.attributeId = reader.GetString();
+            entry.amount = reader.GetInt();
+            character.AttributeLevels.Add(entry);
+        }
+        count = reader.GetInt();
+        for (var i = 0; i < count; ++i)
+        {
+            var entry = new CharacterSkillLevel();
+            entry.skillId = reader.GetString();
+            entry.level = reader.GetInt();
+            character.SkillLevels.Add(entry);
+        }
+        count = reader.GetInt();
+        for (var i = 0; i < count; ++i)
+        {
+            var entry = new CharacterItem();
+            entry.id = reader.GetString();
+            entry.itemId = reader.GetString();
+            entry.level = reader.GetInt();
+            entry.amount = reader.GetInt();
+            character.EquipItems.Add(entry);
+        }
+        count = reader.GetInt();
+        for (var i = 0; i < count; ++i)
+        {
+            var entry = new CharacterItem();
+            entry.id = reader.GetString();
+            entry.itemId = reader.GetString();
+            entry.level = reader.GetInt();
+            entry.amount = reader.GetInt();
+            character.NonEquipItems.Add(entry);
+        }
+        return character;
+    }
+}
