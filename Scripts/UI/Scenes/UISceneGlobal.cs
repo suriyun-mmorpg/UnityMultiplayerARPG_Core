97758eea39677dc3f80c9623cf07ee2dba679f41
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-03-01 00:03:14 +0700

    Add ui scene global which will be used in any ui to show generic uis

---
+++ Scripts/UI/Scenes/UISceneGlobal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISceneGlobal : MonoBehaviour
+{
+    public static UISceneGlobal Singleton { get; private set; }
+    public UIMessageDialog uiMessageDialog;
+
+    private void Awake()
+    {
+        if (Singleton != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Singleton = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ShowMessageDialog(string title,
+        string description,
+        bool showButtonOkay = true,
+        bool showButtonYes = false,
+        bool showButtonNo = false,
+        bool showButtonCancel = false,
+        System.Action onClickOkay = null,
+        System.Action onClickYes = null,
+        System.Action onClickNo = null,
+        System.Action onClickCancel = null)
+    {
+        uiMessageDialog.Show(title,
+            description,
+            showButtonOkay,
+            showButtonYes,
+            showButtonNo,
+            showButtonCancel,
+            onClickOkay,
+            onClickYes,
+            onClickNo,
+            onClickCancel);
+    }
+}
