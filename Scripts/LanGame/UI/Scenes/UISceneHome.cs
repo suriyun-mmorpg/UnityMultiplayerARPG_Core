3a2023ba511a187e4cca778beb8c5e3414417230
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-03-02 15:54:10 +0700

    Implement lan game connection

---
+++ Scripts/LanGame/UI/Scenes/UISceneHome.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISceneHome : UIHistory
+{
+    public UICharacterCreate uiCharacterCreate;
+    public UICharacterList uiCharacterList;
+    public UILanConnection uiLanConnection;
+
+    public void OnClickSinglePlayer()
+    {
+        LanRpgNetworkManager.StartType = LanRpgNetworkManager.GameStartType.SinglePlayer;
+        Next(uiCharacterList);
+    }
+
+    public void OnClickMultiplayer()
+    {
+        Next(uiLanConnection);
+    }
+
+    public void OnClickJoin()
+    {
+        LanRpgNetworkManager.StartType = LanRpgNetworkManager.GameStartType.Client;
+        LanRpgNetworkManager.ConnectingNetworkAddress = uiLanConnection.NetworkAddress;
+        Next(uiCharacterList);
+    }
+
+    public void OnClickHost()
+    {
+        LanRpgNetworkManager.StartType = LanRpgNetworkManager.GameStartType.Host;
+        Next(uiCharacterList);
+    }
+
+    public void OnClickCreateCharacter()
+    {
+        Next(uiCharacterCreate);
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+}
