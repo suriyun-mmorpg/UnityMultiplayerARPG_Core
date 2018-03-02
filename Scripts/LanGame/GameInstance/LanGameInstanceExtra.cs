cb78449a2df1ba80eb04d64cf1a03c22e76ef2a3
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-03-02 15:53:21 +0700

    Add lan game instance extra

---
+++ Scripts/LanGame/GameInstance/LanGameInstanceExtra.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanGameInstanceExtra : BaseGameInstanceExtra
+{
+    public string networkAddress = "127.0.0.1";
+    public int networkPort = 7770;
+    public int maxConnections = 4;
+}
