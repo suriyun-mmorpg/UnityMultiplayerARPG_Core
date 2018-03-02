9c43462d02c3af5feb98306b80cbab3fb20f3027
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-03-02 15:51:01 +0700

    Add lan connection ui

---
+++ Scripts/LanGame/UI/UILanConnection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILanConnection : UIBase
+{
+    public InputField inputNetworkAddress;
+    public string DefaultNetworkAddress
+    {
+        get { return GameInstance.Singleton.GetExtra<LanGameInstanceExtra>().networkAddress; }
+    }
+    public string NetworkAddress
+    {
+        get { return inputNetworkAddress == null ? DefaultNetworkAddress : inputNetworkAddress.text; }
+    }
+
+    public override void Show()
+    {
+        base.Show();
+        if (inputNetworkAddress != null)
+            inputNetworkAddress.text = DefaultNetworkAddress;
+    }
+}
