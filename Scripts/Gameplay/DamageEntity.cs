1fc10ead2f40c50a3fd87ca6422be5fdf6d1197d
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-03-05 17:29:12 +0700

    Change damage entity to abstract, I'll going to implement multiple kind of damage entity later

--- Scripts/Gameplay/DamageEntity.cs
+++ Scripts/Gameplay/DamageEntity.cs
@@ -2,7 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageEntity : RpgNetworkEntity
+public abstract class DamageEntity : RpgNetworkEntity
 {
+    protected CharacterEntity attacker;
+    protected Dictionary<string, DamageAmount> damageAmounts;
+    protected Dictionary<string, DamageEffectivenessAttribute> effectivenessAttributes;
 
+    public virtual void SetupDamage(CharacterEntity attacker,
+        Dictionary<string, DamageAmount> damageAmounts,
+        Dictionary<string, DamageEffectivenessAttribute> effectivenessAttributes)
+    {
+        this.attacker = attacker;
+        this.damageAmounts = damageAmounts;
+        this.effectivenessAttributes = effectivenessAttributes;
+    }
+
+    public virtual void ApplyDamageTo(CharacterEntity target)
+    {
+        if (target == null)
+            return;
+        target.ReceiveDamage(attacker, damageAmounts, effectivenessAttributes);
+    }
 }
---
+++ Scripts/Gameplay/MissileDamageEntity.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MissileDamageEntity : DamageEntity
+{
+    protected float missileDistance;
+    protected float missileSpeed;
+
+    private Rigidbody tempRigidbody;
+    public Rigidbody TempRigidbody
+    {
+        get
+        {
+            if (tempRigidbody == null)
+                tempRigidbody = GetComponent<Rigidbody>();
+            return tempRigidbody;
+        }
+    }
+
+    public void SetupDamage(CharacterEntity attacker,
+        Dictionary<string, DamageAmount> damageAmounts,
+        Dictionary<string, DamageEffectivenessAttribute> effectivenessAttributes,
+        float missileDistance,
+        float missileSpeed)
+    {
+        SetupDamage(attacker, damageAmounts, effectivenessAttributes);
+        this.missileDistance = missileDistance;
+        this.missileSpeed = missileSpeed;
+        TempRigidbody.velocity = attacker.TempTransform.forward * missileSpeed;
+
+        if (missileDistance > 0)
+            NetworkDestroy(missileSpeed / missileDistance);
+        else
+            NetworkDestroy();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!IsServer)
+            return;
+
+        var characterEntity = other.GetComponent<CharacterEntity>();
+        if (characterEntity == null)
+            return;
+
+        ApplyDamageTo(characterEntity);
+        NetworkDestroy();
+    }
+}
