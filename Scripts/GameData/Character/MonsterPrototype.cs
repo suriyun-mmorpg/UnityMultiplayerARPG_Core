158a86a103b699581968b226d8f238e4152d6295
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-03-21 13:48:10 +0700

    Add monster codes

---
+++ Scripts/GameData/Character/MonsterPrototype.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MonsterCharacteristic
+{
+    Normal,
+    Aggressive,
+}
+
+
+[CreateAssetMenu(fileName = "MonsterPrototype", menuName = "Create GameData/MonsterPrototype")]
+public class MonsterPrototype : CharacterPrototype
+{
+    [Header("Monster Data")]
+    public MonsterCharacteristic characteristic;
+    [Tooltip("Level will be used to show only, not calculating with stats/skills")]
+    public int level;
+    public CharacterStats stats;
+    public Skill[] skills;
+
+    [Header("Monster Rewards")]
+    public int randomExpMin;
+    public int randomExpMax;
+    public int randomGoldMin;
+    public int randomGoldMax;
+    public ItemDrop[] randomItems;
+
+    public int RandomExp()
+    {
+        var min = randomExpMin;
+        var max = randomExpMax;
+        if (min > max)
+            min = max;
+        return Random.Range(min, max);
+    }
+
+    public int RandomGold()
+    {
+        var min = randomGoldMin;
+        var max = randomGoldMax;
+        if (min > max)
+            min = max;
+        return Random.Range(min, max);
+    }
+
+    public List<ItemAmountPair> RandomItems()
+    {
+        var rewards = new List<ItemAmountPair>();
+        foreach (var randomItem in randomItems)
+        {
+            if (randomItem.item == null ||
+                randomItem.amount == 0 ||
+                !GameInstance.Items.ContainsKey(randomItem.item.Id) ||
+                Random.value > randomItem.dropRate)
+                continue;
+                rewards.Add(new ItemAmountPair()
+                {
+                    item = randomItem.item,
+                    amount = randomItem.amount,
+                });
+        }
+        return rewards;
+    }
+}
---
+++ Scripts/Gameplay/MonsterCharacterEntity.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(NavMeshAgent))]
+public class MonsterCharacterEntity : CharacterEntity
+{
+    #region Cache components
+    private CapsuleCollider cacheCapsuleCollider;
+    public CapsuleCollider CacheCapsuleCollider
+    {
+        get
+        {
+            if (cacheCapsuleCollider == null)
+                cacheCapsuleCollider = GetComponent<CapsuleCollider>();
+            return cacheCapsuleCollider;
+        }
+    }
+
+    private NavMeshAgent cacheNavMeshAgent;
+    public NavMeshAgent CacheNavMeshAgent
+    {
+        get
+        {
+            if (cacheNavMeshAgent == null)
+                cacheNavMeshAgent = GetComponent<NavMeshAgent>();
+            return cacheNavMeshAgent;
+        }
+    }
+    #endregion
+    protected override void SetupModel(CharacterModel characterModel)
+    {
+        CacheCapsuleCollider.center = characterModel.center;
+        CacheCapsuleCollider.radius = characterModel.radius;
+        CacheCapsuleCollider.height = characterModel.height;
+    }
+
+    protected override Vector3 GetMovementVelocity()
+    {
+        return CacheNavMeshAgent.velocity;
+    }
+}
