f3cf1eb280d42cadaf17bfb7a9569eb9a7195b4d
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-03-31 02:37:59 +0700

    Add more abstract calculation

--- Scripts/GameplayRule/BaseGameplayRule.cs
+++ Scripts/GameplayRule/BaseGameplayRule.cs
@@ -5,5 +5,9 @@ using UnityEngine;
 public abstract class BaseGameplayRule : ScriptableObject
 {
     public abstract float GetHitChance(ICharacterData attacker, ICharacterData damageReceiver);
+    public abstract float GetCriticalChance(ICharacterData attacker, ICharacterData damageReceiver);
+    public abstract float GetCriticalDamage(ICharacterData attacker, ICharacterData damageReceiver, float damage);
+    public abstract float GetBlockChance(ICharacterData attacker, ICharacterData damageReceiver);
+    public abstract float GetBlockDamage(ICharacterData attacker, ICharacterData damageReceiver, float damage);
     public abstract float GetDamageReducedByResistance(ICharacterData damageReceiver, float damageAmount, Resistance resistance);
 }
