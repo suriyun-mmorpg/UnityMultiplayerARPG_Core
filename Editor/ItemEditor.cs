3f70d4505ea6fa3b235f3fbca9a29553fea1d875
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-04-23 13:46:02 +0700

    Improve item editor codes

--- Editor/ItemEditor.cs
+++ Editor/ItemEditor.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
-using System.Linq.Expressions;
-using System;
 
 [CustomEditor(typeof(Item))]
 public class ItemEditor : BaseCustomEditor
@@ -13,37 +11,31 @@ public class ItemEditor : BaseCustomEditor
         if (cacheItem == null)
             cacheItem = CreateInstance<Item>();
         // Armor
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Armor.ToString(), GetMemberName(() => cacheItem.equipmentModels));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Armor.ToString(), GetMemberName(() => cacheItem.requirement));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Armor.ToString(), GetMemberName(() => cacheItem.increaseAttributes));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Armor.ToString(), GetMemberName(() => cacheItem.increaseResistances));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Armor.ToString(), GetMemberName(() => cacheItem.increaseDamages));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Armor.ToString(), GetMemberName(() => cacheItem.increaseStats));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Armor.ToString(), GetMemberName(() => cacheItem.armorType));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Armor.ToString(), cacheItem.GetMemberName(a => a.equipmentModels));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Armor.ToString(), cacheItem.GetMemberName(a => a.requirement));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Armor.ToString(), cacheItem.GetMemberName(a => a.increaseAttributes));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Armor.ToString(), cacheItem.GetMemberName(a => a.increaseResistances));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Armor.ToString(), cacheItem.GetMemberName(a => a.increaseDamages));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Armor.ToString(), cacheItem.GetMemberName(a => a.increaseStats));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Armor.ToString(), cacheItem.GetMemberName(a => a.armorType));
         // Weapon
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Weapon.ToString(), GetMemberName(() => cacheItem.equipmentModels));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Weapon.ToString(), GetMemberName(() => cacheItem.subEquipmentModels));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Weapon.ToString(), GetMemberName(() => cacheItem.requirement));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Weapon.ToString(), GetMemberName(() => cacheItem.increaseAttributes));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Weapon.ToString(), GetMemberName(() => cacheItem.increaseResistances));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Weapon.ToString(), GetMemberName(() => cacheItem.increaseDamages));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Weapon.ToString(), GetMemberName(() => cacheItem.increaseStats));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Weapon.ToString(), GetMemberName(() => cacheItem.weaponType));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Weapon.ToString(), GetMemberName(() => cacheItem.damageAmount));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Weapon.ToString(), cacheItem.GetMemberName(a => a.equipmentModels));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Weapon.ToString(), cacheItem.GetMemberName(a => a.subEquipmentModels));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Weapon.ToString(), cacheItem.GetMemberName(a => a.requirement));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Weapon.ToString(), cacheItem.GetMemberName(a => a.increaseAttributes));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Weapon.ToString(), cacheItem.GetMemberName(a => a.increaseResistances));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Weapon.ToString(), cacheItem.GetMemberName(a => a.increaseDamages));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Weapon.ToString(), cacheItem.GetMemberName(a => a.increaseStats));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Weapon.ToString(), cacheItem.GetMemberName(a => a.weaponType));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Weapon.ToString(), cacheItem.GetMemberName(a => a.damageAmount));
         // Shield
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Shield.ToString(), GetMemberName(() => cacheItem.equipmentModels));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Shield.ToString(), GetMemberName(() => cacheItem.requirement));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Shield.ToString(), GetMemberName(() => cacheItem.increaseAttributes));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Shield.ToString(), GetMemberName(() => cacheItem.increaseResistances));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Shield.ToString(), GetMemberName(() => cacheItem.increaseDamages));
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Shield.ToString(), GetMemberName(() => cacheItem.increaseStats));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Shield.ToString(), cacheItem.GetMemberName(a => a.equipmentModels));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Shield.ToString(), cacheItem.GetMemberName(a => a.requirement));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Shield.ToString(), cacheItem.GetMemberName(a => a.increaseAttributes));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Shield.ToString(), cacheItem.GetMemberName(a => a.increaseResistances));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Shield.ToString(), cacheItem.GetMemberName(a => a.increaseDamages));
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Shield.ToString(), cacheItem.GetMemberName(a => a.increaseStats));
         // Potion
-        ShowOnEnum(GetMemberName(() => cacheItem.itemType), ItemType.Potion.ToString(), GetMemberName(() => cacheItem.buff));
-    }
-
-    public static string GetMemberName<T>(Expression<Func<T>> memberExpression)
-    {
-        MemberExpression expressionBody = (MemberExpression)memberExpression.Body;
-        return expressionBody.Member.Name;
+        ShowOnEnum(cacheItem.GetMemberName(a => a.itemType), ItemType.Potion.ToString(), cacheItem.GetMemberName(a => a.buff));
     }
 }
--- Scripts/Utils/GenericExtension.cs
+++ Scripts/Utils/GenericExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public static class GenericExtension
