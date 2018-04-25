1979c5836bd44b00b31d55ba7efaae0a978cc972
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-04-26 04:35:03 +0700

    Npc/Quest WIP

---
+++ Editor/NpcDialogEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(NpcDialog))]
+[CanEditMultipleObjects]
+public class NpcDialogEditor : BaseCustomEditor
+{
+    private static NpcDialog cacheNpcDialog;
+    protected override void SetFieldCondition()
+    {
+        if (cacheNpcDialog == null)
+            cacheNpcDialog = CreateInstance<NpcDialog>();
+        // Normal
+        ShowOnEnum(cacheNpcDialog.GetMemberName(a => a.type), NpcDialogType.Normal.ToString(), cacheNpcDialog.GetMemberName(a => a.menus));
+        // Quest
+        ShowOnEnum(cacheNpcDialog.GetMemberName(a => a.type), NpcDialogType.Quest.ToString(), cacheNpcDialog.GetMemberName(a => a.quest));
+        ShowOnEnum(cacheNpcDialog.GetMemberName(a => a.type), NpcDialogType.Quest.ToString(), cacheNpcDialog.GetMemberName(a => a.questAcceptedDialog));
+        ShowOnEnum(cacheNpcDialog.GetMemberName(a => a.type), NpcDialogType.Quest.ToString(), cacheNpcDialog.GetMemberName(a => a.questDeclinedDialog));
+        ShowOnEnum(cacheNpcDialog.GetMemberName(a => a.type), NpcDialogType.Quest.ToString(), cacheNpcDialog.GetMemberName(a => a.questAbandonedDialog));
+        ShowOnEnum(cacheNpcDialog.GetMemberName(a => a.type), NpcDialogType.Quest.ToString(), cacheNpcDialog.GetMemberName(a => a.questCompletedDailog));
+    }
+}
