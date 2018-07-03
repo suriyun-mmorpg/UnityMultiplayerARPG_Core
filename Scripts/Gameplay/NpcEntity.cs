758317f26991d23ebc51c9f17dd8bf04efbac1b6
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-07-03 12:41:22 +0700

    Remove unused codes

--- Scripts/Gameplay/NpcEntity.cs
+++ Scripts/Gameplay/NpcEntity.cs
@@ -5,23 +5,8 @@ namespace MultiplayerARPG
 {
     public sealed class NpcEntity : RpgNetworkEntity
     {
-        [Tooltip("Model will be instantiated inside this transform, if not set will use this component's transform")]
-        [SerializeField]
-        private Transform modelContainer;
         [Tooltip("Set it to force to not change character model by data Id, when set it model container will not be used")]
-        [SerializeField]
-        private CharacterModel permanentlyModel;
         public NpcDialog startDialog;
-
-        public Transform CacheModelContainer
-        {
-            get
-            {
-                if (modelContainer == null)
-                    modelContainer = GetComponent<Transform>();
-                return modelContainer;
-            }
-        }
 
         protected override void Awake()
         {
