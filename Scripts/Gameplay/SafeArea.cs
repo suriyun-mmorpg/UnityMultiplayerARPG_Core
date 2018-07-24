640e956871ed4bfd5c3957a12a50762cb90a9b37
insthync <ittipon.bay@gmail.com>
2018-07-24 14:24:13 +0700

    Add safe area

---
+++ Scripts/Gameplay/SafeArea.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public class SafeArea : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag(GameInstance.Singleton.playerTag) && !other.CompareTag(GameInstance.Singleton.monsterTag))
+                return;
+            
+            var characterEntity = other.GetComponent<BasePlayerCharacterEntity>();
+            if (characterEntity == null)
+                return;
+
+            characterEntity.safeArea = this;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!other.CompareTag(GameInstance.Singleton.playerTag) && !other.CompareTag(GameInstance.Singleton.monsterTag))
+                return;
+
+            var characterEntity = other.GetComponent<BasePlayerCharacterEntity>();
+            if (characterEntity == null)
+                return;
+
+            characterEntity.safeArea = null;
+        }
+    }
+}
