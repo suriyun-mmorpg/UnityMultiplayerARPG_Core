853da40ba8e5ca451a90b0db5a614b98a662e816
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-08-07 17:52:51 +0700

    Add migrate ui wrapper codes

---
+++ Scripts/UI/Utils/MigrateUIWrapper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MigrateUIWrapper : MonoBehaviour
+{
+    [ContextMenu("Migrate UIs")]
+    public void MigrateUIs()
+    {
+        var objects = FindObjectsOfType<UIBase>();
+        foreach (var obj in objects)
+        {
+            var type = obj.GetType();
+            var method = type.GetMethod("MigrateUIComponents", BindingFlags.Public | BindingFlags.Instance);
+            var args = new object[0];
+            if (method != null)
+                method.Invoke(obj, args);
+#if UNITY_EDITOR
+            EditorUtility.SetDirty(obj.gameObject);
+#endif
+        }
+    }
+}
