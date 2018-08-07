32172f92bce22fda49d859366a74f85120299b6d
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-08-07 18:03:43 +0700

    Make migration script can find objects from inactive objects

--- Scripts/UI/Utils/MigrateUIWrapper.cs
+++ Scripts/UI/Utils/MigrateUIWrapper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -12,7 +13,22 @@ public class MigrateUIWrapper : MonoBehaviour
     [ContextMenu("Migrate UIs")]
     public void MigrateUIs()
     {
-        var objects = FindObjectsOfType<UIBase>();
+        // Find all components include inactive
+        List<UIBase> objects = new List<UIBase>();
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            var s = SceneManager.GetSceneAt(i);
+            if (s.isLoaded)
+            {
+                var allGameObjects = s.GetRootGameObjects();
+                for (int j = 0; j < allGameObjects.Length; j++)
+                {
+                    var go = allGameObjects[j];
+                    objects.AddRange(go.GetComponentsInChildren<UIBase>(true));
+                }
+            }
+        }
+        // Call migrate ui components
         foreach (var obj in objects)
         {
             var type = obj.GetType();
