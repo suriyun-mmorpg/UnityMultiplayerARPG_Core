11462a0592a03536dc8848559b79c0368f2164fe
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-08-08 13:43:27 +0700

    Add component collector, will use it to collect ui components to detect they are in-use or not

---
+++ Scripts/Utils/ComponentCollector/BaseComponentRegister.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseComponentRegister<T> : MonoBehaviour where T : Component
+{
+    private void Awake()
+    {
+        var components = GetComponents<T>();
+        foreach (var component in components)
+        {
+            ComponentCollector.Add(component);
+        }
+    }
+}
