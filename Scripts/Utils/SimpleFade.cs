ded23c989033061f880fd21d1e3663f8973d23cb
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-08-24 20:41:17 +0700

    Add simple fade script

---
+++ Scripts/Utils/SimpleFade.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFade : MonoBehaviour
+{
+    public float fadeSpeed = 0.2f;
+    private Texture2D blackTexture;
+    private bool fadeIn;
+    private float alpha;
+
+    void Awake()
+    {
+        alpha = 1;
+        blackTexture = new Texture2D(1, 1);
+        blackTexture.SetPixel(0, 0, new Color(0, 0, 0, alpha));
+        blackTexture.Apply();
+    }
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), blackTexture);
+    }
+
+    void Update()
+    {
+        if (!fadeIn)
+        {
+            if (alpha > 0)
+            {
+                alpha -= Time.deltaTime * fadeSpeed;
+                if (alpha < 0) alpha = 0f;
+                blackTexture.SetPixel(0, 0, new Color(0, 0, 0, alpha));
+                blackTexture.Apply();
+            }
+        }
+        if (fadeIn)
+        {
+            if (alpha < 1)
+            {
+                alpha += Time.deltaTime * fadeSpeed;
+                if (alpha > 1) alpha = 1f;
+                blackTexture.SetPixel(0, 0, new Color(0, 0, 0, alpha));
+                blackTexture.Apply();
+            }
+        }
+    }
+
+    public void FadeIn()
+    {
+        fadeIn = true;
+    }
+
+    public void FadeOut()
+    {
+        fadeIn = false;
+    }
+}
