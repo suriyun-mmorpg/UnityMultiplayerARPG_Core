da2521328a50d9384593465292cda6d287300325
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-08-29 05:32:14 +0700

    Add sort order codes

---
+++ Scripts/Utils/SortSpriteOrderY.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SortSpriteOrderY : MonoBehaviour
+{
+    private SpriteRenderer cacheSpriteRenderer;
+    public SpriteRenderer CacheSpriteRenderer
+    {
+        get
+        {
+            if (cacheSpriteRenderer == null)
+                cacheSpriteRenderer = GetComponent<SpriteRenderer>();
+            return cacheSpriteRenderer;
+        }
+    }
+
+    void Update()
+    {
+        CacheSpriteRenderer.sortingOrder = -(int)(transform.position.y * 100);
+    }
+}
