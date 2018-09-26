4a74dbe1445db7823817f4f6c5ff7799be69e06f
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-09-27 02:28:51 +0700

    Add building save data extension

---
+++ Scripts/WorldData/BuildingSaveDataExtension.cs
@@ -0,0 +1,27 @@
+﻿public static partial class BuildingSaveDataExtension
+{
+    private static System.Type classType;
+    public static System.Type ClassType
+    {
+        get
+        {
+            if (classType == null)
+                classType = typeof(PlayerCharacterDataExtension);
+            return classType;
+        }
+    }
+
+    public static T CloneTo<T>(this IBuildingSaveData from, T to) where T : IBuildingSaveData
+    {
+        to.Id = from.Id;
+        to.ParentId = from.ParentId;
+        to.DataId = from.DataId;
+        to.CurrentHp = from.CurrentHp;
+        to.Position = from.Position;
+        to.Rotation = from.Rotation;
+        to.CreatorId = from.CreatorId;
+        to.CreatorName = from.CreatorName;
+        DevExtUtils.InvokeStaticDevExtMethods(ClassType, "CloneTo", from, to);
+        return to;
+    }
+}
