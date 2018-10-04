f8b4c29799760eac5a473c332b8768cb7961bbc5
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-10-04 15:34:39 +0700

    Add guild roles and member roles data to guild data message

--- Scripts/Networking/Messages/ResponseGuildDataMessage.cs
+++ Scripts/Networking/Messages/ResponseGuildDataMessage.cs
@@ -10,6 +10,8 @@ namespace MultiplayerARPG
         public int exp;
         public int skillPoint;
         public string message;
+        public GuildRole[] roles;
+        public byte[] memberRoles;
 
         public override void DeserializeData(NetDataReader reader)
         {
@@ -19,6 +21,34 @@ namespace MultiplayerARPG
             exp = reader.GetInt();
             skillPoint = reader.GetInt();
             message = reader.GetString();
+            // Put roles
+            var length = reader.GetByte();
+            var roles = new GuildRole[length];
+            if (length > 0)
+            {
+                for (var i = 0; i < length; ++i)
+                {
+                    var entry = new GuildRole();
+                    entry.roleName = reader.GetString();
+                    entry.canInvite = reader.GetBool();
+                    entry.canKick = reader.GetBool();
+                    entry.shareExpPercentage = reader.GetByte();
+                    roles[i] = entry;
+                }
+            }
+            this.roles = roles;
+            // Put member roles
+            length = reader.GetByte();
+            var memberRoles = new byte[length];
+            if (length > 0)
+            {
+                for (var i = 0; i < length; ++i)
+                {
+                    memberRoles[i] = reader.GetByte();
+                }
+            }
+            this.memberRoles = memberRoles;
+
             base.DeserializeData(reader);
         }
 
@@ -30,6 +60,31 @@ namespace MultiplayerARPG
             writer.Put(exp);
             writer.Put(skillPoint);
             writer.Put(message);
+            // Put roles
+            var length = (byte)(roles == null ? 0 : roles.Length);
+            writer.Put(length);
+            if (length > 0)
+            {
+                for (var i = 0; i < length; ++i)
+                {
+                    var entry = roles[i];
+                    writer.Put(entry.roleName);
+                    writer.Put(entry.canInvite);
+                    writer.Put(entry.canKick);
+                    writer.Put(entry.shareExpPercentage);
+                }
+            }
+            // Put member roles
+            length = (byte)(memberRoles == null ? 0 : memberRoles.Length);
+            writer.Put(length);
+            if (length > 0)
+            {
+                for (var i = 0; i < length; ++i)
+                {
+                    writer.Put(memberRoles[i]);
+                }
+            }
+
             base.SerializeData(writer);
         }
     }
