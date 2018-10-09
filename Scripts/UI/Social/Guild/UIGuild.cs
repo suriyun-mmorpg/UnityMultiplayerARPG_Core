324246b036e795d377fe9d3389079e32c7159f8a
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-10-09 08:38:37 +0700

    Remove unused using namespaces

--- Scripts/UI/Social/Guild/UIGuild.cs
+++ Scripts/UI/Social/Guild/UIGuild.cs
@@ -1,9 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-using LiteNetLibManager;
 
 namespace MultiplayerARPG
 {
