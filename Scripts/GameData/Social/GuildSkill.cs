15b97d659d663bff2a110d5bc6bb4df6ce8a97a9
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-10-29 14:15:17 +0700

    Add guild skill

---
+++ Scripts/GameData/Social/GuildSkill.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    [CreateAssetMenu(fileName = "Guild Skill", menuName = "Create GameData/Guild Skill")]
+    public partial class GuildSkill : BaseGameData
+    {
+        [Header("Bonus")]
+        public IncrementalInt increaseMaxMember;
+        public IncrementalFloat increaseExpGainPercentage;
+        public IncrementalFloat increaseGoldGainPercentage;
+        public IncrementalFloat increaseShareExpGainPercentage;
+        public IncrementalFloat increaseShareGoldGainPercentage;
+        public IncrementalFloat decreaseExpLostPercentage;
+        public CharacterStatsIncremental increaseStats;
+        public AttributeIncremental[] increaseAttributes;
+        public ResistanceIncremental[] increaseResistances;
+        public DamageIncremental[] increaseDamages;
+    }
+}
