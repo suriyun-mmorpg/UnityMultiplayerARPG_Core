abb993a65f040b684c530e84fc52f994d39b4508
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-10-29 17:02:52 +0700

    Add guild skill editor

---
+++ Editor/GuildSkillEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace MultiplayerARPG
+{
+    [CustomEditor(typeof(GuildSkill))]
+    [CanEditMultipleObjects]
+    public class GuildSkillEditor : BaseCustomEditor
+    {
+        private static GuildSkill cacheGuildSkill;
+        protected override void SetFieldCondition()
+        {
+            if (cacheGuildSkill == null)
+                cacheGuildSkill = CreateInstance<GuildSkill>();
+            // Passive skill
+            ShowOnEnum(cacheGuildSkill.GetMemberName(a => a.skillType), SkillType.Active.ToString(), cacheGuildSkill.GetMemberName(a => a.increaseMaxMember));
+            ShowOnEnum(cacheGuildSkill.GetMemberName(a => a.skillType), SkillType.Active.ToString(), cacheGuildSkill.GetMemberName(a => a.increaseExpGainPercentage));
+            ShowOnEnum(cacheGuildSkill.GetMemberName(a => a.skillType), SkillType.Active.ToString(), cacheGuildSkill.GetMemberName(a => a.increaseGoldGainPercentage));
+            ShowOnEnum(cacheGuildSkill.GetMemberName(a => a.skillType), SkillType.Active.ToString(), cacheGuildSkill.GetMemberName(a => a.increaseShareExpGainPercentage));
+            ShowOnEnum(cacheGuildSkill.GetMemberName(a => a.skillType), SkillType.Active.ToString(), cacheGuildSkill.GetMemberName(a => a.increaseShareGoldGainPercentage));
+            ShowOnEnum(cacheGuildSkill.GetMemberName(a => a.skillType), SkillType.Active.ToString(), cacheGuildSkill.GetMemberName(a => a.decreaseExpLostPercentage));
+            // Active skill
+            ShowOnEnum(cacheGuildSkill.GetMemberName(a => a.skillType), SkillType.Active.ToString(), cacheGuildSkill.GetMemberName(a => a.coolDownDuration));
+            ShowOnEnum(cacheGuildSkill.GetMemberName(a => a.skillType), SkillType.Active.ToString(), cacheGuildSkill.GetMemberName(a => a.buff));
+        }
+    }
+}
