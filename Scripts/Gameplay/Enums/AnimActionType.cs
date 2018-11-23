44133e6c66c392c5085895f3caa897a4d432cd3a
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-11-24 04:53:49 +0700

    Move an enum from another class files

---
+++ Scripts/Gameplay/Enums/AnimActionType.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiplayerARPG
+{
+    public enum AnimActionType : byte
+    {
+        None,
+        Generic,
+        AttackRightHand,
+        AttackLeftHand,
+        Skill,
+    }
+}
