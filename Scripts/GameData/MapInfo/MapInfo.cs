cd73872042f4d074f27c6680453dd390219c9dcf
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-12-20 19:17:39 +0700

    Add options to allows players to return to save point by return key, and override respawn points

--- Scripts/GameData/MapInfo/MapInfo.cs
+++ Scripts/GameData/MapInfo/MapInfo.cs
@@ -10,6 +10,15 @@ namespace MultiplayerARPG
         public UnityScene scene;
         [Tooltip("This will be used when new character have been created, and this map data is start map")]
         public Vector3 startPosition;
+        [Tooltip("If this is `TRUE`, player can battle other players")]
         public bool canPvp;
+        [Tooltip("If this is `TRUE`, player can return to save point by `return` key. Else it will able to do that when dead only")]
+        public bool canReturnToSavePoint;
+        [Tooltip("If this is `TRUE`, player will return to `overrideRespawnPointPosition` in the `overrideRespawnPointScene`")]
+        public bool overrideRespawnPoint;
+        [Tooltip("Scene which player will returning to, when dead or press `return` key")]
+        public UnityScene overrideRespawnPointScene;
+        [Tooltip("Position in the `overrideRespawnPointScene` which player will returning to, when dead or press `return` key")]
+        public Vector3 overrideRespawnPointPosition;
     }
 }
