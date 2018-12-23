38da83b497b30053770d35afa07ba4ac4e2b6597
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-12-23 07:55:18 +0700

    Add some transform utils

---
+++ Scripts/Utils/FixRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixRotation : MonoBehaviour
+{
+    public Vector3 eulerAngles;
+    private void LateUpdate()
+    {
+        transform.eulerAngles = eulerAngles;
+    }
+}
