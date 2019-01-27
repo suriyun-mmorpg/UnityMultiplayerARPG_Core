1ae32da7c2369904e7fd4459f183b8b0a7b88531
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-01-27 16:57:44 +0700

    Add ui character class, will be used in create character scene to change character class

---
+++ Scripts/UI/Character/UICharacterClass.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MultiplayerARPG
+{
+    public class UICharacterClass : UISelectionEntry<BaseCharacter>
+    {
+        [Header("Generic Info Format")]
+        [Tooltip("Title Format => {0} = {Title}")]
+        public string titleFormat = "{0}";
+        [Tooltip("Description Format => {0} = {Description}")]
+        public string descriptionFormat = "{0}";
+
+        [Header("UI Elements")]
+        public TextWrapper uiTextTitle;
+        public TextWrapper uiTextDescription;
+        public Image imageIcon;
+        public UICharacterStats uiStats;
+        public UIAttributeAmounts uiAttributes;
+        public UIResistanceAmounts uiResistances;
+        public UISkillLevels uiSkills;
+
+        protected override void UpdateData()
+        {
+            if (uiTextTitle != null)
+                uiTextTitle.text = string.Format(titleFormat, Data == null ? LanguageManager.GetUnknowTitle() : Data.Title);
+
+            if (uiTextDescription != null)
+                uiTextDescription.text = string.Format(descriptionFormat, Data == null ? LanguageManager.GetUnknowDescription() : Data.Description);
+
+            if (imageIcon != null)
+            {
+                Sprite iconSprite = Data == null ? null : Data.icon;
+                imageIcon.gameObject.SetActive(iconSprite != null);
+                imageIcon.sprite = iconSprite;
+            }
+
+            if (uiStats != null)
+                uiStats.Data = Data.GetCharacterStats(1);
+
+            if (uiAttributes != null)
+                uiAttributes.Data = Data.GetCharacterAttributes(1);
+
+            if (uiResistances != null)
+                uiResistances.Data = Data.GetCharacterResistances(1);
+
+            if (uiSkills != null)
+                uiSkills.Data = Data.CacheSkillLevels;
+        }
+    }
+}
