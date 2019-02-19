7f4f6ee4ac2fcf2a6c898f1615af62de0a8f64fc
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-02-20 01:47:58 +0700

    WIP storage system

---
+++ Scripts/Storage/SerializationSurrogates/StorageSaveDataSerializationSurrogate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+public class StorageSaveDataSerializationSurrogate : ISerializationSurrogate
+{
+    public void GetObjectData(object obj, SerializationInfo info, StreamingContext context)
+    {
+        StorageSaveData data = (StorageSaveData)obj;
+        info.AddListValue("storageItems", data.storageItems);
+    }
+
+    public object SetObjectData(object obj, SerializationInfo info, StreamingContext context, ISurrogateSelector selector)
+    {
+        StorageSaveData data = (StorageSaveData)obj;
+        data.storageItems = new List<StorageCharacterItem>(info.GetListValue<StorageCharacterItem>("storageItems"));
+        obj = data;
+        return obj;
+    }
+}
---
+++ Scripts/Storage/StorageSaveData.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+[System.Serializable]
+public class StorageSaveData
+{
+    public List<StorageCharacterItem> storageItems = new List<StorageCharacterItem>();
+
+    public void SavePersistentWorldData(string id, string map)
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        SurrogateSelector surrogateSelector = new SurrogateSelector();
+        surrogateSelector.AddAllUnitySurrogate();
+        StorageCharacterItemSerializationSurrogate storageCharacterItemSS = new StorageCharacterItemSerializationSurrogate();
+        StorageSaveDataSerializationSurrogate storageSaveDataSS = new StorageSaveDataSerializationSurrogate();
+        surrogateSelector.AddSurrogate(typeof(BuildingSaveData), new StreamingContext(StreamingContextStates.All), storageCharacterItemSS);
+        surrogateSelector.AddSurrogate(typeof(StorageSaveData), new StreamingContext(StreamingContextStates.All), storageSaveDataSS);
+        binaryFormatter.SurrogateSelector = surrogateSelector;
+        string path = Application.persistentDataPath + "/" + id + "_storage_" + map + ".sav";
+        FileStream file = File.Open(path, FileMode.OpenOrCreate);
+        binaryFormatter.Serialize(file, this);
+        file.Close();
+    }
+
+    public void LoadPersistentData(string id, string map)
+    {
+        string path = Application.persistentDataPath + "/" + id + "_storage_" + map + ".sav";
+        if (File.Exists(path))
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            SurrogateSelector surrogateSelector = new SurrogateSelector();
+            surrogateSelector.AddAllUnitySurrogate();
+            StorageCharacterItemSerializationSurrogate storageCharacterItemSS = new StorageCharacterItemSerializationSurrogate();
+            StorageSaveDataSerializationSurrogate storageSaveDataSS = new StorageSaveDataSerializationSurrogate();
+            surrogateSelector.AddSurrogate(typeof(BuildingSaveData), new StreamingContext(StreamingContextStates.All), storageCharacterItemSS);
+            surrogateSelector.AddSurrogate(typeof(StorageSaveData), new StreamingContext(StreamingContextStates.All), storageSaveDataSS);
+            binaryFormatter.SurrogateSelector = surrogateSelector;
+            FileStream file = File.Open(path, FileMode.Open);
+            StorageSaveData result = (StorageSaveData)binaryFormatter.Deserialize(file);
+            storageItems = result.storageItems;
+            file.Close();
+        }
+    }
+}
