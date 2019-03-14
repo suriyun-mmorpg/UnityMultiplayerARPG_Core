08d801abe0cdd872061ab5f00fa7af810ce4b458
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-03-14 23:29:10 +0700

    Empty slot cannot be dragged

--- Scripts/UI/Item/DragAndDropHandler/UICharacterItemDragHandler.cs
+++ Scripts/UI/Item/DragAndDropHandler/UICharacterItemDragHandler.cs
@@ -38,7 +38,7 @@ namespace MultiplayerARPG
                     case SourceLocation.NonEquipItems:
                     case SourceLocation.EquipItems:
                     case SourceLocation.StorageItems:
-                        return uiCharacterItem.IndexOfData >= 0;
+                        return uiCharacterItem.IndexOfData >= 0 && uiCharacterItem.CharacterItem.NotEmptySlot();
                     case SourceLocation.Hotkey:
                         return true;
                 }
