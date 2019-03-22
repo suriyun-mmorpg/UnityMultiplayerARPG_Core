2a131c093df8ab92696b2a8d17e962e1a2a13f05
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-03-23 03:04:30 +0700

    Move buff functions to partial class file

---
+++ Scripts/Gameplay/CharacterEntity/BaseCharacterEntity_BuffFunctions.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public partial class BaseCharacterEntity
+    {
+        public void ApplyBuff(int dataId, BuffType type, short level)
+        {
+            if (IsDead() || !IsServer)
+                return;
+
+            int buffIndex = this.IndexOfBuff(dataId, type);
+            if (buffIndex >= 0)
+                buffs.RemoveAt(buffIndex);
+
+            CharacterBuff newBuff = CharacterBuff.Create(type, dataId, level);
+            newBuff.Apply();
+            buffs.Add(newBuff);
+
+            if (newBuff.GetDuration() <= 0f)
+            {
+                int tempAmount = 0;
+                // Damage over time
+                DamageElement damageElement;
+                MinMaxFloat damageAmount;
+                float tempReceivingDamage;
+                foreach (KeyValuePair<DamageElement, MinMaxFloat> damageOverTime in newBuff.GetDamageOverTimes())
+                {
+                    damageElement = damageOverTime.Key;
+                    damageAmount = damageOverTime.Value;
+                    tempReceivingDamage = (float)damageElement.GetDamageReducedByResistance(this, damageAmount.Random());
+                    if (tempReceivingDamage > 0f)
+                        tempAmount += (int)tempReceivingDamage;
+                }
+                CurrentHp -= tempAmount;
+                // Hp recovery
+                tempAmount = newBuff.GetRecoveryHp();
+                if (tempAmount != 0)
+                {
+                    CurrentHp += tempAmount;
+                    RequestCombatAmount(CombatAmountType.HpRecovery, tempAmount);
+                }
+                // Mp recovery
+                tempAmount = newBuff.GetRecoveryMp();
+                if (tempAmount != 0)
+                {
+                    CurrentMp += tempAmount;
+                    RequestCombatAmount(CombatAmountType.MpRecovery, tempAmount);
+                }
+                // Stamina recovery
+                tempAmount = newBuff.GetRecoveryStamina();
+                if (tempAmount != 0)
+                {
+                    CurrentStamina += tempAmount;
+                    RequestCombatAmount(CombatAmountType.StaminaRecovery, tempAmount);
+                }
+                // Food recovery
+                tempAmount = newBuff.GetRecoveryFood();
+                if (tempAmount != 0)
+                {
+                    CurrentFood += tempAmount;
+                    RequestCombatAmount(CombatAmountType.FoodRecovery, tempAmount);
+                }
+                // Water recovery
+                tempAmount = newBuff.GetRecoveryWater();
+                if (tempAmount != 0)
+                {
+                    CurrentWater += tempAmount;
+                    RequestCombatAmount(CombatAmountType.WaterRecovery, tempAmount);
+                }
+            }
+            ValidateRecovery();
+
+            this.InvokeInstanceDevExtMethods("ApplyBuff", dataId, type, level);
+        }
+
+        protected void ApplyPotionBuff(Item item, short level)
+        {
+            if (IsDead() || !IsServer || item == null || level <= 0)
+                return;
+            ApplyBuff(item.DataId, BuffType.PotionBuff, level);
+        }
+
+        protected virtual void ApplySkillBuff(Skill skill, short level)
+        {
+            if (IsDead() || !IsServer || skill == null || level <= 0)
+                return;
+            List<BaseCharacterEntity> tempCharacters;
+            switch (skill.skillBuffType)
+            {
+                case SkillBuffType.BuffToUser:
+                    ApplyBuff(skill.DataId, BuffType.SkillBuff, level);
+                    break;
+                case SkillBuffType.BuffToNearbyAllies:
+                    tempCharacters = FindAliveCharacters<BaseCharacterEntity>(skill.buffDistance.GetAmount(level), true, false, false);
+                    foreach (BaseCharacterEntity character in tempCharacters)
+                    {
+                        character.ApplyBuff(skill.DataId, BuffType.SkillBuff, level);
+                    }
+                    ApplyBuff(skill.DataId, BuffType.SkillBuff, level);
+                    break;
+                case SkillBuffType.BuffToNearbyCharacters:
+                    tempCharacters = FindAliveCharacters<BaseCharacterEntity>(skill.buffDistance.GetAmount(level), true, false, true);
+                    foreach (BaseCharacterEntity character in tempCharacters)
+                    {
+                        character.ApplyBuff(skill.DataId, BuffType.SkillBuff, level);
+                    }
+                    ApplyBuff(skill.DataId, BuffType.SkillBuff, level);
+                    break;
+            }
+        }
+
+        protected virtual void ApplyGuildSkillBuff(GuildSkill guildSkill, short level)
+        {
+            if (IsDead() || !IsServer || guildSkill == null || level <= 0)
+                return;
+            ApplyBuff(guildSkill.DataId, BuffType.GuildSkillBuff, level);
+        }
+
+        protected virtual void ApplyItemPetSummon(Item item, short level, int exp)
+        {
+            if (IsDead() || !IsServer || item == null || level <= 0)
+                return;
+            // Clear all summoned pets
+            CharacterSummon tempSummon;
+            for (int i = 0; i < Summons.Count; ++i)
+            {
+                tempSummon = summons[i];
+                if (tempSummon.type != SummonType.Pet)
+                    continue;
+                summons.RemoveAt(i);
+                tempSummon.UnSummon(this);
+            }
+            // Summon new pet
+            CharacterSummon newSummon = CharacterSummon.Create(SummonType.Pet, item.DataId);
+            newSummon.Summon(this, level, 0f, exp);
+            summons.Add(newSummon);
+        }
+
+        protected virtual void ApplySkillSummon(Skill skill, short level)
+        {
+            if (IsDead() || !IsServer || skill == null || level <= 0)
+                return;
+            int i = 0;
+            int amountEachTime = skill.summon.amountEachTime.GetAmount(level);
+            for (i = 0; i < amountEachTime; ++i)
+            {
+                CharacterSummon newSummon = CharacterSummon.Create(SummonType.Skill, skill.DataId);
+                newSummon.Summon(this, skill.summon.level.GetAmount(level), skill.summon.duration.GetAmount(level));
+                summons.Add(newSummon);
+            }
+            int count = 0;
+            for (i = 0; i < summons.Count; ++i)
+            {
+                if (summons[i].dataId == skill.DataId)
+                    ++count;
+            }
+            int maxStack = skill.summon.maxStack.GetAmount(level);
+            int unSummonAmount = count > maxStack ? count - maxStack : 0;
+            CharacterSummon tempSummon;
+            for (i = unSummonAmount; i > 0; --i)
+            {
+                int summonIndex = this.IndexOfSummon(skill.DataId, SummonType.Skill);
+                tempSummon = summons[summonIndex];
+                if (summonIndex >= 0)
+                {
+                    summons.RemoveAt(summonIndex);
+                    tempSummon.UnSummon(this);
+                }
+            }
+        }
+
+    }
+}
