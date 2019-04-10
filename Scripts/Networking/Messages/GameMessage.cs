8f1f6d26c41a1bcc36fa894114cf8ac26aba2607
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-04-10 17:08:20 +0700

    WIP socket item enhance system

--- Scripts/Networking/Messages/GameMessage.cs
+++ Scripts/Networking/Messages/GameMessage.cs
@@ -25,6 +25,8 @@ namespace MultiplayerARPG
             RefineItemReachedMaxLevel,
             RefineSuccess,
             RefineFail,
+            // Enhance
+            CannotEnhanceSocket,
             // Repair
             CannotRepair,
             RepairSuccess,
