95585b8687841b33aee5f9af0a833f6634950d8a
insthync <ittipon.bay@gmail.com>
2019-04-12 21:04:07 +0700

    Fix invalid equipment sockets text

--- Scripts/UI/Item/UIEquipmentSockets.cs
+++ Scripts/UI/Item/UIEquipmentSockets.cs
@@ -8,9 +8,9 @@ namespace MultiplayerARPG
         [Header("Equipment Sockets Format")]
         [Tooltip("Filled socket format => {0} = {Item Index}, {1} = {Item Title}, {2} = {Effects}")]
         [Multiline]
-        public string filledSocketFormat = "<color=#800080ff>{0} - {1}</color>\n{2}";
+        public string filledSocketFormat = "<color=#800080ff>({0}) - {1}\n{2}</color>";
         [Tooltip("Empty socket format => {0} = {Item Index}")]
-        public string emptySocketFormat = "<color=#800080ff>{0} - Empty</color>";
+        public string emptySocketFormat = "<color=#800080ff>({0}) - Empty</color>";
 
         protected override void UpdateData()
         {
@@ -19,14 +19,21 @@ namespace MultiplayerARPG
             string tempText;
             for (int i = 0; i < Data.maxSocket; ++i)
             {
-                if (i >= Data.sockets.Count || !GameInstance.Items.TryGetValue(Data.sockets[i], out tempItem))
-                    continue;
-                tempText = GetEquipmentBonusText(tempItem.socketEnhanceEffect);
-                if (!string.IsNullOrEmpty(tempText))
+                if (i < Data.sockets.Count && GameInstance.Items.TryGetValue(Data.sockets[i], out tempItem))
+                {
+                    tempText = GetEquipmentBonusText(tempItem.socketEnhanceEffect);
+                    if (!string.IsNullOrEmpty(tempText))
+                    {
+                        if (!string.IsNullOrEmpty(allBonusText))
+                            allBonusText += "\n";
+                        allBonusText += string.Format(filledSocketFormat, i + 1, tempItem.Title, tempText);
+                    }
+                }
+                else
                 {
                     if (!string.IsNullOrEmpty(allBonusText))
                         allBonusText += "\n";
-                    allBonusText += string.Format(filledSocketFormat, tempItem.Title, tempText);
+                    allBonusText += string.Format(emptySocketFormat, i + 1);
                 }
             }
             if (uiTextAllBonus != null)
