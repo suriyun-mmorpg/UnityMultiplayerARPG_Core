770d6d7d63039a268ac7a506d847a5d3625b3f5e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-05-27 05:32:55 +0700

    Add component for toggle language

---
+++ Scripts/UI/Language/OnToggleChangeLanguage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MultiplayerARPG
+{
+    [RequireComponent(typeof(Toggle))]
+    public class OnToggleChangeLanguage : MonoBehaviour
+    {
+        public string languageKey;
+
+        private void Awake()
+        {
+            Toggle toggle = GetComponent<Toggle>();
+            toggle.onValueChanged.AddListener(OnToggle);
+            toggle.isOn = LanguageManager.CurrentLanguageKey.Equals(languageKey);
+        }
+
+        public void OnToggle(bool selected)
+        {
+            if (selected)
+                LanguageManager.ChangeLanguage(languageKey);
+        }
+    }
+}
