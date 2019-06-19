5435dc3f6635036878d999d807f6225b77d6d796
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-06-19 18:59:48 +0700

    Improve codes

---
+++ Scripts/Gameplay/BaseGameComponent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public abstract class BaseGameComponent<T> : MonoBehaviour
+        where T : BaseGameEntity
+    {
+        private T cacheEntity;
+        public T CacheCharacterEntity
+        {
+            get
+            {
+                if (cacheEntity == null)
+                    cacheEntity = GetComponent<T>();
+                return cacheEntity;
+            }
+        }
+
+        public GameInstance gameInstance { get { return CacheCharacterEntity.gameInstance; } }
+        public BaseGameplayRule gameplayRule { get { return CacheCharacterEntity.gameplayRule; } }
+        public BaseGameNetworkManager gameManager { get { return CacheCharacterEntity.gameManager; } }
+        public Transform CacheTransform { get { return CacheCharacterEntity.CacheTransform; } }
+        public bool IsOwnerClient { get { return CacheCharacterEntity.IsOwnerClient; } }
+        public bool IsServer { get { return CacheCharacterEntity.IsServer; } }
+        public bool IsClient { get { return CacheCharacterEntity.IsClient; } }
+
+        public virtual void EntityOnSetup(T entity) { }
+        public virtual void EntityOnDestroy(T entity) { }
+    }
+}
--- Scripts/Gameplay/CharacterSystems/BaseCharacterComponent.cs
+++ Scripts/Gameplay/CharacterSystems/BaseCharacterComponent.cs
@@ -1,34 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace MultiplayerARPG
+﻿namespace MultiplayerARPG
 {
-    public abstract class BaseCharacterComponent : MonoBehaviour
+    public abstract class BaseCharacterComponent : BaseGameComponent<BaseCharacterEntity>
     {
-        private BaseCharacterEntity cacheCharacterEntity;
-        public BaseCharacterEntity CacheCharacterEntity
-        {
-            get
-            {
-                if (cacheCharacterEntity == null)
-                    cacheCharacterEntity = GetComponent<BaseCharacterEntity>();
-                return cacheCharacterEntity;
-            }
-        }
-
-        public GameInstance gameInstance { get { return CacheCharacterEntity.gameInstance; } }
-        public BaseGameplayRule gameplayRule { get { return CacheCharacterEntity.gameplayRule; } }
-        public BaseGameNetworkManager gameManager { get { return CacheCharacterEntity.gameManager; } }
-        public Transform CacheTransform { get { return CacheCharacterEntity.CacheTransform; } }
-        public bool IsOwnerClient { get { return CacheCharacterEntity.IsOwnerClient; } }
-        public bool IsServer { get { return CacheCharacterEntity.IsServer; } }
-        public bool IsClient { get { return CacheCharacterEntity.IsClient; } }
-
-        public virtual void EntityOnSetup(BaseCharacterEntity entity) { }
-
-        public virtual void EntityOnDestroy(BaseCharacterEntity entity) { }
-
         public bool IsDead()
         {
             return CacheCharacterEntity.IsDead();
