4bef4ebae93c1947816b23ef6410ef32314930de
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-06-20 17:20:07 +0700

    Add condition to show fields

--- Editor/Model/CharacterModelEditor.cs
+++ Editor/Model/CharacterModelEditor.cs
@@ -8,5 +8,16 @@ namespace MultiplayerARPG
     [CanEditMultipleObjects]
     public class CharacterModelEditor : BaseCharacterModelEditor
     {
+        protected override void SetFieldCondition()
+        {
+            base.SetFieldCondition();
+            ShowOnEnum("animatorType", "Animator", "animator");
+            ShowOnEnum("animatorType", "Animator", "animatorController");
+            ShowOnEnum("animatorType", "Animator", "defaultAnimatorData");
+            ShowOnEnum("animatorType", "Animator", "actionStateLayer");
+            ShowOnEnum("animatorType", "Animator", "castSkillStateLayer");
+            ShowOnEnum("animatorType", "LegacyAnimtion", "legacyAnimation");
+            ShowOnEnum("animatorType", "LegacyAnimtion", "legacyAnimationData");
+        }
     }
 }
