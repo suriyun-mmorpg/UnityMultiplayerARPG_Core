376498ab17e3ded40329469dc48bb5e10e2e5e10
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-06-20 13:33:08 +0700

    Add interfaces for model

---
+++ Scripts/GameData/Model/Interfaces/IMoveableModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MultiplayerARPG
+{
+    public interface IMoveableModel
+    {
+        void UpdateMovementAnimation(MovementState movementState, float playMoveSpeedMultiplier = 1f);
+    }
+}
