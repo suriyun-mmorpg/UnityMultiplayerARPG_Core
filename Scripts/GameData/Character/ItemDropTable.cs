433f0849a064d70b80dca528ecd5802e96aa76d3
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-07-20 08:46:47 +0700

    Add create asset menu for item drop table

--- Scripts/GameData/Character/ItemDropTable.cs
+++ Scripts/GameData/Character/ItemDropTable.cs
@@ -2,6 +2,7 @@
 
 namespace MultiplayerARPG
 {
+    [CreateAssetMenu(fileName = "Item Drop Table", menuName = "Create GameData/Item Drop Table", order = -4993)]
     public class ItemDropTable : ScriptableObject
     {
         public ItemDrop[] randomItems;
