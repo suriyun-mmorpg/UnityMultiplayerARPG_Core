fc1461b309894fe739e8bbccc3260d675df27573
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-08-12 03:36:11 +0700

    WIP faction system

---
+++ Scripts/GameData/Other/Faction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    [CreateAssetMenu(fileName = "Faction", menuName = "Create GameData/Faction", order = -4795)]
+    public partial class Faction : BaseGameData
+    {
+    }
+}
