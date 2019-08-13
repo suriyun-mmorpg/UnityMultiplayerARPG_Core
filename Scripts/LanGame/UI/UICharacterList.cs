b6207ce4de0ff8a1bef2e1d62134f923e51df03c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-08-13 20:25:52 +0700

    Remove invalid entity id or invalid data id character from selectable list

--- Scripts/LanGame/UI/UICharacterList.cs
+++ Scripts/LanGame/UI/UICharacterList.cs
@@ -70,8 +70,13 @@ namespace MultiplayerARPG
             for (int i = selectableCharacters.Count - 1; i >= 0; --i)
             {
                 PlayerCharacterData selectableCharacter = selectableCharacters[i];
-                if (selectableCharacter == null || !GameInstance.PlayerCharacters.ContainsKey(selectableCharacter.DataId))
+                if (selectableCharacter == null ||
+                    !GameInstance.PlayerCharacterEntities.ContainsKey(selectableCharacter.EntityId) ||
+                    !GameInstance.PlayerCharacters.ContainsKey(selectableCharacter.DataId))
+                {
+                    // If invalid entity id or data id, remove from selectable character list
                     selectableCharacters.RemoveAt(i);
+                }
             }
 
             if (selectableCharacters.Count > 0)
