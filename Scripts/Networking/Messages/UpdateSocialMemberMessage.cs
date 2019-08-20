5a31c9ecbf35e9214fd22887404215aef1624e8c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-08-20 08:45:10 +0700

    Add clear method to update social member message

--- Scripts/Networking/Messages/UpdateSocialMemberMessage.cs
+++ Scripts/Networking/Messages/UpdateSocialMemberMessage.cs
@@ -9,6 +9,7 @@ namespace MultiplayerARPG
             Add,
             Update,
             Remove,
+            Clear,
         }
         public UpdateType type;
         public int id;
@@ -19,24 +20,28 @@ namespace MultiplayerARPG
         {
             type = (UpdateType)reader.GetByte();
             id = reader.GetInt();
-            data.id = reader.GetString();
-            switch (type)
+            if (type != UpdateType.Clear)
             {
-                case UpdateType.Add:
-                case UpdateType.Update:
-                    isOnline = reader.GetBool();
-                    data.characterName = reader.GetString();
-                    data.dataId = reader.GetInt();
-                    data.level = reader.GetShort();
-                    // Read extra data
-                    if (isOnline)
-                    {
-                        data.currentHp = reader.GetInt();
-                        data.maxHp = reader.GetInt();
-                        data.currentMp = reader.GetInt();
-                        data.maxMp = reader.GetInt();
-                    }
-                    break;
+                // Get social member data
+                data.id = reader.GetString();
+                switch (type)
+                {
+                    case UpdateType.Add:
+                    case UpdateType.Update:
+                        isOnline = reader.GetBool();
+                        data.characterName = reader.GetString();
+                        data.dataId = reader.GetInt();
+                        data.level = reader.GetShort();
+                        // Read extra data
+                        if (isOnline)
+                        {
+                            data.currentHp = reader.GetInt();
+                            data.maxHp = reader.GetInt();
+                            data.currentMp = reader.GetInt();
+                            data.maxMp = reader.GetInt();
+                        }
+                        break;
+                }
             }
         }
 
@@ -44,24 +49,28 @@ namespace MultiplayerARPG
         {
             writer.Put((byte)type);
             writer.Put(id);
-            writer.Put(data.id);
-            switch (type)
+            if (type != UpdateType.Clear)
             {
-                case UpdateType.Add:
-                case UpdateType.Update:
-                    writer.Put(isOnline);
-                    writer.Put(data.characterName);
-                    writer.Put(data.dataId);
-                    writer.Put(data.level);
-                    // Put extra data
-                    if (isOnline)
-                    {
-                        writer.Put(data.currentHp);
-                        writer.Put(data.maxHp);
-                        writer.Put(data.currentMp);
-                        writer.Put(data.maxMp);
-                    }
-                    break;
+                // Put social member data
+                writer.Put(data.id);
+                switch (type)
+                {
+                    case UpdateType.Add:
+                    case UpdateType.Update:
+                        writer.Put(isOnline);
+                        writer.Put(data.characterName);
+                        writer.Put(data.dataId);
+                        writer.Put(data.level);
+                        // Put extra data
+                        if (isOnline)
+                        {
+                            writer.Put(data.currentHp);
+                            writer.Put(data.maxHp);
+                            writer.Put(data.currentMp);
+                            writer.Put(data.maxMp);
+                        }
+                        break;
+                }
             }
         }
 
