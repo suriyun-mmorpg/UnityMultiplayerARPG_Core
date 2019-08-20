647f1f29659bcdf4e28925112ddb79995092cb94
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-08-20 09:20:19 +0700

    Add update social members message

---
+++ Scripts/Networking/Messages/UpdateSocialMembersMessage.cs
@@ -0,0 +1,41 @@
+﻿using LiteNetLib.Utils;
+
+namespace MultiplayerARPG
+{
+    public struct UpdateSocialMembersMessage : INetSerializable
+    {
+        public int id;
+        public SocialCharacterData[] members;
+
+        public void Deserialize(NetDataReader reader)
+        {
+            id = reader.GetInt();
+            members = new SocialCharacterData[reader.GetPackedUInt()];
+            SocialCharacterData tempEntry;
+            for (int i = 0; i < members.Length; ++i)
+            {
+                tempEntry = new SocialCharacterData();
+                tempEntry.id = reader.GetString();
+                tempEntry.characterName = reader.GetString();
+                tempEntry.dataId = reader.GetInt();
+                tempEntry.level = reader.GetShort();
+                members[i] = tempEntry;
+            }
+        }
+
+        public void Serialize(NetDataWriter writer)
+        {
+            writer.Put(id);
+            writer.PutPackedUInt((uint)members.Length);
+            SocialCharacterData tempEntry;
+            for (int i = 0; i < members.Length; ++i)
+            {
+                tempEntry = members[i];
+                writer.Put(tempEntry.id);
+                writer.Put(tempEntry.characterName);
+                writer.Put(tempEntry.dataId);
+                writer.Put(tempEntry.level);
+            }
+        }
+    }
+}
