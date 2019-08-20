10ae638dc0a21ea54fb5bf07368bd34a052bb4fa
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-08-20 12:11:14 +0700

    Add button activator classes

---
+++ Scripts/UI/Utils/ButtonActivators/ActivateButtonActivator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public class ActivateButtonActivator : MonoBehaviour
+    {
+        public GameObject[] activateObjects;
+
+        private bool canActivate;
+        private PlayerCharacterController controller;
+        private ShooterPlayerCharacterController shooterController;
+
+        private void LateUpdate()
+        {
+            canActivate = false;
+
+            controller = BasePlayerCharacterController.Singleton as PlayerCharacterController;
+            shooterController = BasePlayerCharacterController.Singleton as ShooterPlayerCharacterController;
+
+            if (controller != null)
+            {
+                canActivate = controller.activatableEntityDetector.players.Count > 0 ||
+                    controller.activatableEntityDetector.npcs.Count > 0 ||
+                    controller.activatableEntityDetector.buildings.Count > 0;
+            }
+
+            if (shooterController != null && shooterController.SelectedEntity != null)
+            {
+                canActivate = shooterController.SelectedEntity is BasePlayerCharacterEntity ||
+                    shooterController.SelectedEntity is NpcEntity ||
+                    shooterController.SelectedEntity is BuildingEntity;
+            }
+
+            foreach (GameObject obj in activateObjects)
+            {
+                obj.SetActive(canActivate);
+            }
+        }
+    }
+}
---
+++ Scripts/UI/Utils/ButtonActivators/ZoomButtonActivator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public class ZoomButtonActivator : MonoBehaviour
+    {
+        public GameObject[] activateObjects;
+
+        private ShooterPlayerCharacterController controller;
+        private bool canZoom;
+
+        private void LateUpdate()
+        {
+            if (BasePlayerCharacterController.Singleton != null && controller == null)
+                controller = BasePlayerCharacterController.Singleton as ShooterPlayerCharacterController;
+            canZoom = controller != null && controller.weaponAbility != null &&
+                controller.weaponAbility is ZoomWeaponAbility;
+
+            foreach (GameObject obj in activateObjects)
+            {
+                obj.SetActive(canZoom);
+            }
+        }
+    }
+}
