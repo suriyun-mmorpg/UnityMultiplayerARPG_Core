3e0a951ccd3bf3459f551f3f71811051a1638fd1
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-08-20 12:10:46 +0700

    Add friend UIs

---
+++ Scripts/UI/Social/Etc/UIFindCharacters.cs
@@ -0,0 +1,101 @@
+﻿namespace MultiplayerARPG
+{
+    public class UIFindCharacters : UISocialGroup<UISocialCharacter>
+    {
+        public InputFieldWrapper inputCharacterName;
+
+        private void OnEnable()
+        {
+            BaseGameNetworkManager.Singleton.onClientUpdateFoundCharacters += UpdateFoundCharactersUIs;
+        }
+
+        private void OnDisable()
+        {
+            BaseGameNetworkManager.Singleton.onClientUpdateFoundCharacters -= UpdateFoundCharactersUIs;
+        }
+
+        private void UpdateFoundCharactersUIs(SocialGroupData group)
+        {
+            if (group == null)
+                return;
+
+            memberAmount = group.CountMember();
+            UpdateUIs();
+
+            int selectedIdx = MemberSelectionManager.SelectedUI != null ? MemberSelectionManager.IndexOf(MemberSelectionManager.SelectedUI) : -1;
+            MemberSelectionManager.DeselectSelectedUI();
+            MemberSelectionManager.Clear();
+
+            SocialCharacterData[] members = group.GetMembers();
+            MemberList.Generate(members, (index, foundCharacter, ui) =>
+            {
+                SocialCharacterEntityTuple foundCharacterEntity = new SocialCharacterEntityTuple();
+                foundCharacterEntity.socialCharacter = foundCharacter;
+
+                UISocialCharacter uiFoundCharacter = ui.GetComponent<UISocialCharacter>();
+                uiFoundCharacter.uiSocialGroup = this;
+                uiFoundCharacter.Data = foundCharacterEntity;
+                uiFoundCharacter.Show();
+                MemberSelectionManager.Add(uiFoundCharacter);
+                if (selectedIdx == index)
+                    uiFoundCharacter.OnClickSelect();
+            });
+        }
+
+        public override bool CanKick(string characterId)
+        {
+            return false;
+        }
+
+        public override int GetMaxMemberAmount()
+        {
+            // TODO: Implement this
+            return 0;
+        }
+
+        public override int GetSocialId()
+        {
+            return 0;
+        }
+
+        public override bool IsLeader(string characterId)
+        {
+            return false;
+        }
+
+        public override bool IsOnline(string characterId)
+        {
+            return false;
+        }
+
+        public override bool OwningCharacterCanKick()
+        {
+            return false;
+        }
+
+        public override bool OwningCharacterIsLeader()
+        {
+            return false;
+        }
+
+        public void OnClickFindCharacters()
+        {
+            string characterName = string.Empty;
+            if (inputCharacterName != null)
+                characterName = inputCharacterName.text;
+            BasePlayerCharacterController.OwningCharacter.RequestFindCharacters(characterName);
+        }
+
+        public void OnClickAddFriend()
+        {
+            if (MemberSelectionManager.SelectedUI == null)
+                return;
+
+            SocialCharacterData friend = MemberSelectionManager.SelectedUI.Data.socialCharacter;
+            UISceneGlobal.Singleton.ShowMessageDialog(LanguageManager.GetText(UILocaleKeys.UI_FRIEND_REMOVE.ToString()), string.Format(LanguageManager.GetText(UILocaleKeys.UI_FRIEND_REMOVE_DESCRIPTION.ToString()), friend.characterName), false, true, true, false, null, () =>
+            {
+                BasePlayerCharacterController.OwningCharacter.RequestAddFriend(friend.id);
+            });
+        }
+    }
+}
---
+++ Scripts/UI/Social/Friend/UIFriend.cs
@@ -0,0 +1,92 @@
+﻿namespace MultiplayerARPG
+{
+    public class UIFriend : UISocialGroup<UISocialCharacter>
+    {
+        private void OnEnable()
+        {
+            BaseGameNetworkManager.Singleton.onClientUpdateFriends += UpdateFriendsUIs;
+            BasePlayerCharacterController.OwningCharacter.RequestGetFriends();
+        }
+
+        private void OnDisable()
+        {
+            BaseGameNetworkManager.Singleton.onClientUpdateFriends -= UpdateFriendsUIs;
+        }
+
+        private void UpdateFriendsUIs(SocialGroupData group)
+        {
+            if (group == null)
+                return;
+
+            memberAmount = group.CountMember();
+            UpdateUIs();
+
+            int selectedIdx = MemberSelectionManager.SelectedUI != null ? MemberSelectionManager.IndexOf(MemberSelectionManager.SelectedUI) : -1;
+            MemberSelectionManager.DeselectSelectedUI();
+            MemberSelectionManager.Clear();
+
+            SocialCharacterData[] members = group.GetMembers();
+            MemberList.Generate(members, (index, friend, ui) =>
+            {
+                SocialCharacterEntityTuple friendEntity = new SocialCharacterEntityTuple();
+                friendEntity.socialCharacter = friend;
+
+                UISocialCharacter uiFriend = ui.GetComponent<UISocialCharacter>();
+                uiFriend.uiSocialGroup = this;
+                uiFriend.Data = friendEntity;
+                uiFriend.Show();
+                MemberSelectionManager.Add(uiFriend);
+                if (selectedIdx == index)
+                    uiFriend.OnClickSelect();
+            });
+        }
+
+        public override bool CanKick(string characterId)
+        {
+            return false;
+        }
+
+        public override int GetMaxMemberAmount()
+        {
+            // TODO: Implement this
+            return 0;
+        }
+
+        public override int GetSocialId()
+        {
+            return 0;
+        }
+
+        public override bool IsLeader(string characterId)
+        {
+            return false;
+        }
+
+        public override bool IsOnline(string characterId)
+        {
+            return false;
+        }
+
+        public override bool OwningCharacterCanKick()
+        {
+            return false;
+        }
+
+        public override bool OwningCharacterIsLeader()
+        {
+            return false;
+        }
+
+        public void OnClickRemoveFriend()
+        {
+            if (MemberSelectionManager.SelectedUI == null)
+                return;
+
+            SocialCharacterData friend = MemberSelectionManager.SelectedUI.Data.socialCharacter;
+            UISceneGlobal.Singleton.ShowMessageDialog(LanguageManager.GetText(UILocaleKeys.UI_FRIEND_REMOVE.ToString()), string.Format(LanguageManager.GetText(UILocaleKeys.UI_FRIEND_REMOVE_DESCRIPTION.ToString()), friend.characterName), false, true, true, false, null, () =>
+            {
+                BasePlayerCharacterController.OwningCharacter.RequestRemoveFriend(friend.id);
+            });
+        }
+    }
+}
