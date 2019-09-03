7ab2c0f81ffbfdf26c8593d2d697825f37ea64b0
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-09-03 16:11:12 +0700

    Add simple ui component to manage equip weapon set

---
+++ Scripts/UI/Item/UIEquipWeaponsManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public class UIEquipWeaponsManager : MonoBehaviour
+    {
+        [Tooltip("Index of this array is equip weapons set")]
+        public ActivatingGameObjects[] activatingGameObjects;
+        
+        private void Update()
+        {
+            for (byte i = 0; i < activatingGameObjects.Length; ++i)
+            {
+                for (byte j = 0; j < activatingGameObjects[i].gameObjects.Length; ++j)
+                {
+                    activatingGameObjects[i].gameObjects[j].SetActive(BasePlayerCharacterController.OwningCharacter.EquipWeaponSet == i);
+                }
+            }
+        }
+
+        public void OnClickSwitchEquipWeaponSet(byte equipWeaponSet)
+        {
+            BasePlayerCharacterController.OwningCharacter.RequestSwitchEquipWeaponSet(equipWeaponSet);
+        }
+
+        [System.Serializable]
+        public struct ActivatingGameObjects
+        {
+            public GameObject[] gameObjects;
+        }
+    }
+}
