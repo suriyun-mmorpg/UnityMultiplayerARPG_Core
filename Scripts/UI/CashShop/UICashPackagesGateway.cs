afc1adfffbe0984d4fb194ef571754da9623013a
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-10-21 05:01:06 +0700

    Add option to always show ui

--- Scripts/UI/CashShop/UICashPackagesGateway.cs
+++ Scripts/UI/CashShop/UICashPackagesGateway.cs
@@ -4,12 +4,13 @@ namespace MultiplayerARPG
 {
     public class UICashPackagesGateway : MonoBehaviour
     {
+        public bool alwaysShowPackagesUI;
         public string nonMobileUrl = "http://localhost/";
         public UICashPackages uiCashPackages;
 
         public void OnClickShowCashPackages()
         {
-            if (Application.isMobilePlatform)
+            if (Application.isMobilePlatform || alwaysShowPackagesUI)
             {
                 if (uiCashPackages != null)
                     uiCashPackages.Show();
