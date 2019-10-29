87e2321b6279986464eaa056378b4e18efc2d627
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-10-30 02:02:30 +0700

    Add npc dialog graph

---
+++ Scripts/GameData/Npc/NpcDialogGraph.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MultiplayerARPG
+{
+    [CreateAssetMenu(fileName = "Npc Dialog Graph", menuName = "Create GameData/Npc Dialog Graph", order = -4797)]
+    public class NpcDialogGraph : NodeGraph
+    {
+    }
+}
