87214a267e1787d65a0c5e6563fa909b38acd6d6
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-10-30 14:46:08 +0700

    Set dialog name when get nodes

--- Scripts/GameData/Npc/NpcDialogGraph.cs
+++ Scripts/GameData/Npc/NpcDialogGraph.cs
@@ -16,92 +16,13 @@ namespace MultiplayerARPG
             List<NpcDialog> dialogs = new List<NpcDialog>();
             if (nodes != null && nodes.Count > 0)
             {
-                foreach (Node node in nodes)
-                {
-                    dialogs.Add(node as NpcDialog);
-                }
-            }
-            return dialogs;
-        }
-
-        private void OnValidate()
-        {
-            SetDialogName();
-        }
-
-        public void SetDialogName()
-        {
-            if (nodes != null && nodes.Count > 0)
-            {
                 for (int i = 0; i < nodes.Count; ++i)
                 {
                     nodes[i].name = name + " " + i;
-#if UNITY_EDITOR
-                    EditorUtility.SetDirty(nodes[i]);
-#endif
+                    dialogs.Add(nodes[i] as NpcDialog);
                 }
             }
-            EditorUtility.SetDirty(this);
-        }
-
-        public override Node AddNode(System.Type type)
-        {
-            if (type == typeof(NpcDialog))
-            {
-                NpcDialog npcDialog = AddNode<NpcDialog>();
-                SetDialogName();
-            }
-            return base.AddNode(type);
-        }
-
-        public override Node CopyNode(Node original)
-        {
-            if (original is NpcDialog)
-            {
-                NpcDialog npcDialog = AddNode<NpcDialog>();
-                CopyNode(original as NpcDialog, npcDialog);
-                SetDialogName();
-            }
-            return base.CopyNode(original);
-        }
-
-        public NpcDialog CopyNode(NpcDialog from, NpcDialog to)
-        {
-            to.title = from.title;
-            to.titles = from.titles;
-            to.description = from.description;
-            to.descriptions = from.descriptions;
-            to.icon = from.icon;
-            to.type = from.type;
-            to.menus = new List<NpcDialogMenu>(from.menus).ToArray();
-            to.quest = from.quest;
-            to.questAcceptedDialog = from.questAcceptedDialog;
-            to.questDeclinedDialog = from.questDeclinedDialog;
-            to.questAbandonedDialog = from.questAbandonedDialog;
-            to.questCompletedDialog = from.questCompletedDialog;
-            to.sellItems = new List<NpcSellItem>(from.sellItems).ToArray();
-            to.itemCraft = from.itemCraft;
-            to.craftDoneDialog = from.craftDoneDialog;
-            to.craftItemWillOverwhelmingDialog = from.craftItemWillOverwhelmingDialog;
-            to.craftNotMeetRequirementsDialog = from.craftNotMeetRequirementsDialog;
-            to.craftCancelDialog = from.craftCancelDialog;
-            to.saveRespawnMap = from.saveRespawnMap;
-            to.saveRespawnPosition = from.saveRespawnPosition;
-            to.saveRespawnConfirmDialog = from.saveRespawnConfirmDialog;
-            to.saveRespawnCancelDialog = from.saveRespawnCancelDialog;
-            to.warpPortalType = from.warpPortalType;
-            to.warpMap = from.warpMap;
-            to.warpPosition = from.warpPosition;
-            to.warpCancelDialog = from.warpCancelDialog;
-            to.storageCancelDialog = from.storageCancelDialog;
-            return to;
-        }
-
-        public override NodeGraph Copy()
-        {
-            NpcDialogGraph graph = base.Copy() as NpcDialogGraph;
-            graph.SetDialogName();
-            return graph;
+            return dialogs;
         }
     }
 }
