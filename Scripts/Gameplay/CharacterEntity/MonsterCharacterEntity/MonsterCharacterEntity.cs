6c567094733f8bb806301c27fb1826352f440528
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-12-25 06:08:44 +0700

    Just warning developers to setup entity movement component by themself because an entity movement components may require other components too.

--- Scripts/Gameplay/CharacterEntity/MonsterCharacterEntity/MonsterCharacterEntity.cs
+++ Scripts/Gameplay/CharacterEntity/MonsterCharacterEntity/MonsterCharacterEntity.cs
@@ -1,16 +1,13 @@
-﻿namespace MultiplayerARPG
+﻿using UnityEngine;
+
+namespace MultiplayerARPG
 {
     public partial class MonsterCharacterEntity : BaseMonsterCharacterEntity
     {
         public override void InitialRequiredComponents()
         {
             if (Movement == null)
-            {
-                if (CurrentGameInstance.DimensionType == DimensionType.Dimension3D)
-                    Movement = gameObject.AddComponent<NavMeshEntityMovement>();
-                else
-                    Movement = gameObject.AddComponent<RigidBodyEntityMovement2D>();
-            }
+                Debug.LogError("[" + ToString() + "] Did not setup entity movement component to this entity.");
         }
     }
 }
--- Scripts/Gameplay/CharacterEntity/PlayerCharacterEntity/PlayerCharacterEntity.cs
+++ Scripts/Gameplay/CharacterEntity/PlayerCharacterEntity/PlayerCharacterEntity.cs
@@ -1,16 +1,13 @@
-﻿namespace MultiplayerARPG
+﻿using UnityEngine;
+
+namespace MultiplayerARPG
 {
     public partial class PlayerCharacterEntity : BasePlayerCharacterEntity
     {
         public override void InitialRequiredComponents()
         {
             if (Movement == null)
-            {
-                if (CurrentGameInstance.DimensionType == DimensionType.Dimension3D)
-                    Movement = gameObject.AddComponent<RigidBodyEntityMovement>();
-                else
-                    Movement = gameObject.AddComponent<RigidBodyEntityMovement2D>();
-            }
+                Debug.LogError("[" + ToString() + "] Did not setup entity movement component to this entity.");
         }
     }
 }
