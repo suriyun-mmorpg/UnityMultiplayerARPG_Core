ff3bc74086b617af2bf2e47997e69f6818056271
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-12-27 00:05:51 +0700

    Add movement collider adjustment component

---
+++ Scripts/Gameplay/CharacterSystems/MovementColliderAdjustment.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MultiplayerARPG
+{
+    public class MovementColliderAdjustment : BaseGameEntityComponent<BaseGameEntity>
+    {
+        [System.Serializable]
+        public struct Settings
+        {
+            public Vector3 center;
+            public float radius;
+            public float height;
+        }
+        
+        [SerializeField]
+        private Settings standSettings;
+        [SerializeField]
+        private Settings crouchSettings;
+        [SerializeField]
+        private Settings crawlSettings;
+        [SerializeField]
+        private Settings swimSettings;
+
+        private CapsuleCollider capsuleCollider;
+
+        public override void EntityAwake()
+        {
+            if (capsuleCollider == null)
+                capsuleCollider = GetComponent<CapsuleCollider>();
+        }
+
+        public override void EntityLateUpdate()
+        {
+            if (capsuleCollider == null)
+                return;
+
+            if (CacheEntity.IsUnderWater)
+            {
+                Apply(swimSettings);
+            }
+            else
+            {
+                switch (CacheEntity.ExtraMovementState)
+                {
+                    case ExtraMovementState.IsCrouching:
+                        Apply(crouchSettings);
+                        break;
+                    case ExtraMovementState.IsCrawling:
+                        Apply(crawlSettings);
+                        break;
+                    default:
+                        Apply(standSettings);
+                        break;
+                }
+            }
+        }
+
+        private void Apply(Settings settings)
+        {
+            capsuleCollider.center = settings.center;
+            capsuleCollider.radius = settings.radius;
+            capsuleCollider.height = settings.height;
+        }
+    }
+}
