706f68ab4f8153b732d386c9830a77d77d0beab7
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-01-06 02:36:51 +0700

    Add empty seat data

--- Scripts/Gameplay/VehicleSystems/VehicleSeat.cs
+++ Scripts/Gameplay/VehicleSystems/VehicleSeat.cs
@@ -5,6 +5,7 @@ namespace MultiplayerARPG
     [System.Serializable]
     public struct VehicleSeat
     {
+        public static readonly VehicleSeat Empty = new VehicleSeat();
         public Transform passengingTransform;
         public Transform exitTransform;
         public bool canAttack;
