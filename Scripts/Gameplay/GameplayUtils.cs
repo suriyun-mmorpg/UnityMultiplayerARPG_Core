44eb28d6e762f50603eebfe5632d278a828e6a2e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-01-17 16:44:35 +0700

    Fix unused codes

--- Scripts/Gameplay/GameplayUtils.cs
+++ Scripts/Gameplay/GameplayUtils.cs
@@ -8,39 +8,38 @@ namespace MultiplayerARPG
     {
         public static DirectionType2D GetDirectionTypeByVector2(Vector2 direction)
         {
-            Vector2 normalized = direction.normalized;
-            float absX = Mathf.Abs(normalized.x);
-            float absY = Mathf.Abs(normalized.y);
+            float absX = Mathf.Abs(direction.x);
+            float absY = Mathf.Abs(direction.y);
             if (absX / absY > 0.8f)
             {
-                if (normalized.x < 0) return DirectionType2D.Left;
-                if (normalized.x > 0) return DirectionType2D.Right;
+                if (direction.x < 0) return DirectionType2D.Left;
+                if (direction.x > 0) return DirectionType2D.Right;
             }
             else if (absY / absX > 0.8f)
             {
-                if (normalized.y < 0) return DirectionType2D.Down;
-                if (normalized.y > 0) return DirectionType2D.Up;
+                if (direction.y < 0) return DirectionType2D.Down;
+                if (direction.y > 0) return DirectionType2D.Up;
             }
             else
             {
                 DirectionType2D result = DirectionType2D.Down;
-                if (normalized.x > 0.01f)
+                if (direction.x > 0.01f)
                 {
                     result = DirectionType2D.Left;
-                    if (normalized.y > 0.01f)
+                    if (direction.y > 0.01f)
                         result |= DirectionType2D.Up;
-                    if (normalized.y < -0.01f)
+                    if (direction.y < -0.01f)
                         result |= DirectionType2D.Down;
                 }
-                else if (normalized.x < -0.01f)
+                else if (direction.x < -0.01f)
                 {
                     result = DirectionType2D.Right;
-                    if (normalized.y > 0.01f)
+                    if (direction.y > 0.01f)
                         result |= DirectionType2D.Up;
-                    if (normalized.y < -0.01f)
+                    if (direction.y < -0.01f)
                         result |= DirectionType2D.Down;
                 }
-                else if (normalized.y > 0.01f)
+                else if (direction.y > 0.01f)
                 {
                     result = DirectionType2D.Up;
                 }
