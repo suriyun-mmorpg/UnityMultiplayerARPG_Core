898aa3df93cb2810bb8bab892e584fc32eb72348
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-02-09 01:11:45 +0700

    Add camp fire entity

--- Scripts/Gameplay/BuildingSystems/CampFireEntity.cs
+++ Scripts/Gameplay/BuildingSystems/CampFireEntity.cs
@@ -10,12 +10,58 @@ namespace MultiplayerARPG
     public class CampFireEntity : StorageEntity
     {
         [Header("Campfire data")]
+        public ConvertItem[] convertItems;
         public UnityEvent onInitialTurnOn;
         public UnityEvent onInitialTurnOff;
         public UnityEvent onTurnOn;
         public UnityEvent onTurnOff;
         [SerializeField]
         protected SyncFieldBool isTurnOn = new SyncFieldBool();
+
+        private float tempDeltaTime;
+        protected readonly Dictionary<int, float> convertElapsed = new Dictionary<int, float>();
+
+        protected Dictionary<int, ConvertItem> cacheFuelItems = new Dictionary<int, ConvertItem>();
+        public Dictionary<int, ConvertItem> CacheFuelItems
+        {
+            get
+            {
+                if (cacheFuelItems == null)
+                {
+                    cacheFuelItems = new Dictionary<int, ConvertItem>();
+                    if (convertItems != null && convertItems.Length > 0)
+                    {
+                        foreach (ConvertItem convertItem in convertItems)
+                        {
+                            if (convertItem.item.item == null || !convertItem.isFuel) continue;
+                            cacheFuelItems[convertItem.item.item.DataId] = convertItem;
+                        }
+                    }
+                }
+                return cacheFuelItems;
+            }
+        }
+
+        protected Dictionary<int, ConvertItem> cacheConvertItems = new Dictionary<int, ConvertItem>();
+        public Dictionary<int, ConvertItem> CacheConvertItems
+        {
+            get
+            {
+                if (cacheConvertItems == null)
+                {
+                    cacheConvertItems = new Dictionary<int, ConvertItem>();
+                    if (convertItems != null && convertItems.Length > 0)
+                    {
+                        foreach (ConvertItem convertItem in convertItems)
+                        {
+                            if (convertItem.item.item == null) continue;
+                            cacheConvertItems[convertItem.item.item.DataId] = convertItem;
+                        }
+                    }
+                }
+                return cacheConvertItems;
+            }
+        }
 
         public override void OnSetup()
         {
@@ -60,7 +106,62 @@ namespace MultiplayerARPG
             base.EntityUpdate();
             if (!IsServer)
                 return;
-            // Consume fuel
+
+            if (!isTurnOn.Value)
+            {
+                if (convertElapsed.Count > 0)
+                    convertElapsed.Clear();
+                return;
+            }
+
+            if (!CanTurnOn())
+                isTurnOn.Value = false;
+
+            // Consume fuel and convert item
+            tempDeltaTime = Time.unscaledDeltaTime;
+
+            ConvertItem convertData;
+            List<CharacterItem> items = new List<CharacterItem>(CurrentGameManager.GetStorageEntityItems(this));
+            foreach (CharacterItem item in items)
+            {
+                if (!CacheConvertItems.ContainsKey(item.dataId))
+                    continue;
+
+                convertData = CacheConvertItems[item.dataId];
+                if (convertData.item.amount > item.amount)
+                {
+                    if (convertElapsed.ContainsKey(item.dataId))
+                        convertElapsed.Remove(item.dataId);
+                    continue;
+                }
+
+                if (!convertElapsed.ContainsKey(item.dataId))
+                    convertElapsed.Add(item.dataId, convertData.convertInterval);
+
+                convertElapsed[item.dataId] -= tempDeltaTime;
+
+                if (convertElapsed[item.dataId] <= 0f)
+                {
+                    convertElapsed[item.dataId] = convertData.convertInterval;
+                    ConvertItem(convertData);
+                }
+            }
+        }
+
+        protected void ConvertItem(ConvertItem convertData)
+        {
+            StorageId storageId = new StorageId(StorageType.Building, Id);
+            ItemAmount tempItemAmount = convertData.item;
+            CurrentGameManager.DecreaseStorageItems(storageId, tempItemAmount.item.DataId, tempItemAmount.amount, null);
+            if (convertData.convertedItem.item != null)
+            {
+                tempItemAmount = convertData.convertedItem;
+                CharacterItem convertedItem = CharacterItem.Create(tempItemAmount.item.DataId, 1, tempItemAmount.amount);
+                CurrentGameManager.IncreaseStorageItems(storageId, convertedItem, (success) =>
+                {
+                    ItemDropEntity.DropItem(this, convertedItem, new uint[0]);
+                });
+            }
         }
 
         protected void NetFuncTurnOn()
@@ -81,6 +182,37 @@ namespace MultiplayerARPG
         public void RequestTurnOff()
         {
             CallNetFunction(NetFuncTurnOff, FunctionReceivers.Server);
+        }
+
+        public bool CanTurnOn()
+        {
+            if (CacheFuelItems.Count == 0)
+            {
+                // Not require fuel
+                return true;
+            }
+            List<CharacterItem> items = CurrentGameManager.GetStorageEntityItems(this);
+            foreach (CharacterItem item in items)
+            {
+                if (CacheFuelItems.ContainsKey(item.dataId) &&
+                    CacheFuelItems[item.dataId].item.amount <= item.amount)
+                    return true;
+            }
+            return true;
+        }
+
+        public override void PrepareRelatesData()
+        {
+            List<Item> items = new List<Item>();
+            if (convertItems != null && convertItems.Length > 0)
+            {
+                foreach (ConvertItem convertItem in convertItems)
+                {
+                    items.Add(convertItem.item.item);
+                    items.Add(convertItem.convertedItem.item);
+                }
+            }
+            GameInstance.AddItems(items);
         }
     }
 }
