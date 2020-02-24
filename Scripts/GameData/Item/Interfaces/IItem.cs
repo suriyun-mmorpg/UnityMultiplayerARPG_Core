d90b14e8c9d7fc955487798f5254907492eaf54b
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-02-24 19:02:35 +0700

    Remove unused functions

--- Scripts/GameData/Item/Interfaces/IItem.cs
+++ Scripts/GameData/Item/Interfaces/IItem.cs
@@ -15,19 +15,5 @@ namespace MultiplayerARPG
         float LockDuration { get; }
         int DismantleReturnGold { get; }
         ItemAmount[] DismantleReturnItems { get; }
-
-        bool IsEquipment();
-        bool IsUsable();
-        bool IsDefendEquipment();
-        bool IsArmor();
-        bool IsShield();
-        bool IsWeapon();
-        bool IsPotion();
-        bool IsAmmo();
-        bool IsBuilding();
-        bool IsPet();
-        bool IsSocketEnhancer();
-        bool IsMount();
-        bool IsSkill();
     }
 }
