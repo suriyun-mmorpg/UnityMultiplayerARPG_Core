89b01cd5aa9e11f32e4278e3cb30b9d8a9da9144
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-02-28 18:16:44 +0700

    Add ammo item

---
+++ Scripts/GameData/Item/Implements/AmmoItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    [CreateAssetMenu(fileName = "Socket Enhancer Item", menuName = "Create GameData/Item/Socket Enhancer Item", order = -4880)]
+    public class AmmoItem : BaseItem, IAmmoItem
+    {
+        public override string TypeTitle
+        {
+            get { return LanguageManager.GetText(UIItemTypeKeys.UI_ITEM_TYPE_AMMO.ToString()); }
+        }
+
+        public override ItemType ItemType
+        {
+            get { return ItemType.Armor; }
+        }
+
+        [SerializeField]
+        private AmmoType ammoType;
+        public AmmoType AmmoType
+        {
+            get { return ammoType; }
+        }
+
+        [SerializeField]
+        private DamageIncremental[] increaseDamages;
+        public DamageIncremental[] IncreaseDamages
+        {
+            get { return increaseDamages; }
+        }
+    }
+}
