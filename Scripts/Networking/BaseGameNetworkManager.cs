faaef93fc78e602932cbc17fca3ec1dd280c2b86
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-04-21 04:35:54 +0700

    Revert "Add update events"
    This reverts commit 45388f203c47a94adc59b0c0cc4f147ea4b5c499.

--- Scripts/Networking/BaseGameNetworkManager.cs
+++ Scripts/Networking/BaseGameNetworkManager.cs
@@ -62,8 +62,6 @@ namespace MultiplayerARPG
         public static readonly SocialGroupData ClientFriends = new SocialGroupData(1);
         public static BaseMapInfo CurrentMapInfo { get; protected set; }
         // Events
-        public System.Action onServerUpdate;
-        public System.Action onClientUpdate;
         public System.Action onClientWarp;
         public System.Action<ChatMessage> onClientReceiveChat;
         public System.Action<GameMessage> onClientReceiveGameMessage;
@@ -124,11 +122,6 @@ namespace MultiplayerARPG
                     UpdateOnlineCharacters();
                 lastUpdateOnlineCharacterTime = tempUnscaledTime;
             }
-            // Handle update events
-            if (IsServer && onServerUpdate != null)
-                onServerUpdate.Invoke();
-            if (IsClientConnected && onClientUpdate != null)
-                onClientUpdate.Invoke();
         }
 
         protected override void RegisterClientMessages()
