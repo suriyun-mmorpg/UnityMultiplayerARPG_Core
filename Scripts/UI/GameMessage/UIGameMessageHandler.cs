1d22ebf4cb30f23958b27cfd8d470125b9ff301d
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-05-04 06:52:47 +0700

    Add reward notifications

--- Scripts/UI/GameMessage/UIGameMessageHandler.cs
+++ Scripts/UI/GameMessage/UIGameMessageHandler.cs
@@ -5,17 +5,42 @@ namespace MultiplayerARPG
     public class UIGameMessageHandler : MonoBehaviour
     {
         public TextWrapper messagePrefab;
+        public TextWrapper rewardExpPrefab;
+        public TextWrapper rewardGoldPrefab;
+        public TextWrapper rewardItemPrefab;
+        [Tooltip("Format => {0} = {Exp Amount}")]
+        public UILocaleKeySetting formatKeyRewardExp = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_NOTIFY_REWARD_EXP);
+        [Tooltip("Format => {0} = {Gold Amount}")]
+        public UILocaleKeySetting formatKeyRewardGold = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_NOTIFY_REWARD_GOLD);
+        [Tooltip("Format => {0} = {Item Title}, {1} => {Amount}")]
+        public UILocaleKeySetting formatKeyRewardItem = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_NOTIFY_REWARD_ITEM);
         public Transform messageContainer;
         public float visibleDuration;
+
+        private void Awake()
+        {
+            if (!rewardExpPrefab)
+                rewardExpPrefab = messagePrefab;
+            if (!rewardGoldPrefab)
+                rewardGoldPrefab = messagePrefab;
+            if (!rewardItemPrefab)
+                rewardItemPrefab = messagePrefab;
+        }
 
         private void OnEnable()
         {
             BaseGameNetworkManager.Singleton.onClientReceiveGameMessage += OnReceiveGameMessage;
+            BaseGameNetworkManager.Singleton.onNotifyRewardExp += OnNotifyRewardExp;
+            BaseGameNetworkManager.Singleton.onNotifyRewardGold += OnNotifyRewardGold;
+            BaseGameNetworkManager.Singleton.onNotifyRewardItem += OnNotifyRewardItem;
         }
 
         private void OnDisable()
         {
             BaseGameNetworkManager.Singleton.onClientReceiveGameMessage -= OnReceiveGameMessage;
+            BaseGameNetworkManager.Singleton.onNotifyRewardExp -= OnNotifyRewardExp;
+            BaseGameNetworkManager.Singleton.onNotifyRewardGold -= OnNotifyRewardGold;
+            BaseGameNetworkManager.Singleton.onNotifyRewardItem -= OnNotifyRewardItem;
         }
 
         private void OnReceiveGameMessage(GameMessage gameMessage)
@@ -25,6 +50,46 @@ namespace MultiplayerARPG
 
             TextWrapper newMessage = Instantiate(messagePrefab);
             newMessage.text = LanguageManager.GetText(gameMessage.type.ToString());
+            newMessage.transform.SetParent(messageContainer);
+            newMessage.transform.localScale = Vector3.one;
+            newMessage.transform.localRotation = Quaternion.identity;
+            Destroy(newMessage.gameObject, visibleDuration);
+        }
+
+        private void OnNotifyRewardExp(int exp)
+        {
+            if (rewardExpPrefab == null)
+                return;
+
+            TextWrapper newMessage = Instantiate(rewardExpPrefab);
+            newMessage.text = string.Format(LanguageManager.GetText(formatKeyRewardExp.ToString()), exp);
+            newMessage.transform.SetParent(messageContainer);
+            newMessage.transform.localScale = Vector3.one;
+            newMessage.transform.localRotation = Quaternion.identity;
+            Destroy(newMessage.gameObject, visibleDuration);
+        }
+
+        private void OnNotifyRewardGold(int gold)
+        {
+            if (rewardGoldPrefab == null)
+                return;
+
+            TextWrapper newMessage = Instantiate(rewardGoldPrefab);
+            newMessage.text = string.Format(LanguageManager.GetText(formatKeyRewardGold.ToString()), gold);
+            newMessage.transform.SetParent(messageContainer);
+            newMessage.transform.localScale = Vector3.one;
+            newMessage.transform.localRotation = Quaternion.identity;
+            Destroy(newMessage.gameObject, visibleDuration);
+        }
+
+        private void OnNotifyRewardItem(int dataId, short amount)
+        {
+            BaseItem item;
+            if (rewardItemPrefab == null || !GameInstance.Items.TryGetValue(dataId, out item))
+                return;
+
+            TextWrapper newMessage = Instantiate(rewardItemPrefab);
+            newMessage.text = string.Format(LanguageManager.GetText(formatKeyRewardItem.ToString()), item.Title, amount);
             newMessage.transform.SetParent(messageContainer);
             newMessage.transform.localScale = Vector3.one;
             newMessage.transform.localRotation = Quaternion.identity;
