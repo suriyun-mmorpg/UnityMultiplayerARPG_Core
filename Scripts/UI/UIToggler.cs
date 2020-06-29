a06634eda8f94862aea6ad8e627c2c7c9cb3c6e3
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-06-29 09:14:49 +0700

    Add helper component to toggle ui

---
+++ Scripts/UI/UIToggler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIToggler : MonoBehaviour
+{
+    public UIBase ui;
+    public KeyCode key;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(key))
+            ui.Toggle();
+    }
+}
