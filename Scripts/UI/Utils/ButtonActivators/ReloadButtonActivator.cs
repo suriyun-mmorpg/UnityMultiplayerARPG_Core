b18035186a262f8484a6dc1dc7c3560173bd6555
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-07-12 17:39:56 +0700

    Add reload button activator

---
+++ Scripts/UI/Utils/ButtonActivators/ReloadButtonActivator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public class ReloadButtonActivator : MonoBehaviour
+    {
+        public GameObject[] activateObjects;
+
+        private bool canReload;
+
+        private void LateUpdate()
+        {
+            canReload = IsReloadable(BasePlayerCharacterController.OwningCharacter.EquipWeapons.rightHand) ||
+                IsReloadable(BasePlayerCharacterController.OwningCharacter.EquipWeapons.leftHand);
+
+            foreach (GameObject obj in activateObjects)
+            {
+                obj.SetActive(canReload);
+            }
+        }
+
+        private bool IsReloadable(CharacterItem characterItem)
+        {
+            IWeaponItem weaponItem = characterItem.GetWeaponItem();
+            return weaponItem != null && weaponItem.WeaponType.RequireAmmoType != null;
+        }
+    }
+}
