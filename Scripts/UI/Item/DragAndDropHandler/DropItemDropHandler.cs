6be5a4f53c87d291cbdf6cbdfc0ae383187a3873
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-07-24 14:52:29 +0700

    Add drop item drop handler

---
+++ Scripts/UI/Item/DragAndDropHandler/DropItemDropHandler.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace MultiplayerARPG
+{
+    public class DropItemDropHandler : MonoBehaviour, IDropHandler
+    {
+        private RectTransform dropRect;
+        public RectTransform DropRect
+        {
+            get
+            {
+                if (dropRect == null)
+                    dropRect = transform as RectTransform;
+                return dropRect;
+            }
+        }
+
+        public void OnDrop(PointerEventData eventData)
+        {
+            // Validate drop position
+            if (!RectTransformUtility.RectangleContainsScreenPoint(DropRect, Input.mousePosition))
+                return;
+            // Validate dragging UI
+            UIDragHandler dragHandler = eventData.pointerDrag.GetComponent<UIDragHandler>();
+            if (dragHandler == null || dragHandler.isDropped)
+                return;
+            // Get dragged item UI, if dragging item UI is UI for character item.
+            // try to equip the item
+            UICharacterItemDragHandler draggedItemUI = dragHandler as UICharacterItemDragHandler;
+            if (draggedItemUI != null)
+            {
+                switch (draggedItemUI.sourceLocation)
+                {
+                    case UICharacterItemDragHandler.SourceLocation.EquipItems:
+                        break;
+                    case UICharacterItemDragHandler.SourceLocation.NonEquipItems:
+                        draggedItemUI.uiCharacterItem.OnClickDrop();
+                        break;
+                    case UICharacterItemDragHandler.SourceLocation.StorageItems:
+                        break;
+                }
+            }
+        }
+    }
+}
