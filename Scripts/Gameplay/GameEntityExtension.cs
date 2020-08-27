df1829dea64a226e97343a91b1fce616953e2e8f
Insthync <ittipon.bay@gmail.com>
2020-08-27 17:37:32 +0700

    Add get connection id extension for game entity

--- Scripts/Gameplay/GameEntityExtension.cs
+++ Scripts/Gameplay/GameEntityExtension.cs
@@ -4,11 +4,18 @@ namespace MultiplayerARPG
 {
     public static class GameEntityExtension
     {
+        public static long GetConnectionId(this IGameEntity gameEntity)
+        {
+            if (gameEntity == null || !gameEntity.Entity)
+                return -1;
+            return gameEntity.Identity.ConnectionId;
+        }
+
         public static uint GetObjectId(this IGameEntity gameEntity)
         {
             if (gameEntity == null || !gameEntity.Entity)
                 return 0;
-            return gameEntity.Entity.ObjectId;
+            return gameEntity.Identity.ObjectId;
         }
 
         public static Transform GetTransform(this IGameEntity gameEntity)
