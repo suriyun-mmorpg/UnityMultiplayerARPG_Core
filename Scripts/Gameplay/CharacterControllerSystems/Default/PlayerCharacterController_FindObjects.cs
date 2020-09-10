8d03f8b8bb05901dd437fd6c389dea9433a81855
Insthync <ittipon.bay@gmail.com>
2020-09-11 00:09:49 +0700

    Improve building area detection

--- Scripts/Gameplay/CharacterControllerSystems/Default/PlayerCharacterController_FindObjects.cs
+++ Scripts/Gameplay/CharacterControllerSystems/Default/PlayerCharacterController_FindObjects.cs
@@ -40,7 +40,7 @@ namespace MultiplayerARPG
 
                 buildingArea = tempTransform.GetComponent<BuildingArea>();
                 if (buildingArea == null ||
-                    (buildingArea.Entity && buildingArea.GetObjectId() == ConstructingBuildingEntity.ObjectId) ||
+                    buildingArea.Entity == ConstructingBuildingEntity ||
                     !ConstructingBuildingEntity.buildingTypes.Contains(buildingArea.buildingType))
                 {
                     // Skip because this area is not allowed to build the building that you are going to build
