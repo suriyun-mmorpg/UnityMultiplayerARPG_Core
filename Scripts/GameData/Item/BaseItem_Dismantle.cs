5cdda85d52a3f8fdb9d1141e0b4fb3dea57d0daf
Insthync <ittipon.bay@gmail.com>
2020-09-26 14:45:42 +0700

    Improve dismantle item amount codes

--- Scripts/GameData/Item/BaseItem_Dismantle.cs
+++ Scripts/GameData/Item/BaseItem_Dismantle.cs
@@ -5,10 +5,13 @@ namespace MultiplayerARPG
 {
     public partial class BaseItem
     {
-        public static List<ItemAmount> GetDismantleReturnItems(CharacterItem dismantlingItem)
+        public static List<ItemAmount> GetDismantleReturnItems(CharacterItem dismantlingItem, short amount)
         {
-            if (dismantlingItem.IsEmptySlot())
+            if (dismantlingItem.IsEmptySlot() || amount == 0)
                 return new List<ItemAmount>();
+
+            if (amount < 0 || amount > dismantlingItem.amount)
+                amount = dismantlingItem.amount;
 
             List<ItemAmount> result = new List<ItemAmount>();
             ItemAmount[] dismantleReturnItems = dismantlingItem.GetItem().dismantleReturnItems;
@@ -17,7 +20,7 @@ namespace MultiplayerARPG
                 result.Add(new ItemAmount()
                 {
                     item = dismantleReturnItems[i].item,
-                    amount = (short)(dismantleReturnItems[i].amount * dismantlingItem.amount)
+                    amount = (short)(dismantleReturnItems[i].amount * amount)
                 });
             }
             if (dismantlingItem.Sockets.Count > 0)
