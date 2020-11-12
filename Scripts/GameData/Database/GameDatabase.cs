049726472ccededa690faebf9ac66547d05ac0fb
Insthync <ittipon.bay@gmail.com>
2020-11-12 09:21:36 +0700

    WIP game database improvement

--- Scripts/GameData/Database/GameDatabase.cs
+++ Scripts/GameData/Database/GameDatabase.cs
@@ -9,16 +9,40 @@ namespace MultiplayerARPG
     [CreateAssetMenu(fileName = "Game Database", menuName = "Create GameDatabase/Game Database", order = -5999)]
     public class GameDatabase : BaseGameDatabase
     {
-        public Attribute[] attributes;
-        public BaseItem[] items;
-        public GuildSkill[] guildSkills;
+        [Header("Entity")]
+        public UnityHelpBox entityHelpBox = new UnityHelpBox("Game database will load referring game data from an entities when game instance initializing");
         public BasePlayerCharacterEntity[] playerCharacterEntities;
         public BaseMonsterCharacterEntity[] monsterCharacterEntities;
         [FormerlySerializedAs("mountEntities")]
         public VehicleEntity[] vehicleEntities;
+
+        [Header("Game Data")]
+        public UnityHelpBox gameDataHelpBox = new UnityHelpBox("Only Attributes, Items, Guild Skills, Map Infos, Quests and Factions game data are required. Other game data can be loaded because they were referred by those game data.");
+        public Attribute[] attributes;
+        public BaseItem[] items;
+        public GuildSkill[] guildSkills;
         public BaseMapInfo[] mapInfos;
+        public Quest[] quests;
         public Faction[] factions;
-        
+
+        // TODO: WIP game database editor
+        /*
+        public Attribute[] attributes;
+        public DamageElement[] damageElements;
+        public BaseItem[] items;
+        public ArmorType[] armorTypes;
+        public WeaponType[] weaponTypes;
+        public AmmoType[] ammoTypes;
+        public BaseSkill[] skills;
+        public GuildSkill[] guildSkills;
+        public BaseCharacter[] characters;
+        public Harvestable[] harvestables;
+        public BaseMapInfo[] mapInfos;
+        public BaseNpcDialog[] npcDialogs;
+        public Quest[] quests;
+        public Faction[] factions;
+        */
+
         public override void LoadData(GameInstance gameInstance)
         {
             GameInstance.AddAttributes(attributes);
@@ -28,9 +52,16 @@ namespace MultiplayerARPG
             GameInstance.AddCharacterEntities(monsterCharacterEntities);
             GameInstance.AddVehicleEntities(vehicleEntities);
             GameInstance.AddMapInfos(mapInfos);
+            GameInstance.AddQuests(quests);
             GameInstance.AddFactions(factions);
             // Tell game instance that data loaded
             gameInstance.LoadedGameData();
+        }
+
+        public void LoadReferredData()
+        {
+            // TODO: Users have to use this function to load and set referred data to game database
+            // then game database editor will be able to load and show them in game data finder.
         }
     }
 }
