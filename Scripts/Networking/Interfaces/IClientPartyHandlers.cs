365fef6852df5ba7f1e35b630259346ff347dc0a
Insthync <ittipon.bay@gmail.com>
2020-12-13 11:02:41 +0700

    WIP guild/party structure changes: Fix wrong function name

--- Scripts/Networking/Interfaces/IClientPartyHandlers.cs
+++ Scripts/Networking/Interfaces/IClientPartyHandlers.cs
@@ -8,7 +8,7 @@ namespace MultiplayerARPG
 
         bool RequestCreateParty(RequestCreatePartyMessage data, ResponseDelegate<ResponseCreatePartyMessage> callback);
         bool RequestChangePartyLeader(RequestChangePartyLeaderMessage data, ResponseDelegate<ResponseChangePartyLeaderMessage> callback);
-        bool RequestChangePartyMessage(RequestChangePartySettingMessage data, ResponseDelegate<ResponseChangePartySettingMessage> callback);
+        bool RequestChangePartySetting(RequestChangePartySettingMessage data, ResponseDelegate<ResponseChangePartySettingMessage> callback);
         bool RequestSendPartyInvitation(RequestSendPartyInvitationMessage data, ResponseDelegate<ResponseSendPartyInvitationMessage> callback);
         bool RequestAcceptPartyInvitation(RequestAcceptPartyInvitationMessage data, ResponseDelegate<ResponseAcceptPartyInvitationMessage> callback);
         bool RequestDeclinePartyInvitation(RequestDeclinePartyInvitationMessage data, ResponseDelegate<ResponseDeclinePartyInvitationMessage> callback);
