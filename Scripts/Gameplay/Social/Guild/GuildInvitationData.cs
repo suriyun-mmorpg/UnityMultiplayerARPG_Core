72c3c678704da7e0015bdfad2663cf9efc220de8
Insthync <ittipon.bay@gmail.com>
2020-12-15 00:10:56 +0700

    Implement INetSerialize to invitation data

--- Scripts/Gameplay/Social/Guild/GuildInvitationData.cs
+++ Scripts/Gameplay/Social/Guild/GuildInvitationData.cs
@@ -1,6 +1,8 @@
-﻿namespace MultiplayerARPG
+﻿using LiteNetLib.Utils;
+
+namespace MultiplayerARPG
 {
-    public struct GuildInvitationData
+    public struct GuildInvitationData : INetSerializable
     {
         public string InviterId { get; set; }
         public string InviterName { get; set; }
@@ -8,5 +10,25 @@
         public int GuildId { get; set; }
         public string GuildName { get; set; }
         public short GuildLevel { get; set; }
+
+        public void Deserialize(NetDataReader reader)
+        {
+            InviterId = reader.GetString();
+            InviterName = reader.GetString();
+            InviterLevel = reader.GetPackedShort();
+            GuildId = reader.GetPackedInt();
+            GuildName = reader.GetString();
+            GuildLevel = reader.GetPackedShort();
+        }
+
+        public void Serialize(NetDataWriter writer)
+        {
+            writer.Put(InviterId);
+            writer.Put(InviterName);
+            writer.PutPackedShort(InviterLevel);
+            writer.PutPackedInt(GuildId);
+            writer.Put(GuildName);
+            writer.PutPackedShort(GuildLevel);
+        }
     }
 }
