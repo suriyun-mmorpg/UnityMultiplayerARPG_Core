cadec46c05363e4671804adbee308d6ff32a82b3
Insthync <ittipon.bay@gmail.com>
2020-12-17 06:27:41 +0700

    Implement default client bank handlers

--- Scripts/Networking/Implements/DefaultClientBankHandlers.cs
+++ Scripts/Networking/Implements/DefaultClientBankHandlers.cs
@@ -1,5 +1,6 @@
 ﻿using LiteNetLibManager;
 using UnityEngine;
+using ReqTypes = MultiplayerARPG.BaseGameNetworkManager.ReqTypes;
 
 namespace MultiplayerARPG
 {
@@ -7,22 +8,22 @@ namespace MultiplayerARPG
     {
         public bool RequestDepositGuildGold(RequestDepositGuildGoldMessage data, ResponseDelegate<ResponseDepositGuildGoldMessage> callback)
         {
-            throw new System.NotImplementedException();
+            return BaseGameNetworkManager.Singleton.ClientSendRequest(ReqTypes.DepositGuildGold, data, responseDelegate: callback);
         }
 
         public bool RequestDepositUserGold(RequestDepositUserGoldMessage data, ResponseDelegate<ResponseDepositUserGoldMessage> callback)
         {
-            throw new System.NotImplementedException();
+            return BaseGameNetworkManager.Singleton.ClientSendRequest(ReqTypes.DepositUserGold, data, responseDelegate: callback);
         }
 
         public bool RequestWithdrawGuildGold(RequestWithdrawGuildGoldMessage data, ResponseDelegate<ResponseWithdrawGuildGoldMessage> callback)
         {
-            throw new System.NotImplementedException();
+            return BaseGameNetworkManager.Singleton.ClientSendRequest(ReqTypes.WithdrawGuildGold, data, responseDelegate: callback);
         }
 
         public bool RequestWithdrawUserGold(RequestWithdrawUserGoldMessage data, ResponseDelegate<ResponseWithdrawUserGoldMessage> callback)
         {
-            throw new System.NotImplementedException();
+            return BaseGameNetworkManager.Singleton.ClientSendRequest(ReqTypes.WithdrawUserGold, data, responseDelegate: callback);
         }
     }
 }
