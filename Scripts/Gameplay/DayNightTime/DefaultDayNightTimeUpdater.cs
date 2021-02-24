f7ece33dce5d2770487dbb588213128836dd32fc
Insthync <ittipon.bay@gmail.com>
2021-02-24 22:23:54 +0700

    Change default seconds to one day to 15 minutes

--- Scripts/Gameplay/DayNightTime/DefaultDayNightTimeUpdater.cs
+++ Scripts/Gameplay/DayNightTime/DefaultDayNightTimeUpdater.cs
@@ -11,7 +11,7 @@ namespace MultiplayerARPG
         [SerializeField]
         [Tooltip("If this value is 60 it will turn to a new day in 60 seconds")]
         [Min(10)]
-        private float secondsToOneDay = 60;
+        private float secondsToOneDay = 60 * 15;
         [SerializeField]
         [Range(0, 23)]
         private int startHourOfDay = 12;
