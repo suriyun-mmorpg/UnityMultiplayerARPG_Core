b3126496dfde3708381012a839539769edafd256
Insthync <ittipon.bay@gmail.com>
2021-03-31 05:00:49 +0700

    Implement cash shop item buying by currency type

--- Scripts/Networking/Messages/CashShop/RequestCashShopBuyMessage.cs
+++ Scripts/Networking/Messages/CashShop/RequestCashShopBuyMessage.cs
@@ -5,15 +5,21 @@ namespace MultiplayerARPG
     public struct RequestCashShopBuyMessage : INetSerializable
     {
         public int dataId;
+        public CashShopItemCurrencyType currencyType;
+        public int amount;
 
         public void Deserialize(NetDataReader reader)
         {
             dataId = reader.GetInt();
+            currencyType = (CashShopItemCurrencyType)reader.GetByte();
+            amount = reader.GetInt();
         }
 
         public void Serialize(NetDataWriter writer)
         {
             writer.Put(dataId);
+            writer.Put((byte)currencyType);
+            writer.Put(amount);
         }
     }
 }
