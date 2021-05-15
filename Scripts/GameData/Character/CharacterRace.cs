51f9f59fbd47fc6386a6958b51773eb0ec1c1d4e
Insthync <ittipon.bay@gmail.com>
2021-05-15 23:58:41 +0700

    Make character race partial

--- Scripts/GameData/Character/CharacterRace.cs
+++ Scripts/GameData/Character/CharacterRace.cs
@@ -3,7 +3,7 @@
 namespace MultiplayerARPG
 {
     [CreateAssetMenu(fileName = "Character Race", menuName = "Create GameData/Character Race", order = -4990)]
-    public class CharacterRace : BaseGameData
+    public partial class CharacterRace : BaseGameData
     {
     }
 }
