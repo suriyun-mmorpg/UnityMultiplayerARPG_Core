287162efbf1edcf2eec163c74c0abc4bcb550a9b
Insthync <ittipon.bay@gmail.com>
2021-06-09 14:49:20 +0700

    Change order of create menu

--- Scripts/GameData/Character/CharacterRace.cs
+++ Scripts/GameData/Character/CharacterRace.cs
@@ -2,7 +2,7 @@
 
 namespace MultiplayerARPG
 {
-    [CreateAssetMenu(fileName = "Character Race", menuName = "Create GameData/Character Race", order = -4990)]
+    [CreateAssetMenu(fileName = "Character Race", menuName = "Create GameData/Character Race", order = -4992)]
     public partial class CharacterRace : BaseGameData
     {
     }
