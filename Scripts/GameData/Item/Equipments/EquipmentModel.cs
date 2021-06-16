c0085cb56ebb04e4187ea4b7d56eca97f5c9e62b
Insthync <ittipon.bay@gmail.com>
2021-06-17 01:32:42 +0700

    Changes equipment model back to struct

--- Scripts/GameData/Item/Equipments/EquipmentModel.cs
+++ Scripts/GameData/Item/Equipments/EquipmentModel.cs
@@ -3,7 +3,7 @@
 namespace MultiplayerARPG
 {
     [System.Serializable]
-    public class EquipmentModel
+    public struct EquipmentModel
     {
         public string equipSocket;
         public bool useInstantiatedObject;
@@ -11,8 +11,8 @@ namespace MultiplayerARPG
         public GameObject model;
         [BoolShowConditional(nameof(useInstantiatedObject), true)]
         public int instantiatedObjectIndex;
-        public Vector3 localPosition = Vector3.zero;
-        public Vector3 localEulerAngles = Vector3.zero;
-        public Vector3 localScale = Vector3.one;
+        public Vector3 localPosition;
+        public Vector3 localEulerAngles;
+        public Vector3 localScale;
     }
 }
