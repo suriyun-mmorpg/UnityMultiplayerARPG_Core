29d00e0c3a6ed8a414ca2410fbeae1fb8cd19d8b
Insthync <ittipon.bay@gmail.com>
2021-07-14 02:31:32 +0700

    Add simple component to do something on enable / disable while game network manager is Lan RPG

---
+++ Scripts/LanGame/Utils/LanGameEnableDisableEvents.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace MultiplayerARPG
+{
+    public class LanGameEnableDisableEvents : MonoBehaviour
+    {
+        public UnityEvent onEnable = new UnityEvent();
+        public UnityEvent onDisable = new UnityEvent();
+
+        private void OnEnable()
+        {
+            onEnable.Invoke();
+        }
+
+        private void OnDisable()
+        {
+            onDisable.Invoke();
+        }
+    }
+}
