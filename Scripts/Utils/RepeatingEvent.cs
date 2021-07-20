abab59caeb98735a0c1cd5605a5d53cba6f40551
Insthync <ittipon.bay@gmail.com>
2021-07-20 10:54:38 +0700

    Add repeating event

---
+++ Scripts/Utils/RepeatingEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UtilsComponents
+{
+    public class RepeatingEvent : MonoBehaviour
+    {
+        public float firstDelay;
+        public float repeatDelay;
+        public UnityEvent repeating;
+
+        private void Start()
+        {
+            InvokeRepeating(nameof(Repeating), firstDelay, repeatDelay);
+        }
+
+        private void Repeating()
+        {
+            repeating.Invoke();
+        }
+    }
+}
