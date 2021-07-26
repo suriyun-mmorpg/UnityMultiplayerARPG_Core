b4a1590b10c63cf780699b867e759e96866bf4f2
Insthync <ittipon.bay@gmail.com>
2021-07-27 00:09:49 +0700

    Improve game entity editor

---
+++ Editor/BaseGameEntityEditor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace MultiplayerARPG
+{
+    [CustomEditor(typeof(BaseGameEntity), true)]
+    public class BaseGameEntityEditor : BaseCustomCategorizedEditor
+    {
+    }
+}
