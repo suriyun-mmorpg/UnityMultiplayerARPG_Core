2ae84dd87152183cfbecd8c6710fa0b3547af5c1
Insthync <ittipon.bay@gmail.com>
2021-07-28 04:30:11 +0700

    Don't include mail data with claim mail items message

--- Scripts/Networking/Messages/Mail/ResponseClaimMailItemsMessage.cs
+++ Scripts/Networking/Messages/Mail/ResponseClaimMailItemsMessage.cs
@@ -5,20 +5,15 @@ namespace MultiplayerARPG
     public struct ResponseClaimMailItemsMessage : INetSerializable
     {
         public UITextKeys message;
-        public Mail mail;
 
         public void Deserialize(NetDataReader reader)
         {
             message = (UITextKeys)reader.GetPackedUShort();
-            if (message == UITextKeys.NONE)
-                mail = reader.GetValue<Mail>();
         }
 
         public void Serialize(NetDataWriter writer)
         {
             writer.PutPackedUShort((ushort)message);
-            if (message == UITextKeys.NONE)
-                writer.PutValue(mail);
         }
     }
 }
