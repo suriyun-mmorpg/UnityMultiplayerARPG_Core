ad7bb6784b3ddf36138c3a3779bf34eb9cf89977
Insthync <ittipon.bay@gmail.com>
2021-07-28 01:00:47 +0700

    Add latest chat message UI

---
+++ Scripts/UI/Chat/UILatestChatMessage.cs
@@ -0,0 +1,22 @@
+﻿namespace MultiplayerARPG
+{
+    public partial class UILatestChatMessage : UIChatMessage
+    {
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            ClientGenericActions.onClientReceiveChatMessage += OnReceiveChat;
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            ClientGenericActions.onClientReceiveChatMessage -= OnReceiveChat;
+        }
+
+        private void OnReceiveChat(ChatMessage chatMessage)
+        {
+            Data = chatMessage;
+        }
+    }
+}
