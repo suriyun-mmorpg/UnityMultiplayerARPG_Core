bfbf4f40e09100e44145020dc09b7df90d5d281b
Insthync <ittipon.bay@gmail.com>
2021-08-24 14:43:05 +0700

    Delete unused namespace

--- Scripts/Gameplay/Area/GameSpawnArea.cs
+++ Scripts/Gameplay/Area/GameSpawnArea.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Cysharp.Threading.Tasks;
 using LiteNetLibManager;
 using UnityEngine;
 using UnityEngine.Serialization;
--- Scripts/Gameplay/Area/MonsterSpawnArea.cs
+++ Scripts/Gameplay/Area/MonsterSpawnArea.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using LiteNetLibManager;
 using System.Collections.Generic;
 #if UNITY_EDITOR
