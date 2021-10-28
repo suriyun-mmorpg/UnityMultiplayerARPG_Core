e9cad665c1c2e1d7c704162da6bea8ccf0fa33d6
Insthync <ittipon.bay@gmail.com>
2021-10-29 05:05:11 +0700

    Fix building area cannot be triggered

--- Scripts/Gameplay/BuildingSystems/BuildingArea.cs
+++ Scripts/Gameplay/BuildingSystems/BuildingArea.cs
@@ -12,13 +12,34 @@ namespace MultiplayerARPG
         public string buildingType;
         public bool snapBuildingObject;
         public bool allowRotateInSocket;
+        public Collider CacheCollider { get; private set; }
+        public Rigidbody CacheRigidbody { get; private set; }
+        public Collider2D CacheCollider2D { get; private set; }
+        public Rigidbody2D CacheRigidbody2D { get; private set; }
 
         private void Start()
         {
             if (entity == null)
                 entity = GetComponentInParent<BuildingEntity>();
             if (entity != null)
+            {
                 gameObject.GetOrAddComponent<UnHittable>();
+                CacheCollider = GetComponent<Collider>();
+                if (CacheCollider)
+                {
+                    CacheRigidbody = gameObject.GetOrAddComponent<Rigidbody>();
+                    CacheRigidbody.useGravity = false;
+                    CacheRigidbody.isKinematic = true;
+                    return;
+                }
+                CacheCollider2D = GetComponent<Collider2D>();
+                if (CacheCollider2D)
+                {
+                    CacheRigidbody2D = gameObject.GetOrAddComponent<Rigidbody2D>();
+                    CacheRigidbody2D.gravityScale = 0;
+                    CacheRigidbody2D.isKinematic = true;
+                }
+            }
         }
 
         public bool IsPartOfBuildingEntity(BuildingEntity entity)
