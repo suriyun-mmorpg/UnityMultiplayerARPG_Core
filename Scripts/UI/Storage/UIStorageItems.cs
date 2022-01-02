531c9cc121ad065ce388d804313a49b782cb8639
Insthync <ittipon.bay@gmail.com>
2022-01-03 05:29:43 +0700

    Improve ui storage list filtering

--- Scripts/UI/Storage/UIStorageItems.cs
+++ Scripts/UI/Storage/UIStorageItems.cs
@@ -4,56 +4,17 @@ using UnityEngine.Serialization;
 
 namespace MultiplayerARPG
 {
-    public class UIStorageItems : UIBase
+    public class UIStorageItems : UICharacterItems
     {
-        [Header("String Formats")]
+        [Header("Storage String Formats")]
         [Tooltip("Format => {0} = {Current Total Weights}, {1} = {Weight Limit}")]
         public UILocaleKeySetting formatKeyWeightLimit = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_CURRENT_WEIGHT);
         [Tooltip("Format => {0} = {Current Used Slots}, {1} = {Slot Limit}")]
         public UILocaleKeySetting formatKeySlotLimit = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_CURRENT_SLOT);
 
-        [Header("Filter")]
-        public List<string> filterCategories = new List<string>();
-        public List<ItemType> filterItemTypes = new List<ItemType>();
-        public bool doNotShowEmptySlots;
-
-        [Header("UI Elements")]
-        public GameObject listEmptyObject;
-        [FormerlySerializedAs("uiItemDialog")]
-        public UICharacterItem uiDialog;
-        [FormerlySerializedAs("uiCharacterItemPrefab")]
-        public UICharacterItem uiPrefab;
-        [FormerlySerializedAs("uiCharacterItemContainer")]
-        public Transform uiContainer;
+        [Header("Storage UI Elements")]
         public TextWrapper uiTextWeightLimit;
         public TextWrapper uiTextSlotLimit;
-
-        private UIList cacheList;
-        public UIList CacheList
-        {
-            get
-            {
-                if (cacheList == null)
-                {
-                    cacheList = gameObject.AddComponent<UIList>();
-                    cacheList.uiPrefab = uiPrefab.gameObject;
-                    cacheList.uiContainer = uiContainer;
-                }
-                return cacheList;
-            }
-        }
-
-        private UICharacterItemSelectionManager cacheSelectionManager;
-        public UICharacterItemSelectionManager CacheSelectionManager
-        {
-            get
-            {
-                if (cacheSelectionManager == null)
-                    cacheSelectionManager = gameObject.GetOrAddComponent<UICharacterItemSelectionManager>();
-                cacheSelectionManager.selectionMode = UISelectionMode.SelectSingle;
-                return cacheSelectionManager;
-            }
-        }
 
         public StorageType StorageType { get; private set; }
         public string StorageOwnerId { get; private set; }
@@ -62,24 +23,21 @@ namespace MultiplayerARPG
         public short SlotLimit { get; private set; }
         public float TotalWeight { get; private set; }
         public short UsedSlots { get; private set; }
-        public List<CharacterItem> LoadedList { get; private set; } = new List<CharacterItem>();
 
         private bool doNotCloseStorageOnDisable;
 
-        protected virtual void OnEnable()
+        protected override void OnEnable()
         {
+            inventoryType = InventoryType.StorageItems;
             ClientStorageActions.onNotifyStorageItemsUpdated += UpdateData;
-            CacheSelectionManager.eventOnSelected.RemoveListener(OnSelect);
-            CacheSelectionManager.eventOnSelected.AddListener(OnSelect);
-            CacheSelectionManager.eventOnDeselected.RemoveListener(OnDeselect);
-            CacheSelectionManager.eventOnDeselected.AddListener(OnDeselect);
-            if (uiDialog != null)
-                uiDialog.onHide.AddListener(OnDialogHide);
+            onGenerateEntry += OnGenerateEntry;
+            base.OnEnable();
         }
 
-        protected virtual void OnDisable()
+        protected override void OnDisable()
         {
             ClientStorageActions.onNotifyStorageItemsUpdated -= UpdateData;
+            onGenerateEntry -= OnGenerateEntry;
             // Close storage
             if (!doNotCloseStorageOnDisable)
                 GameInstance.ClientStorageHandlers.RequestCloseStorage(ClientStorageActions.ResponseCloseStorage);
@@ -90,10 +48,7 @@ namespace MultiplayerARPG
             TargetEntity = null;
             WeightLimit = 0;
             SlotLimit = 0;
-            // Hide
-            if (uiDialog != null)
-                uiDialog.onHide.RemoveListener(OnDialogHide);
-            CacheSelectionManager.DeselectSelectedUI();
+            base.OnDisable();
         }
 
         public override void Hide()
@@ -108,7 +63,7 @@ namespace MultiplayerARPG
             base.Hide();
         }
 
-        protected virtual void Update()
+        protected override void Update()
         {
             if (uiTextWeightLimit != null)
             {
@@ -125,6 +80,8 @@ namespace MultiplayerARPG
                 else
                     uiTextSlotLimit.text = string.Format(LanguageManager.GetText(formatKeySlotLimit), UsedSlots.ToString("N0"), SlotLimit.ToString("N0"));
             }
+
+            base.Update();
         }
 
         public void Show(StorageType storageType, string storageOwnerId, BaseGameEntity targetEntity, short weightLimit, short slotLimit)
@@ -137,144 +94,26 @@ namespace MultiplayerARPG
             Show();
         }
 
-        protected virtual void OnDialogHide()
-        {
-            CacheSelectionManager.DeselectSelectedUI();
-        }
-
-        protected virtual void OnSelect(UICharacterItem ui)
-        {
-            if (ui.Data.characterItem.IsEmptySlot())
-            {
-                CacheSelectionManager.DeselectSelectedUI();
-                return;
-            }
-            if (uiDialog != null)
-            {
-                uiDialog.selectionManager = CacheSelectionManager;
-                uiDialog.Setup(ui.Data, GameInstance.PlayingCharacter, ui.IndexOfData);
-                uiDialog.Show();
-            }
-        }
-
-        protected virtual void OnDeselect(UICharacterItem ui)
-        {
-            if (uiDialog != null)
-            {
-                uiDialog.onHide.RemoveListener(OnDialogHide);
-                uiDialog.Hide();
-                uiDialog.onHide.AddListener(OnDialogHide);
-            }
-        }
-
         public virtual void UpdateData(IList<CharacterItem> characterItems)
         {
-            LoadedList.Clear();
-            if (characterItems != null && characterItems.Count > 0)
-                LoadedList.AddRange(characterItems);
-            GenerateList();
+            UpdateData(GameInstance.PlayingCharacter, characterItems);
         }
 
-        public virtual void GenerateList()
+        public override void GenerateList()
         {
-            string selectedId = CacheSelectionManager.SelectedUI != null ? CacheSelectionManager.SelectedUI.CharacterItem.id : string.Empty;
-            CacheSelectionManager.Clear();
-            ConvertFilterCategoriesToTrimedLowerChar();
-
+            // Reset total weight and used slots, it will be increased in `OnGenerateEntry` function
             TotalWeight = 0;
             UsedSlots = 0;
-
-            if (LoadedList.Count == 0)
-            {
-                if (uiDialog != null)
-                    uiDialog.Hide();
-                CacheList.HideAll();
-                if (listEmptyObject != null)
-                    listEmptyObject.SetActive(true);
-                return;
-            }
-
-            int showingCount = 0;
-            UICharacterItem selectedUI = null;
-            UICharacterItem tempUI;
-            BaseItem tempItem;
-            CacheList.Generate(LoadedList, (index, data, ui) =>
-            {
-                tempUI = ui.GetComponent<UICharacterItem>();
-                tempItem = data.GetItem();
-
-                if (data.NotEmptySlot())
-                {
-                    // Increase total weight and used slots, if data is not empty slot
-                    TotalWeight += data.GetItem().Weight * data.amount;
-                    UsedSlots++;
-                }
-
-                if (!GameInstance.Singleton.IsLimitInventorySlot || doNotShowEmptySlots ||
-                    (filterCategories != null && filterCategories.Count > 0) ||
-                    (filterItemTypes != null && filterItemTypes.Count > 0))
-                {
-                    // If inventory type isn't limit inventory slot, hide empty slot
-                    if (tempItem == null)
-                    {
-                        tempUI.Hide();
-                        return;
-                    }
-                }
-
-                if (tempItem == null ||
-                    (filterCategories.Count == 0 || (!string.IsNullOrEmpty(tempItem.Category) &&
-                    filterCategories.Contains(tempItem.Category.Trim().ToLower()))))
-                {
-                    if (filterItemTypes == null || filterItemTypes.Count == 0 ||
-                        filterItemTypes.Contains(tempItem.ItemType))
-                    {
-                        tempUI.Setup(new UICharacterItemData(data, InventoryType.StorageItems), GameInstance.PlayingCharacter, index);
-                        tempUI.Show();
-                        UICharacterItemDragHandler dragHandler = tempUI.GetComponentInChildren<UICharacterItemDragHandler>();
-                        if (dragHandler != null)
-                            dragHandler.SetupForStorageItems(tempUI);
-                        CacheSelectionManager.Add(tempUI);
-                        if (!string.IsNullOrEmpty(selectedId) && selectedId.Equals(data.id))
-                            selectedUI = tempUI;
-                        showingCount++;
-                    }
-                    else
-                    {
-                        // Hide because item's type not matches in the filter list
-                        tempUI.Hide();
-                    }
-                }
-                else
-                {
-                    // Hide because item's category not matches in the filter list
-                    tempUI.Hide();
-                }
-            });
-
-            if (listEmptyObject != null)
-                listEmptyObject.SetActive(showingCount == 0);
-
-            if (selectedUI == null)
-            {
-                CacheSelectionManager.DeselectSelectedUI();
-            }
-            else
-            {
-                bool defaultDontShowComparingEquipments = uiDialog != null ? uiDialog.dontShowComparingEquipments : false;
-                if (uiDialog != null)
-                    uiDialog.dontShowComparingEquipments = true;
-                selectedUI.OnClickSelect();
-                if (uiDialog != null)
-                    uiDialog.dontShowComparingEquipments = defaultDontShowComparingEquipments;
-            }
+            base.GenerateList();
         }
 
-        protected void ConvertFilterCategoriesToTrimedLowerChar()
+        private void OnGenerateEntry(int indexOfData, CharacterItem characterItem)
         {
-            for (int i = 0; i < filterCategories.Count; ++i)
+            if (characterItem.NotEmptySlot())
             {
-                filterCategories[i] = filterCategories[i].Trim().ToLower();
+                // Increase total weight and used slots, if data is not empty slot
+                TotalWeight += characterItem.GetItem().Weight * characterItem.amount;
+                UsedSlots++;
             }
         }
     }
