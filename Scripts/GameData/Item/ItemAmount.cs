6a837d8edf150733fcc62670e435692dccea25fd
Insthync <ittipon.bay@gmail.com>
2022-01-03 18:30:55 +0700

    Serialize item amount data

--- Scripts/GameData/Item/ItemAmount.cs
+++ Scripts/GameData/Item/ItemAmount.cs
@@ -1,9 +1,28 @@
-﻿namespace MultiplayerARPG
+﻿using LiteNetLib.Utils;
+
+namespace MultiplayerARPG
 {
     [System.Serializable]
-    public struct ItemAmount
+    public struct ItemAmount : INetSerializable
     {
         public BaseItem item;
         public short amount;
+
+        public void SetItemByDataId(int dataId)
+        {
+            item = GameInstance.Items.ContainsKey(dataId) ? GameInstance.Items[dataId] : null;
+        }
+
+        public void Deserialize(NetDataReader reader)
+        {
+            SetItemByDataId(reader.GetPackedInt());
+            amount = reader.GetPackedShort();
+        }
+
+        public void Serialize(NetDataWriter writer)
+        {
+            writer.PutPackedInt(item != null ? item.DataId : 0);
+            writer.PutPackedShort(amount);
+        }
     }
 }
