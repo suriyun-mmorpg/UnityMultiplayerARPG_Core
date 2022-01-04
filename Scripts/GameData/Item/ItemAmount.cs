80555bcbfb6bae7ea813b438487b29bb364c9fe0
Insthync <ittipon.bay@gmail.com>
2022-01-04 09:24:48 +0700

    Delete serializing codes from item amount

--- Scripts/GameData/Item/ItemAmount.cs
+++ Scripts/GameData/Item/ItemAmount.cs
@@ -1,28 +1,9 @@
-﻿using LiteNetLib.Utils;
-
-namespace MultiplayerARPG
+﻿namespace MultiplayerARPG
 {
     [System.Serializable]
-    public struct ItemAmount : INetSerializable
+    public struct ItemAmount
     {
         public BaseItem item;
         public short amount;
-
-        public void SetItemByDataId(int dataId)
-        {
-            item = GameInstance.Items.ContainsKey(dataId) ? GameInstance.Items[dataId] : null;
-        }
-
-        public void Deserialize(NetDataReader reader)
-        {
-            SetItemByDataId(reader.GetPackedInt());
-            amount = reader.GetPackedShort();
-        }
-
-        public void Serialize(NetDataWriter writer)
-        {
-            writer.PutPackedInt(item != null ? item.DataId : 0);
-            writer.PutPackedShort(amount);
-        }
     }
 }
