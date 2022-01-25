e69bbc1b40ce7b88f2f4c9f16a7900bc1339a50a
Insthync <ittipon.bay@gmail.com>
2022-01-26 03:59:02 +0700

    Add update function to ICustomSummonManager

--- Scripts/GameInstance/ICustomSummonManager.cs
+++ Scripts/GameInstance/ICustomSummonManager.cs
@@ -4,5 +4,6 @@
     {
         BaseMonsterCharacterEntity GetPrefab(int dataId);
         void UnSummon(CharacterSummon characterSummon);
+        void Update(CharacterSummon characterSummon, float deltaTime);
     }
 }
