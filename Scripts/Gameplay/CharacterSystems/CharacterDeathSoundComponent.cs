c2e9517b62eddb01b30b84176a8f8028a0f22929
Insthync <ittipon.bay@gmail.com>
2022-06-09 18:25:46 +0700

    Change audio settings in `CharacterDeathSoundComponent`

--- Scripts/Gameplay/CharacterSystems/CharacterDeathSoundComponent.cs
+++ Scripts/Gameplay/CharacterSystems/CharacterDeathSoundComponent.cs
@@ -4,11 +4,43 @@ namespace MultiplayerARPG
 {
     public class CharacterDeathSoundComponent : BaseGameEntityComponent<BaseCharacterEntity>
     {
+        [System.Serializable]
+        public struct DeathSoundData
+        {
+            public AudioClip[] randomAudioClips;
+
+            public AudioClip GetRandomedAudioClip()
+            {
+                if (randomAudioClips == null || randomAudioClips.Length == 0)
+                    return null;
+                return randomAudioClips[Random.Range(0, randomAudioClips.Length)];
+            }
+        }
+
         public AudioSource audioSource;
+        public AudioComponentSettingType settingType = AudioComponentSettingType.Sfx;
+        public string otherSettingId;
         public DeathSoundData soundData;
-        [Range(0f, 1f)]
-        public float volume = 1f;
         private bool dirtyIsDead;
+
+        public string SettingId
+        {
+            get
+            {
+                switch (settingType)
+                {
+                    case AudioComponentSettingType.Master:
+                        return AudioManager.Singleton.masterVolumeSetting.id;
+                    case AudioComponentSettingType.Bgm:
+                        return AudioManager.Singleton.bgmVolumeSetting.id;
+                    case AudioComponentSettingType.Sfx:
+                        return AudioManager.Singleton.sfxVolumeSetting.id;
+                    case AudioComponentSettingType.Ambient:
+                        return AudioManager.Singleton.ambientVolumeSetting.id;
+                }
+                return otherSettingId;
+            }
+        }
 
         public override void EntityStart()
         {
@@ -42,21 +74,8 @@ namespace MultiplayerARPG
         public void PlaySound()
         {
             audioSource.clip = soundData.GetRandomedAudioClip();
-            audioSource.volume = volume;
+            audioSource.volume = AudioManager.Singleton.GetVolumeLevel(SettingId);
             audioSource.Play();
-        }
-    }
-
-    [System.Serializable]
-    public struct DeathSoundData
-    {
-        public AudioClip[] randomAudioClips;
-
-        public AudioClip GetRandomedAudioClip()
-        {
-            if (randomAudioClips == null || randomAudioClips.Length == 0)
-                return null;
-            return randomAudioClips[Random.Range(0, randomAudioClips.Length)];
         }
     }
 }
