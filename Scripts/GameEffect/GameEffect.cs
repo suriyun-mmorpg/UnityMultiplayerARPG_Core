681af52de386412de8ee3dcc70f58f4a43faf5e2
Insthync <ittipon.bay@gmail.com>
2022-06-24 19:06:40 +0700

    Add audio component setting to game effect

--- Scripts/GameEffect/GameEffect.cs
+++ Scripts/GameEffect/GameEffect.cs
@@ -6,6 +6,27 @@ namespace MultiplayerARPG
 {
     public class GameEffect : PoolDescriptor
     {
+        public AudioComponentSettingType settingType = AudioComponentSettingType.Sfx;
+        public string otherSettingId;
+        public string SettingId
+        {
+            get
+            {
+                switch (settingType)
+                {
+                    case AudioComponentSettingType.Master:
+                        return AudioManager.Singleton.masterVolumeSetting.id;
+                    case AudioComponentSettingType.Bgm:
+                        return AudioManager.Singleton.bgmVolumeSetting.id;
+                    case AudioComponentSettingType.Sfx:
+                        return AudioManager.Singleton.sfxVolumeSetting.id;
+                    case AudioComponentSettingType.Ambient:
+                        return AudioManager.Singleton.ambientVolumeSetting.id;
+                }
+                return otherSettingId;
+            }
+        }
+
         public string effectSocket;
         public bool isLoop;
         public float lifeTime;
@@ -106,7 +127,7 @@ namespace MultiplayerARPG
             destroyTime = isLoop ? -1 : Time.time + lifeTime;
             // Play random audio
             if (randomSoundEffects.Length > 0)
-                AudioManager.PlaySfxClipAtPoint(randomSoundEffects[Random.Range(0, randomSoundEffects.Length)], CacheTransform.position);
+                AudioSource.PlayClipAtPoint(randomSoundEffects[Random.Range(0, randomSoundEffects.Length)], CacheTransform.position, AudioManager.Singleton.GetVolumeLevel(SettingId));
             FxCollection.Play();
         }
     }
