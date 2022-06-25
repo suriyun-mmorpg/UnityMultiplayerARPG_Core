5d56c6a791546bd4c7f9514c95dee2a1fc9631fa
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-06-25 16:51:38 +0700

    Improve chat notification component

--- Scripts/UI/Chat/UIChatHandler.cs
+++ Scripts/UI/Chat/UIChatHandler.cs
@@ -27,8 +27,6 @@ namespace MultiplayerARPG
         [Range(0, UIChatHistory.MAX_CHAT_HISTORY)]
         public int chatEntrySize = 30;
         public EntryClickResponseMode entryClickResponseMode = EntryClickResponseMode.SetWhisterAsCommand;
-        public GameObject[] notificationObjects;
-        public TextWrapper[] notificationCountTexts;
 
         [Header("Channel Commands")]
         public string globalCommand = "/a";
@@ -117,46 +115,6 @@ namespace MultiplayerARPG
         private void OnDestroy()
         {
             RemoveOnClientReceiveChatMessage();
-        }
-
-        public void SetNotificationCount(int count)
-        {
-            if (notificationObjects != null && notificationObjects.Length > 0)
-            {
-                foreach (GameObject obj in notificationObjects)
-                {
-                    obj.SetActive(count > 0);
-                }
-            }
-            if (notificationCountTexts != null && notificationCountTexts.Length > 0)
-            {
-                foreach (TextWrapper txt in notificationCountTexts)
-                {
-                    if (count >= 99)
-                        txt.text = "99+";
-                    else if (count > 89)
-                        txt.text = "90+";
-                    else if (count > 79)
-                        txt.text = "80+";
-                    else if (count > 69)
-                        txt.text = "70+";
-                    else if (count > 59)
-                        txt.text = "60+";
-                    else if (count > 49)
-                        txt.text = "50+";
-                    else if (count > 39)
-                        txt.text = "40+";
-                    else if (count > 29)
-                        txt.text = "30+";
-                    else if (count > 19)
-                        txt.text = "20+";
-                    else if (count > 9)
-                        txt.text = "10+";
-                    else
-                        txt.text = count.ToString();
-                    txt.SetGameObjectActive(count > 0);
-                }
-            }
         }
 
         public void SetOnClientReceiveChatMessage()
@@ -309,11 +267,6 @@ namespace MultiplayerARPG
                 ChatMessages.RemoveAt(0);
 
             FillChatMessages();
-            if (!enabled)
-                notificationCount++;
-            else
-                notificationCount = 0;
-            SetNotificationCount(notificationCount);
         }
 
         private void FillChatMessages()
