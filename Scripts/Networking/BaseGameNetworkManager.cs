2a1e632d597782400f5b8f5ac04e717327531dc0
Insthync <ittipon.bay@gmail.com>
2022-07-12 06:44:57 +0700

    Increase packet version

--- Scripts/Networking/BaseGameNetworkManager.cs
+++ Scripts/Networking/BaseGameNetworkManager.cs
@@ -82,7 +82,7 @@ namespace MultiplayerARPG
 
         public override uint PacketVersion()
         {
-            return 34;
+            return 40;
         }
 
         protected override void Awake()
