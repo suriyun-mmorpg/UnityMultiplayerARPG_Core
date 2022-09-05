71c641e1d171592f8f87f50b6fe6f3e00993793d
Insthync <ittipon.bay@gmail.com>
2022-09-05 17:32:36 +0700

    Fix zstring nested usage (UICurrencyAmounts)

--- Scripts/UI/Currency/UICurrencyAmounts.cs
+++ Scripts/UI/Currency/UICurrencyAmounts.cs
@@ -67,7 +67,7 @@ namespace MultiplayerARPG
             }
             else
             {
-                using (Utf16ValueStringBuilder tempAllText = ZString.CreateStringBuilder(true))
+                using (Utf16ValueStringBuilder tempAllText = ZString.CreateStringBuilder(false))
                 {
                     Currency tempData;
                     int tempCurrentAmount;
