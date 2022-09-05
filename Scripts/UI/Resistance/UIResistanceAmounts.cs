dcb9fbfb33da13f13e8e0d41648315c0f7de8301
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-09-05 11:03:52 +0700

    Use `ZString` to improve performance (ResistanceUIs)

--- Scripts/UI/Resistance/UIResistanceAmounts.cs
+++ Scripts/UI/Resistance/UIResistanceAmounts.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.Text;
+using Cysharp.Text;
 using UnityEngine;
 
 namespace MultiplayerARPG
@@ -52,45 +52,47 @@ namespace MultiplayerARPG
             }
             else
             {
-                StringBuilder tempAllText = new StringBuilder();
-                DamageElement tempElement;
-                float tempAmount;
-                string tempValue;
-                string tempAmountText;
-                UIResistanceTextPair tempComponentPair;
-                foreach (KeyValuePair<DamageElement, float> dataEntry in Data)
+                using (Utf16ValueStringBuilder tempAllText = ZString.CreateStringBuilder(true))
                 {
-                    if (dataEntry.Key == null)
-                        continue;
-                    // Set temp data
-                    tempElement = dataEntry.Key;
-                    tempAmount = dataEntry.Value;
-                    // Set current elemental resistance text
-                    if (isBonus)
-                        tempValue = (tempAmount * 100).ToBonusString("N2");
-                    else
-                        tempValue = (tempAmount * 100).ToString("N2");
-                    tempAmountText = string.Format(
-                        LanguageManager.GetText(formatKeyAmount),
-                        tempElement.Title,
-                        tempValue);
-                    // Append current elemental resistance text
-                    if (dataEntry.Value != 0)
+                    DamageElement tempElement;
+                    float tempAmount;
+                    string tempValue;
+                    string tempAmountText;
+                    UIResistanceTextPair tempComponentPair;
+                    foreach (KeyValuePair<DamageElement, float> dataEntry in Data)
                     {
-                        // Add new line if text is not empty
-                        if (tempAllText.Length > 0)
-                            tempAllText.Append('\n');
-                        tempAllText.Append(tempAmountText);
+                        if (dataEntry.Key == null)
+                            continue;
+                        // Set temp data
+                        tempElement = dataEntry.Key;
+                        tempAmount = dataEntry.Value;
+                        // Set current elemental resistance text
+                        if (isBonus)
+                            tempValue = (tempAmount * 100).ToBonusString("N2");
+                        else
+                            tempValue = (tempAmount * 100).ToString("N2");
+                        tempAmountText = ZString.Format(
+                            LanguageManager.GetText(formatKeyAmount),
+                            tempElement.Title,
+                            tempValue);
+                        // Append current elemental resistance text
+                        if (dataEntry.Value != 0)
+                        {
+                            // Add new line if text is not empty
+                            if (tempAllText.Length > 0)
+                                tempAllText.Append('\n');
+                            tempAllText.Append(tempAmountText);
+                        }
+                        // Set current elemental resistance text to UI
+                        if (CacheTextAmounts.TryGetValue(dataEntry.Key, out tempComponentPair))
+                            tempComponentPair.uiText.text = tempAmountText;
                     }
-                    // Set current elemental resistance text to UI
-                    if (CacheTextAmounts.TryGetValue(dataEntry.Key, out tempComponentPair))
-                        tempComponentPair.uiText.text = tempAmountText;
-                }
 
-                if (uiTextAllAmounts != null)
-                {
-                    uiTextAllAmounts.SetGameObjectActive(tempAllText.Length > 0);
-                    uiTextAllAmounts.text = tempAllText.ToString();
+                    if (uiTextAllAmounts != null)
+                    {
+                        uiTextAllAmounts.SetGameObjectActive(tempAllText.Length > 0);
+                        uiTextAllAmounts.text = tempAllText.ToString();
+                    }
                 }
             }
         }
@@ -98,7 +100,7 @@ namespace MultiplayerARPG
         private void SetDefaultValue(UIResistanceTextPair componentPair)
         {
             DamageElement tempElement = componentPair.damageElement == null ? GameInstance.Singleton.DefaultDamageElement : componentPair.damageElement;
-            componentPair.uiText.text = string.Format(
+            componentPair.uiText.text = ZString.Format(
                     LanguageManager.GetText(formatKeyAmount),
                     tempElement.Title,
                     isBonus ? 0f.ToBonusString("N2") : 0f.ToString("N2"));
