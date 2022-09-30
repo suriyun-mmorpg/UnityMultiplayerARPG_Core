8995db591791db58d0b8656d8bccd431a05e415b
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-10-01 00:15:37 +0700

    Add icon, frame, title UIs

---
+++ Scripts/UI/Social/PlayerTitle/UIPlayerTitle.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MultiplayerARPG
+{
+    public partial class UIPlayerTitle : UISelectionEntry<PlayerTitle>
+    {
+        public Image imageIcon;
+        public GameObject[] lockedObjects = new GameObject[0];
+        public GameObject[] unlockedObjects = new GameObject[0];
+        public bool IsLocked { get; private set; }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            UpdateData();
+        }
+
+        protected override void UpdateData()
+        {
+            PlayerTitle icon = Data;
+            if (icon == null)
+                icon = GameInstance.PlayerTitles.Values.FirstOrDefault();
+            if (imageIcon != null)
+            {
+                Sprite iconSprite = icon == null ? null : icon.Icon;
+                imageIcon.gameObject.SetActive(iconSprite != null);
+                imageIcon.sprite = iconSprite;
+                imageIcon.preserveAspect = true;
+            }
+        }
+
+        public void SetDataByDataId(int dataId)
+        {
+            PlayerTitle guildIcon;
+            if (GameInstance.PlayerTitles.TryGetValue(dataId, out guildIcon))
+                Data = guildIcon;
+            else
+                Data = null;
+        }
+
+        public void SetIsLocked(bool isLocked)
+        {
+            foreach (GameObject lockedObject in lockedObjects)
+            {
+                lockedObject.SetActive(isLocked);
+            }
+            foreach (GameObject unlockedObject in unlockedObjects)
+            {
+                unlockedObject.SetActive(!isLocked);
+            }
+        }
+    }
+}
