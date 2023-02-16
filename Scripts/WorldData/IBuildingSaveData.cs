5aed73c066cc8745b10f793776c5d7a881553870
Insthync <ittipon.bay@gmail.com>
2023-02-16 13:32:08 +0700

    feat: gradually move world saves data codes to shared data repo

--- Scripts/WorldData/IBuildingSaveData.cs
+++ Scripts/WorldData/IBuildingSaveData.cs
@@ -1,17 +1,6 @@
-﻿using UnityEngine;
-
-public partial interface IBuildingSaveData
+﻿namespace MultiplayerARPG
 {
-    string Id { get; set; }
-    string ParentId { get; set; }
-    int EntityId { get; set; }
-    int CurrentHp { get; set; }
-    float RemainsLifeTime { get; set; }
-    bool IsLocked { get; set; }
-    string LockPassword { get; set; }
-    Vector3 Position { get; set; }
-    Quaternion Rotation { get; set; }
-    string CreatorId { get; set; }
-    string CreatorName { get; set; }
-    string ExtraData { get; set; }
+    public partial interface IBuildingSaveData
+    {
+    }
 }
