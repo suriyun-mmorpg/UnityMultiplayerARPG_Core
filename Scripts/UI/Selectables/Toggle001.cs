486cde0bcb20de4067e0956f038ce5c891b9e581
Insthync <ittipon.bay@gmail.com>
2023-02-25 22:30:12 +0700

    feat: Add new selectable components for console platforms demo

---
+++ Scripts/UI/Selectables/Toggle001.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MultiplayerARPG
+{
+    /// <summary>
+    /// Made this component for navigation demo, because built-in Unity's components not good enough
+    /// </summary>
+    public class Toggle001 : Toggle
+    {
+        public float selectedScaleDuration = 1f;
+        public float selectedScaling = 0.05f;
+        public bool selectDisabledSelectable = false;
+        private SelectionState _currentSelectionState;
+        private Vector3 _defaultLocalScale = Vector3.one;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _defaultLocalScale = transform.localScale;
+        }
+
+        protected override void DoStateTransition(SelectionState state, bool instant)
+        {
+            base.DoStateTransition(state, instant);
+            _currentSelectionState = state;
+        }
+
+        void Update()
+        {
+            if (_currentSelectionState == SelectionState.Selected)
+                transform.localScale = new Vector3(_defaultLocalScale.x + Mathf.PingPong(Time.time, selectedScaleDuration) * selectedScaling, _defaultLocalScale.y + Mathf.PingPong(Time.time, selectedScaleDuration) * selectedScaling, transform.localScale.z);
+            else
+                transform.localScale = _defaultLocalScale;
+        }
+
+        public override Selectable FindSelectableOnLeft()
+        {
+            Selectable selectable = base.FindSelectableOnLeft();
+            if (selectable != null && !selectable.interactable && !selectDisabledSelectable)
+                selectable = null;
+            return selectable;
+        }
+
+        public override Selectable FindSelectableOnRight()
+        {
+            Selectable selectable = base.FindSelectableOnRight();
+            if (selectable != null && !selectable.interactable && !selectDisabledSelectable)
+                selectable = null;
+            return selectable;
+        }
+
+        public override Selectable FindSelectableOnUp()
+        {
+            Selectable selectable = base.FindSelectableOnUp();
+            if (selectable != null && !selectable.interactable && !selectDisabledSelectable)
+                selectable = null;
+            return selectable;
+        }
+
+        public override Selectable FindSelectableOnDown()
+        {
+            Selectable selectable = base.FindSelectableOnDown();
+            if (selectable != null && !selectable.interactable && !selectDisabledSelectable)
+                selectable = null;
+            return selectable;
+        }
+    }
+}
