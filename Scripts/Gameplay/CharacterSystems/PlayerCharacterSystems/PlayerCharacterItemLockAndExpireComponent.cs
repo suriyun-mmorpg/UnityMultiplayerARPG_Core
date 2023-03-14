5eaf1591f4d4a4417228cfc9406ed465ff271757
Insthync <ittipon.bay@gmail.com>
2023-03-15 02:50:04 +0700

    feat: WIP item expiring system (item removing)

--- Scripts/Gameplay/CharacterSystems/PlayerCharacterSystems/PlayerCharacterItemLockAndExpireComponent.cs
+++ Scripts/Gameplay/CharacterSystems/PlayerCharacterSystems/PlayerCharacterItemLockAndExpireComponent.cs
@@ -26,11 +26,16 @@ namespace MultiplayerARPG
                 // Removing non-equip items if it should
                 long currentTime = System.DateTimeOffset.UtcNow.ToUnixTimeSeconds();
                 bool haveRemovedItems = false;
-                CharacterItem nonEquipItem;
+                CharacterItem tempItem;
                 for (int i = Entity.NonEquipItems.Count - 1; i >= 0; --i)
                 {
-                    nonEquipItem = Entity.NonEquipItems[i];
-                    if (nonEquipItem.ShouldRemove(currentTime))
+                    tempItem = Entity.NonEquipItems[i];
+                    if (tempItem.IsEmptySlot())
+                    {
+                        // Skip empty slot
+                        continue;
+                    }
+                    if (tempItem.ShouldRemove(currentTime))
                     {
                         if (CurrentGameInstance.IsLimitInventorySlot)
                             Entity.NonEquipItems[i] = CharacterItem.Empty;
@@ -38,17 +43,45 @@ namespace MultiplayerARPG
                             Entity.NonEquipItems.RemoveAt(i);
                         haveRemovedItems = true;
                     }
-                    else
+                    else if (tempItem.IsLocked())
                     {
-                        if (nonEquipItem.IsLocked())
-                        {
-                            nonEquipItem.Update(updatingTime);
-                            Entity.NonEquipItems[i] = nonEquipItem;
-                        }
+                        tempItem.Update(updatingTime);
+                        Entity.NonEquipItems[i] = tempItem;
                     }
                 }
                 if (haveRemovedItems)
+                {
                     Entity.FillEmptySlots();
+                }
+                for (int i = Entity.EquipItems.Count - 1; i >= 0; --i)
+                {
+                    tempItem = Entity.EquipItems[i];
+                    if (!tempItem.IsEmptySlot() && tempItem.ShouldRemove(currentTime))
+                    {
+                        Entity.EquipItems.RemoveAt(i);
+                    }
+                }
+                for (int i = Entity.SelectableWeaponSets.Count - 1; i >= 0; --i)
+                {
+                    bool hasUpdate = false;
+                    EquipWeapons tempEquipWeapons = Entity.SelectableWeaponSets[i];
+                    tempItem = tempEquipWeapons.leftHand;
+                    if (!tempItem.IsEmptySlot() && tempItem.ShouldRemove(currentTime))
+                    {
+                        tempEquipWeapons.leftHand = new CharacterItem();
+                        hasUpdate = true;
+                    }
+                    tempItem = tempEquipWeapons.rightHand;
+                    if (!tempItem.IsEmptySlot() && tempItem.ShouldRemove(currentTime))
+                    {
+                        tempEquipWeapons.rightHand = new CharacterItem();
+                        hasUpdate = true;
+                    }
+                    if (hasUpdate)
+                    {
+                        Entity.SelectableWeaponSets[i] = tempEquipWeapons;
+                    }
+                }
                 updatingTime = 0;
             }
         }
