c77d6815556f252051619f2e6001ccb493f4c77e
Insthync <ittipon.bay@gmail.com>
2023-03-20 18:11:29 +0700

    feat: set amount's min to 1

--- Scripts/GameData/Item/ItemDrop.cs
+++ Scripts/GameData/Item/ItemDrop.cs
@@ -7,8 +7,10 @@ namespace MultiplayerARPG
     public struct ItemDrop
     {
         public BaseItem item;
+        [Min(1)]
         public int minAmount;
         [FormerlySerializedAs("amount")]
+        [Min(1)]
         public int maxAmount;
         [Range(0f, 1f)]
         public float dropRate;
