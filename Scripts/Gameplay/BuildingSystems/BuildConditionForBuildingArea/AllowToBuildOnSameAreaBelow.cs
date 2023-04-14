dc5ce1c3900856fac88aa1c52217b3209c20bad2
Insthync <ittipon.bay@gmail.com>
2023-04-15 02:42:12 +0700

    improve: gradually change codes conventions

--- Scripts/Gameplay/BuildingSystems/BuildConditionForBuildingArea/AllowToBuildOnSameAreaBelow.cs
+++ Scripts/Gameplay/BuildingSystems/BuildConditionForBuildingArea/AllowToBuildOnSameAreaBelow.cs
@@ -7,17 +7,17 @@ namespace MultiplayerARPG
         public Vector3 raycastOriginOffsets = Vector3.up;
         public float raycastDistance = 10f;
         public LayerMask layerMask = 1;
-        private RaycastHit[] raycastHits = new RaycastHit[32];
+        private RaycastHit[] _raycastHits = new RaycastHit[32];
 
         public override bool AllowToBuild(BuildingArea sourceArea, BuildingEntity newBuilding)
         {
-            int hitCount = PhysicUtils.SortedRaycastNonAlloc3D(sourceArea.transform.position + raycastOriginOffsets, Vector3.down, raycastHits, raycastDistance, layerMask.value);
+            int hitCount = PhysicUtils.SortedRaycastNonAlloc3D(sourceArea.transform.position + raycastOriginOffsets, Vector3.down, _raycastHits, raycastDistance, layerMask.value);
             BuildingArea tempBuildingArea;
             for (int i = 0; i < hitCount; ++i)
             {
-                if (raycastHits[i].transform == sourceArea.transform)
+                if (_raycastHits[i].transform == sourceArea.transform)
                     continue;
-                tempBuildingArea = raycastHits[i].transform.GetComponent<BuildingArea>();
+                tempBuildingArea = _raycastHits[i].transform.GetComponent<BuildingArea>();
                 if (tempBuildingArea != null && newBuilding.BuildingTypes.Contains(tempBuildingArea.buildingType))
                     return true;
             }
