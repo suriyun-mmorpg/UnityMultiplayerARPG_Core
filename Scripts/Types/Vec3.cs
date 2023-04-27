6d3c4f9681b50aa4810955727ef2c048f324af15
Insthync <ittipon.bay@gmail.com>
2023-04-27 07:13:08 +0700

    feat: gradually move networking codes

--- Scripts/Types/Vec3.cs
+++ Scripts/Types/Vec3.cs
@@ -1,9 +1,8 @@
 using UnityEngine;
-using LiteNetLib.Utils;
 
 namespace MultiplayerARPG
 {
-    public partial struct Vec3 : INetSerializable
+    public partial struct Vec3
     {
         public static implicit operator Vec3(Vector3 value) { return new Vec3(value); }
         public static implicit operator Vector3(Vec3 value) { return new Vector3(value.x, value.y, value.z); }
@@ -13,20 +12,6 @@ namespace MultiplayerARPG
             x = vector3.x;
             y = vector3.y;
             z = vector3.z;
-        }
-
-        public void Serialize(NetDataWriter writer)
-        {
-            writer.Put(x);
-            writer.Put(y);
-            writer.Put(z);
-        }
-
-        public void Deserialize(NetDataReader reader)
-        {
-            x = reader.GetFloat();
-            y = reader.GetFloat();
-            z = reader.GetFloat();
         }
     }
 }
