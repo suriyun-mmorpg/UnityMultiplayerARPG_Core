55fb1efad016ed43b491d3fe286d59c2d006d7bb
Insthync <ittipon.bay@gmail.com>
2023-04-27 08:28:17 +0700

    feat: add generic utils to shared data repo

--- Scripts/Utils/GenericUtils.cs
+++ Scripts/Utils/GenericUtils.cs
@@ -1,11 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using Cysharp.Text;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.EventSystems;
@@ -196,45 +193,6 @@ public static class GenericUtils
         foreach (T component in components)
         {
             Object.DestroyImmediate(component);
-        }
-    }
-
-    public static string GetUniqueId(int length = 16, string mask = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-")
-    {
-        return Nanoid.Nanoid.Generate(mask, length);
-    }
-
-    public static string GetMD5(this string text)
-    {
-        // byte array representation of that string
-        byte[] encodedPassword = new UTF8Encoding().GetBytes(text);
-
-        // need MD5 to calculate the hash
-        byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
-
-        // string representation (similar to UNIX format)
-        return System.BitConverter.ToString(hash).Replace("-", string.Empty).ToLower();
-    }
-
-    public static int GenerateHashId(this string id)
-    {
-        if (string.IsNullOrEmpty(id))
-            return 0;
-
-        unchecked
-        {
-            int hash1 = 5381;
-            int hash2 = hash1;
-
-            for (int i = 0; i < id.Length && id[i] != '\0'; i += 2)
-            {
-                hash1 = ((hash1 << 5) + hash1) ^ id[i];
-                if (i == id.Length - 1 || id[i + 1] == '\0')
-                    break;
-                hash2 = ((hash2 << 5) + hash2) ^ id[i + 1];
-            }
-
-            return hash1 + (hash2 * 1566083941);
         }
     }
 
