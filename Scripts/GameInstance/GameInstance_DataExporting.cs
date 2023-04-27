8d3937e4f24aabd6035a364df422f9d389be4d26
Insthync <ittipon.bay@gmail.com>
2023-04-28 01:23:00 +0700

    feat: implement character creation data exporting

--- Scripts/GameInstance/GameInstance_DataExporting.cs
+++ Scripts/GameInstance/GameInstance_DataExporting.cs
@@ -23,18 +23,18 @@ namespace MultiplayerARPG
                 File.WriteAllText(path, JsonConvert.SerializeObject(socialSystemSetting, Formatting.Indented));
         }
 
-        [InspectorButton(nameof(ExportItemsAsJson))]
-        public bool exportItemsAsJson;
-        public void ExportItemsAsJson()
+        [InspectorButton(nameof(ExportMinimalItemsAsJson))]
+        public bool exportMinimalItemsAsJson;
+        public void ExportMinimalItemsAsJson()
         {
             ClearData();
-            onGameDataLoaded += OnGameDataLoadedToExportItemsAsJson;
+            onGameDataLoaded += OnGameDataLoadedToExportMinimalItemsAsJson;
             gameDatabase.LoadData(this).Forget();
         }
 
-        private void OnGameDataLoadedToExportItemsAsJson()
+        private void OnGameDataLoadedToExportMinimalItemsAsJson()
         {
-            onGameDataLoaded -= OnGameDataLoadedToExportItemsAsJson;
+            onGameDataLoaded -= OnGameDataLoadedToExportMinimalItemsAsJson;
             Dictionary<int, Dictionary<string, object>> exportingItems = new Dictionary<int, Dictionary<string, object>>();
             foreach (var kv in Items)
             {
@@ -46,17 +46,100 @@ namespace MultiplayerARPG
                     { "SellPrice", kv.Value.SellPrice },
                     { "Weight", kv.Value.Weight },
                     { "MaxStack", kv.Value.MaxStack },
+                    { "MaxLevel", kv.Value.MaxLevel },
                     { "LockDuration", kv.Value.LockDuration },
                     { "ExpireDuration", kv.Value.ExpireDuration },
                 };
-            }
-            string path = EditorUtility.SaveFilePanel("Export Items", Application.dataPath, "items", "json");
-            if (path.Length > 0)
-                File.WriteAllText(path, JsonConvert.SerializeObject(exportingItems, Formatting.Indented, new JsonSerializerSettings()
+                if (kv.Value.IsEquipment())
                 {
-                    NullValueHandling = NullValueHandling.Ignore,
-                    ContractResolver = new GameDataContractResolver(),
-                }));
+                    exportingItems[kv.Key]["MaxDurability"] = (kv.Value as IEquipmentItem).MaxDurability;
+                    exportingItems[kv.Key]["DestroyIfBroken"] = (kv.Value as IEquipmentItem).DestroyIfBroken;
+                    exportingItems[kv.Key]["MaxSocket"] = (kv.Value as IEquipmentItem).MaxSocket;
+                }
+                if (kv.Value.IsWeapon())
+                {
+                    exportingItems[kv.Key]["AmmoCapacity"] = (kv.Value as IWeaponItem).AmmoCapacity;
+                }
+            }
+            string path = EditorUtility.SaveFilePanel("Export Minimal Items", Application.dataPath, "items", "json");
+            if (path.Length > 0)
+                File.WriteAllText(path, JsonConvert.SerializeObject(exportingItems, Formatting.Indented));
+        }
+
+        [InspectorButton(nameof(ExportCharacterCreationDataAsJson))]
+        public bool exportCharacterCreationDataAsJson;
+        public void ExportCharacterCreationDataAsJson()
+        {
+            ClearData();
+            onGameDataLoaded += OnGameDataLoadedToExportCharacterCreationDataAsJson;
+            gameDatabase.LoadData(this).Forget();
+        }
+
+        private void OnGameDataLoadedToExportCharacterCreationDataAsJson()
+        {
+            onGameDataLoaded -= OnGameDataLoadedToExportCharacterCreationDataAsJson;
+
+            Singleton = this;
+
+            DefaultArmorType = ScriptableObject.CreateInstance<ArmorType>()
+                .GenerateDefaultArmorType();
+
+            DefaultWeaponType = ScriptableObject.CreateInstance<WeaponType>()
+                .GenerateDefaultWeaponType();
+
+            bool haveToClearDefaultWeaponItem = false;
+            // Setup default weapon item if not existed
+            if (defaultWeaponItem == null || !defaultWeaponItem.IsWeapon())
+            {
+                defaultWeaponItem = ScriptableObject.CreateInstance<Item>()
+                    .GenerateDefaultItem(DefaultWeaponType);
+                haveToClearDefaultWeaponItem = true;
+            }
+
+            bool haveToClearDefaultDamageElement = false;
+            // Setup default damage element if not existed
+            if (defaultDamageElement == null)
+            {
+                defaultDamageElement = ScriptableObject.CreateInstance<DamageElement>()
+                    .GenerateDefaultDamageElement(DefaultDamageHitEffects);
+                haveToClearDefaultDamageElement = true;
+            }
+
+            CharacterCreationData data = new CharacterCreationData();
+            foreach (var kv in PlayerCharacterEntities)
+            {
+                if (kv.Value.CharacterDatabases == null || kv.Value.CharacterDatabases.Length == 0)
+                    continue;
+                if (!data.AvailableCharacters.ContainsKey(kv.Key))
+                    data.AvailableCharacters[kv.Key] = new Dictionary<int, PlayerCharacterData>();
+                foreach (var database in kv.Value.CharacterDatabases)
+                {
+                    data.AvailableCharacters[kv.Key][database.DataId] = new PlayerCharacterData().SetNewPlayerCharacterData(string.Empty, database.DataId, kv.Key, 0);
+                }
+            }
+            data.AvailableFactionIds.AddRange(Factions.Keys);
+
+            DestroyImmediate(DefaultArmorType);
+            DefaultArmorType = null;
+
+            DestroyImmediate(DefaultWeaponType);
+            DefaultWeaponType = null;
+
+            if (haveToClearDefaultWeaponItem)
+            {
+                DestroyImmediate(defaultWeaponItem);
+                defaultWeaponItem = null;
+            }
+
+            if (haveToClearDefaultDamageElement)
+            {
+                DestroyImmediate(defaultDamageElement);
+                defaultDamageElement = null;
+            }
+
+            string path = EditorUtility.SaveFilePanel("Export Character Creation Data", Application.dataPath, "characterCreationData", "json");
+            if (path.Length > 0)
+                File.WriteAllText(path, JsonConvert.SerializeObject(data, Formatting.Indented));
         }
     }
 }
