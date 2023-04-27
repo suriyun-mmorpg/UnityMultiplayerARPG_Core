d49e589209c8b4e522fc3bec51710733d6094293
Insthync <ittipon.bay@gmail.com>
2023-04-27 07:53:50 +0700

    feat: gradually move networking codes

--- Scripts/CharacterData/CharacterDataExtensions.cs
+++ Scripts/CharacterData/CharacterDataExtensions.cs
@@ -6,13 +6,6 @@ namespace MultiplayerARPG
 {
     public static partial class CharacterDataExtensions
     {
-        public static System.Type ClassType { get; private set; }
-
-        static CharacterDataExtensions()
-        {
-            ClassType = typeof(CharacterDataExtensions);
-        }
-
         public static BaseCharacter GetDatabase(this ICharacterData data)
         {
             if (data == null || data.DataId == 0)
@@ -1378,110 +1371,6 @@ namespace MultiplayerARPG
             return false;
         }
         #endregion
-
-        public static List<EquipWeapons> Clone(this IList<EquipWeapons> src)
-        {
-            List<EquipWeapons> result = new List<EquipWeapons>();
-            for (int i = 0; i < src.Count; ++i)
-            {
-                result.Add(new EquipWeapons()
-                {
-                    rightHand = src[i].rightHand.Clone(),
-                    leftHand = src[i].leftHand.Clone(),
-                });
-            }
-            return result;
-        }
-
-        public static List<CharacterAttribute> Clone(this IList<CharacterAttribute> src)
-        {
-            List<CharacterAttribute> result = new List<CharacterAttribute>();
-            for (int i = 0; i < src.Count; ++i)
-            {
-                result.Add(src[i].Clone());
-            }
-            return result;
-        }
-
-        public static List<CharacterBuff> Clone(this IList<CharacterBuff> src)
-        {
-            List<CharacterBuff> result = new List<CharacterBuff>();
-            for (int i = 0; i < src.Count; ++i)
-            {
-                result.Add(src[i].Clone());
-            }
-            return result;
-        }
-
-        public static List<CharacterHotkey> Clone(this IList<CharacterHotkey> src)
-        {
-            List<CharacterHotkey> result = new List<CharacterHotkey>();
-            for (int i = 0; i < src.Count; ++i)
-            {
-                result.Add(src[i].Clone());
-            }
-            return result;
-        }
-
-        public static List<CharacterQuest> Clone(this IList<CharacterQuest> src)
-        {
-            List<CharacterQuest> result = new List<CharacterQuest>();
-            for (int i = 0; i < src.Count; ++i)
-            {
-                result.Add(src[i].Clone());
-            }
-            return result;
-        }
-
-        public static List<CharacterCurrency> Clone(this IList<CharacterCurrency> src)
-        {
-            List<CharacterCurrency> result = new List<CharacterCurrency>();
-            for (int i = 0; i < src.Count; ++i)
-            {
-                result.Add(src[i].Clone());
-            }
-            return result;
-        }
-
-        public static List<CharacterItem> Clone(this IList<CharacterItem> src)
-        {
-            List<CharacterItem> result = new List<CharacterItem>();
-            for (int i = 0; i < src.Count; ++i)
-            {
-                result.Add(src[i].Clone());
-            }
-            return result;
-        }
-
-        public static List<CharacterSkill> Clone(this IList<CharacterSkill> src)
-        {
-            List<CharacterSkill> result = new List<CharacterSkill>();
-            for (int i = 0; i < src.Count; ++i)
-            {
-                result.Add(src[i].Clone());
-            }
-            return result;
-        }
-
-        public static List<CharacterSkillUsage> Clone(this IList<CharacterSkillUsage> src)
-        {
-            List<CharacterSkillUsage> result = new List<CharacterSkillUsage>();
-            for (int i = 0; i < src.Count; ++i)
-            {
-                result.Add(src[i].Clone());
-            }
-            return result;
-        }
-
-        public static List<CharacterSummon> Clone(this IList<CharacterSummon> src)
-        {
-            List<CharacterSummon> result = new List<CharacterSummon>();
-            for (int i = 0; i < src.Count; ++i)
-            {
-                result.Add(src[i].Clone());
-            }
-            return result;
-        }
 
         public static bool HasOneInNonEquipItems(this ICharacterData data, int dataId)
         {
