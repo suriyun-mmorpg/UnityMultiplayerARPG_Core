0323d97602248f4a7c24bcede5e338518b914983
Insthync <ittipon.bay@gmail.com>
2023-05-07 06:33:12 +0700

    improve: reduce updating

--- Scripts/Gameplay/CharacterEntity/CharacterAllianceIndicator.cs
+++ Scripts/Gameplay/CharacterEntity/CharacterAllianceIndicator.cs
@@ -18,61 +18,116 @@ namespace MultiplayerARPG
         [Tooltip("This will activate when character is neutral with owning character")]
         public GameObject neutralIndicator;
         public float updateWithinRange = 30f;
-        public float updateRepeatRate = 0.5f;
+        public float updateRepeatDelay = 5f;
 
         private BaseCharacterEntity _characterEntity;
-        private float _lastUpdateTime;
+        private BasePlayerCharacterEntity _previousEntity;
 
-        private void Awake()
+        private void Start()
         {
             _characterEntity = GetComponentInParent<BaseCharacterEntity>();
+            GameInstance.onSetPlayingCharacter += GameInstance_onSetPlayingCharacter;
+            if (GameInstance.PlayingCharacterEntity != null)
+                GameInstance_onSetPlayingCharacter(GameInstance.PlayingCharacterEntity);
         }
 
-        private void Update()
+        private void OnDestroy()
         {
-            if (_characterEntity == null || !_characterEntity.IsClient || (_characterEntity.IsServer && _characterEntity.Identity.CountSubscribers() == 0) ||
-                GameInstance.PlayingCharacterEntity == null || Vector3.Distance(_characterEntity.EntityTransform.position, GameInstance.PlayingCharacterEntity.EntityTransform.position) > updateWithinRange)
+            GameInstance.onSetPlayingCharacter -= GameInstance_onSetPlayingCharacter;
+            RemoveEvents(_previousEntity);
+        }
+
+        private void GameInstance_onSetPlayingCharacter(IPlayerCharacterData playingCharacterData)
+        {
+            RemoveEvents(_previousEntity);
+            BasePlayerCharacterEntity playerCharacterEntity = playingCharacterData as BasePlayerCharacterEntity;
+            _previousEntity = playerCharacterEntity;
+            AddEvents(_previousEntity);
+            SetupUpdating();
+        }
+
+        private void AddEvents(BasePlayerCharacterEntity PlayingCharacterEntity)
+        {
+            if (PlayingCharacterEntity == null)
+                return;
+            PlayingCharacterEntity.onPartyIdChange += PlayingCharacterEntity_onPartyIdChange;
+            PlayingCharacterEntity.onGuildIdChange += PlayingCharacterEntity_onGuildIdChange;
+            PlayingCharacterEntity.onFactionIdChange += PlayingCharacterEntity_onFactionIdChange;
+        }
+
+        private void RemoveEvents(BasePlayerCharacterEntity PlayingCharacterEntity)
+        {
+            if (PlayingCharacterEntity == null)
+                return;
+            PlayingCharacterEntity.onPartyIdChange -= PlayingCharacterEntity_onPartyIdChange;
+            PlayingCharacterEntity.onGuildIdChange -= PlayingCharacterEntity_onGuildIdChange;
+            PlayingCharacterEntity.onFactionIdChange -= PlayingCharacterEntity_onFactionIdChange;
+        }
+
+        private void PlayingCharacterEntity_onPartyIdChange(int obj)
+        {
+            SetupUpdating();
+        }
+
+        private void PlayingCharacterEntity_onGuildIdChange(int obj)
+        {
+            SetupUpdating();
+        }
+
+        private void PlayingCharacterEntity_onFactionIdChange(int obj)
+        {
+            SetupUpdating();
+        }
+
+        private void SetupUpdating()
+        {
+            CancelInvoke(nameof(UpdateIndicator));
+            InvokeRepeating(nameof(UpdateIndicator), 0, updateRepeatDelay);
+        }
+
+        private void UpdateIndicator()
+        {
+            if (_characterEntity == null)
             {
-                HideAll();
+                DestroyImmediate(this);
                 return;
             }
 
-            if (Time.unscaledTime - _lastUpdateTime >= updateRepeatRate)
+            HideAll();
+            if (!_characterEntity.IsClient || (_characterEntity.IsServer && _characterEntity.Identity.CountSubscribers() == 0) || GameInstance.PlayingCharacterEntity == null || Vector3.Distance(_characterEntity.EntityTransform.position, GameInstance.PlayingCharacterEntity.EntityTransform.position) > updateWithinRange)
+                return;
+
+            EntityInfo entityInfo = _characterEntity.GetInfo();
+            List<GameObject> showingObjects = new List<GameObject>();
+            bool isShowing;
+
+            isShowing = GameInstance.PlayingCharacterEntity == _characterEntity;
+            if (owningIndicator != null && isShowing && !showingObjects.Contains(owningIndicator))
+                showingObjects.Add(owningIndicator);
+
+            isShowing = _characterEntity.IsAlly(GameInstance.PlayingCharacterEntity.GetInfo());
+            if (allyIndicator != null && isShowing && !showingObjects.Contains(allyIndicator))
+                showingObjects.Add(allyIndicator);
+
+            isShowing = entityInfo.PartyId > 0 && entityInfo.PartyId == GameInstance.PlayingCharacter.PartyId;
+            if (partyMemberIndicator != null && isShowing && !showingObjects.Contains(partyMemberIndicator))
+                showingObjects.Add(partyMemberIndicator);
+
+            isShowing = entityInfo.GuildId > 0 && entityInfo.GuildId == GameInstance.PlayingCharacter.GuildId;
+            if (guildMemberIndicator != null && isShowing && !showingObjects.Contains(guildMemberIndicator))
+                showingObjects.Add(guildMemberIndicator);
+
+            isShowing = _characterEntity.IsEnemy(GameInstance.PlayingCharacterEntity.GetInfo());
+            if (enemyIndicator != null && isShowing && !showingObjects.Contains(enemyIndicator))
+                showingObjects.Add(enemyIndicator);
+
+            isShowing = _characterEntity.IsNeutral(GameInstance.PlayingCharacterEntity.GetInfo());
+            if (neutralIndicator != null && isShowing && !showingObjects.Contains(neutralIndicator))
+                showingObjects.Add(neutralIndicator);
+
+            for (int i = 0; i < showingObjects.Count; ++i)
             {
-                _lastUpdateTime = Time.unscaledTime;
-                HideAll();
-                EntityInfo entityInfo = _characterEntity.GetInfo();
-                List<GameObject> showingObjects = new List<GameObject>();
-                bool isShowing;
-
-                isShowing = GameInstance.PlayingCharacterEntity == _characterEntity;
-                if (owningIndicator != null && isShowing && !showingObjects.Contains(owningIndicator))
-                    showingObjects.Add(owningIndicator);
-
-                isShowing = _characterEntity.IsAlly(GameInstance.PlayingCharacterEntity.GetInfo());
-                if (allyIndicator != null && isShowing && !showingObjects.Contains(allyIndicator))
-                    showingObjects.Add(allyIndicator);
-
-                isShowing = entityInfo.PartyId > 0 && entityInfo.PartyId == GameInstance.PlayingCharacter.PartyId;
-                if (partyMemberIndicator != null && isShowing && !showingObjects.Contains(partyMemberIndicator))
-                    showingObjects.Add(partyMemberIndicator);
-
-                isShowing = entityInfo.GuildId > 0 && entityInfo.GuildId == GameInstance.PlayingCharacter.GuildId;
-                if (guildMemberIndicator != null && isShowing && !showingObjects.Contains(guildMemberIndicator))
-                    showingObjects.Add(guildMemberIndicator);
-
-                isShowing = _characterEntity.IsEnemy(GameInstance.PlayingCharacterEntity.GetInfo());
-                if (enemyIndicator != null && isShowing && !showingObjects.Contains(enemyIndicator))
-                    showingObjects.Add(enemyIndicator);
-
-                isShowing = _characterEntity.IsNeutral(GameInstance.PlayingCharacterEntity.GetInfo());
-                if (neutralIndicator != null && isShowing && !showingObjects.Contains(neutralIndicator))
-                    showingObjects.Add(neutralIndicator);
-
-                for (int i = 0; i < showingObjects.Count; ++i)
-                {
-                    showingObjects[i].SetActive(true);
-                }
+                showingObjects[i].SetActive(true);
             }
         }
 
