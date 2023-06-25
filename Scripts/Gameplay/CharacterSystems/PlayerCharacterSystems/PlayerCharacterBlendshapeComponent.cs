f950d9570593895fac3db1fd7742105dda33e2d6
Insthync <ittipon.bay@gmail.com>
2023-06-25 12:09:26 +0700

    feat: WIP character blendshape customization system

--- Scripts/Gameplay/CharacterSystems/PlayerCharacterSystems/PlayerCharacterBlendshapeComponent.cs
+++ Scripts/Gameplay/CharacterSystems/PlayerCharacterSystems/PlayerCharacterBlendshapeComponent.cs
@@ -1,3 +1,4 @@
+using Cysharp.Text;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,19 +7,20 @@ namespace MultiplayerARPG
     public class PlayerCharacterBlendshapeComponent : BaseGameEntityComponent<BasePlayerCharacterEntity>
     {
         [System.Serializable]
-        public struct Setting
+        public struct BlendshapeOption
         {
-            public string settingId;
             public string blendshapeName;
             [Range(0f, 100f)]
             public float defaultValue;
         }
 
+        public string settingIdPrefix;
         public SkinnedMeshRenderer skinnedMeshRenderer;
-        public Setting[] settings = new Setting[0];
+        public BlendshapeOption[] options = new BlendshapeOption[0];
         private bool _setupProperly;
         private bool _applying;
         private float[] _currentValues;
+        private Dictionary<int, BlendshapeOption> _optionsByHashedId = new Dictionary<int, BlendshapeOption>();
         private Dictionary<string, int> _blendshapeIndexesByName = new Dictionary<string, int>();
 
         public override void EntityAwake()
@@ -27,10 +29,11 @@ namespace MultiplayerARPG
                 return;
             _applying = true;
             _setupProperly = true;
-            _currentValues = new float[settings.Length];
-            for (int i = 0; i < settings.Length; ++i)
+            _currentValues = new float[options.Length];
+            for (int i = 0; i < options.Length; ++i)
             {
-                _currentValues[i] = settings[i].defaultValue;
+                _currentValues[i] = options[i].defaultValue;
+                _optionsByHashedId[GetHashedSettingId(options[i])] = options[i];
             }
             for (int i = 0; i < skinnedMeshRenderer.sharedMesh.blendShapeCount; ++i)
             {
@@ -47,13 +50,19 @@ namespace MultiplayerARPG
             }
         }
 
+        /// <summary>
+        /// This function should be called by server or being called in character creation only, it is not allow client to set custom data.
+        /// </summary>
+        /// <param name="optionIndex"></param>
+        /// <param name="value"></param>
         public void SetData(int optionIndex, float value)
         {
             if (!_setupProperly || optionIndex < 0 || optionIndex >= _currentValues.Length)
                 return;
             _currentValues[optionIndex] = value;
             _applying = true;
-            // TODO: Save to entity's `PublicFloats`
+            // Save to entity's `PublicFloats`
+            Entity.SetPublicFloat32(GetHashedSettingId(options[optionIndex]), value);
         }
 
         public float GetData(int optionIndex)
@@ -65,12 +74,22 @@ namespace MultiplayerARPG
 
         public void Apply()
         {
-            for (int i = 0; i < settings.Length; ++i)
+            for (int i = 0; i < options.Length; ++i)
             {
-                if (!_blendshapeIndexesByName.TryGetValue(settings[i].blendshapeName, out int blendshapeIndex))
+                if (!_blendshapeIndexesByName.TryGetValue(options[i].blendshapeName, out int blendshapeIndex))
                     continue;
                 skinnedMeshRenderer.SetBlendShapeWeight(blendshapeIndex, _currentValues[i]);
             }
+        }
+
+        public string GetSettingId(BlendshapeOption option)
+        {
+            return ZString.Concat(settingIdPrefix, option.blendshapeName);
+        }
+
+        public int GetHashedSettingId(BlendshapeOption option)
+        {
+            return GetSettingId(option).GenerateHashId();
         }
     }
 }
