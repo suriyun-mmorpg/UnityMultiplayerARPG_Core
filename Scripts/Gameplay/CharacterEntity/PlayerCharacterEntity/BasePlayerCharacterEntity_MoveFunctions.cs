a0f966218b10cc68b00dc39b180337078af33c00
Insthync <ittipon.bay@gmail.com>
2023-07-17 21:31:20 +0700

    feat: don't allow to move while private store is opened

---
+++ Scripts/Gameplay/CharacterEntity/PlayerCharacterEntity/BasePlayerCharacterEntity_MoveFunctions.cs
@@ -0,0 +1,54 @@
+namespace MultiplayerARPG
+{
+    public partial class BasePlayerCharacterEntity
+    {
+        protected override bool CanMove_Implementation()
+        {
+            if (PrivateStore.Data.isOpen)
+                return false;
+            return base.CanMove_Implementation();
+        }
+
+        protected override bool CanSprint_Implementation()
+        {
+            if (PrivateStore.Data.isOpen)
+                return false;
+            return base.CanSprint_Implementation();
+        }
+
+        protected override bool CanWalk_Implementation()
+        {
+            if (PrivateStore.Data.isOpen)
+                return false;
+            return base.CanWalk_Implementation();
+        }
+
+        protected override bool CanCrouch_Implementation()
+        {
+            if (PrivateStore.Data.isOpen)
+                return false;
+            return base.CanCrouch_Implementation();
+        }
+
+        protected override bool CanCrawl_Implementation()
+        {
+            if (PrivateStore.Data.isOpen)
+                return false;
+            return base.CanCrawl_Implementation();
+        }
+
+        protected override bool CanJump_Implementation()
+        {
+            if (PrivateStore.Data.isOpen)
+                return false;
+            return base.CanJump_Implementation();
+        }
+
+        protected override bool CanTurn_Implementation()
+        {
+            if (PrivateStore.Data.isOpen)
+                return false;
+            return base.CanTurn_Implementation();
+        }
+    }
+}
