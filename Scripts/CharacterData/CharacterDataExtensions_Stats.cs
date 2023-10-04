f46063465793c074fd80d274cb0395d2d2dfd340
Insthync <ittipon.bay@gmail.com>
2023-10-04 07:56:56 +0700

    fix: buff stats rate not being calculated

--- Scripts/CharacterData/CharacterDataExtensions_Stats.cs
+++ Scripts/CharacterData/CharacterDataExtensions_Stats.cs
@@ -852,7 +852,7 @@ namespace MultiplayerARPG
             {
                 resultStats += resultAttributes.GetStats();
                 resultStats += buffStats;
-                resultStats *= buffStatsRate;
+                resultStats += buffStats * buffStatsRate;
                 if (onGetStats != null)
                     onGetStats.Invoke(resultStats);
             }
