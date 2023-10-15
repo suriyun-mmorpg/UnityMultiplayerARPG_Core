558a00a54108cff559cc0b182de49e6d427160d5
Insthync <ittipon.bay@gmail.com>
2023-10-16 00:59:58 +0700

    improve: change execution order

--- Scripts/Consts/DefaultExecutionOrders.cs
+++ Scripts/Consts/DefaultExecutionOrders.cs
@@ -5,12 +5,12 @@ namespace MultiplayerARPG
         public const int GAME_INSTANCE = int.MinValue;
 
         // Game Entity
-        public const int BASE_GAME_ENTITY = 2;
+        public const int BASE_GAME_ENTITY = 0;
         public const int CHARACTER_MODEL_MANAGER = 100;
         public const int GAME_ENTITY_MODEL = 101;
 
         // Controller
-        public const int PLAYER_CHARACTER_CONTROLLER = 1;
+        public const int PLAYER_CHARACTER_CONTROLLER = -1;
 
         // IK
         public const int CHARACTER_ALIGN_ON_GROUND = -105;
