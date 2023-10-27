91f73b675ab3c891316d5bc2a8e2ba8de7b6d051
Insthync <ittipon.bay@gmail.com>
2023-10-28 01:28:06 +0700

    improve: avoid data missing problems

--- Scripts/GameData/CashShop/CashShopDatabase.cs
+++ Scripts/GameData/CashShop/CashShopDatabase.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace MultiplayerARPG
 {
     [CreateAssetMenu(fileName = GameDataMenuConsts.CASH_SHOP_DATABASE_FILE, menuName = GameDataMenuConsts.CASH_SHOP_DATABASE_MENU, order = GameDataMenuConsts.CASH_SHOP_DATABASE_ORDER)]
     public class CashShopDatabase : ScriptableObject
     {
+        [FormerlySerializedAs("cashStopItems")]
         public CashShopItem[] cashShopItems;
         public CashPackage[] cashPackages;
     }
