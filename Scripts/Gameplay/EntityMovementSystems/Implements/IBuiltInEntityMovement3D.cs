fcab7862400bd03ffbdbaaf7555f9c7360da2f84
Callepo <clifford.agata@hotmail.com>
2023-11-14 23:29:46 +0100

    Partial Classes

--- Scripts/Gameplay/EntityMovementSystems/Implements/IBuiltInEntityMovement3D.cs
+++ Scripts/Gameplay/EntityMovementSystems/Implements/IBuiltInEntityMovement3D.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace MultiplayerARPG
 {
-    public interface IBuiltInEntityMovement3D
+    public partial interface IBuiltInEntityMovement3D
     {
         bool GroundCheck();
         void Move(Vector3 motion);
