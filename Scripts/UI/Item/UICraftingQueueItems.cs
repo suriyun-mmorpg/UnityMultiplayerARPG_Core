328a1eb5dca62927ec0f7a70189273c848721526
Insthync <ittipon.bay@gmail.com>
2023-11-27 07:22:22 +0700

    improve: change naming conventions

--- Scripts/UI/Item/UICraftingQueueItems.cs
+++ Scripts/UI/Item/UICraftingQueueItems.cs
@@ -16,30 +16,30 @@ namespace MultiplayerARPG
 
         public ICraftingQueueSource Source { get; set; }
 
-        private UIList cacheList;
+        private UIList _cacheList;
         public UIList CacheList
         {
             get
             {
-                if (cacheList == null)
+                if (_cacheList == null)
                 {
-                    cacheList = gameObject.AddComponent<UIList>();
-                    cacheList.uiPrefab = uiPrefab.gameObject;
-                    cacheList.uiContainer = uiContainer;
+                    _cacheList = gameObject.AddComponent<UIList>();
+                    _cacheList.uiPrefab = uiPrefab.gameObject;
+                    _cacheList.uiContainer = uiContainer;
                 }
-                return cacheList;
+                return _cacheList;
             }
         }
 
-        private UICraftingQueueItemSelectionManager cacheSelectionManager;
+        private UICraftingQueueItemSelectionManager _cacheSelectionManager;
         public UICraftingQueueItemSelectionManager CacheSelectionManager
         {
             get
             {
-                if (cacheSelectionManager == null)
-                    cacheSelectionManager = gameObject.GetOrAddComponent<UICraftingQueueItemSelectionManager>();
-                cacheSelectionManager.selectionMode = UISelectionMode.SelectSingle;
-                return cacheSelectionManager;
+                if (_cacheSelectionManager == null)
+                    _cacheSelectionManager = gameObject.GetOrAddComponent<UICraftingQueueItemSelectionManager>();
+                _cacheSelectionManager.selectionMode = UISelectionMode.SelectSingle;
+                return _cacheSelectionManager;
             }
         }
 
