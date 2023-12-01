502a38603c2df6caa27cdc26eccb2adec8a48bac
Insthync <ittipon.bay@gmail.com>
2023-12-02 06:44:59 +0700

    feat: add more UI elements to UI buff

--- Scripts/UI/Buff/UIBuff.cs
+++ Scripts/UI/Buff/UIBuff.cs
@@ -22,6 +22,18 @@ namespace MultiplayerARPG
         public UILocaleKeySetting formatKeyRecoveryFood = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_BUFF_RECOVERY_FOOD);
         [Tooltip("Format => {0} = {Buff Recovery Water}")]
         public UILocaleKeySetting formatKeyRecoveryWater = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_BUFF_RECOVERY_WATER);
+        [Tooltip("Format => {0} = {Max Stack}")]
+        public UILocaleKeySetting formatKeyMaxStack = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_BUFF_MAX_STACK);
+        [Tooltip("Format => {0} = {Chance * 100}")]
+        public UILocaleKeySetting formatKeyRemoveBuffWhenAttackChance = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_BUFF_REMOVE_BUFF_WHEN_ATTACK_CHANCE);
+        [Tooltip("Format => {0} = {Chance * 100}")]
+        public UILocaleKeySetting formatKeyRemoveBuffWhenAttackedChance = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_BUFF_REMOVE_BUFF_WHEN_ATTACKED_CHANCE);
+        [Tooltip("Format => {0} = {Chance * 100}")]
+        public UILocaleKeySetting formatKeyRemoveBuffWhenUseSkillChance = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_BUFF_REMOVE_BUFF_USE_SKILL_CHANCE);
+        [Tooltip("Format => {0} = {Chance * 100}")]
+        public UILocaleKeySetting formatKeyRemoveBuffWhenUseItemChance = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_BUFF_REMOVE_BUFF_USE_ITEM_CHANCE);
+        [Tooltip("Format => {0} = {Chance * 100}")]
+        public UILocaleKeySetting formatKeyRemoveBuffWhenPickupItemChance = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_BUFF_REMOVE_BUFF_PICKUP_ITEM_CHANCE);
 
         [Header("UI Elements")]
         public TextWrapper uiTextDuration;
@@ -30,6 +42,12 @@ namespace MultiplayerARPG
         public TextWrapper uiTextRecoveryStamina;
         public TextWrapper uiTextRecoveryFood;
         public TextWrapper uiTextRecoveryWater;
+        public TextWrapper uiTextMaxStack;
+        public TextWrapper uiTextRemoveBuffWhenAttackChance;
+        public TextWrapper uiTextRemoveBuffWhenAttackedChance;
+        public TextWrapper uiTextRemoveBuffWhenUseSkillChance;
+        public TextWrapper uiTextRemoveBuffWhenUseItemChance;
+        public TextWrapper uiTextRemoveBuffWhenPickupItemChance;
         public UICharacterStats uiBuffStats;
         public UICharacterStats uiBuffStatsRate;
         public UIAttributeAmounts uiBuffAttributes;
@@ -62,61 +80,239 @@ namespace MultiplayerARPG
         public GameObject disallowUseSkillObject;
         [Tooltip("This will activate if buff's disallow use item is `TRUE`, developer may set text or icon here")]
         public GameObject disallowUseItemObject;
+        [Tooltip("This will activate if buff's freeze animation is `TRUE`, developer may set text or icon here")]
+        public GameObject freezeAnimationObject;
+        [Tooltip("This will activate if buff's is hide is `TRUE`, developer may set text or icon here")]
+        public GameObject isHideObject;
+        [Tooltip("This will activate if buff's is reveals hide is `TRUE`, developer may set text or icon here")]
+        public GameObject isRevealsHideObject;
+        [Tooltip("This will activate if buff's is blind is `TRUE`, developer may set text or icon here")]
+        public GameObject isBlindObject;
+        [Tooltip("This will activate if buff's do not remove on dead is `TRUE`, developer may set text or icon here")]
+        public GameObject doNotRemoveOnDeadObject;
+        [Tooltip("This will activate if buff's mute footstep sound is `TRUE`, developer may set text or icon here")]
+        public GameObject muteFootstepSoundObject;
+        [Tooltip("This will activate if buff's is extend duration is `TRUE`, developer may set text or icon here")]
+        public GameObject isExtendDurationObject;
+        [Tooltip("Text of all ailments will be written here")]
+        public TextWrapper uiTextAilments;
+        [Tooltip("Seperator for ailments")]
+        public string ailmentsSeparator = ", ";
 
         protected override void UpdateData()
         {
             if (uiTextDuration != null)
             {
-                float duration = Buff.GetDuration(Level);
-                uiTextDuration.SetGameObjectActive(duration != 0);
-                uiTextDuration.text = ZString.Format(
-                    LanguageManager.GetText(formatKeyDuration),
-                    duration.ToString("N0"));
+                float value = Buff.GetDuration(Level);
+                bool activated = value != 0;
+                uiTextDuration.SetGameObjectActive(activated);
+                if (activated)
+                {
+                    uiTextDuration.text = ZString.Format(
+                        LanguageManager.GetText(formatKeyDuration),
+                        value.ToString("N0"));
+                }
             }
 
             if (uiTextRecoveryHp != null)
             {
-                int recoveryHp = Buff.GetRecoveryHp(Level);
-                uiTextRecoveryHp.SetGameObjectActive(recoveryHp != 0);
-                uiTextRecoveryHp.text = ZString.Format(
-                    LanguageManager.GetText(formatKeyRecoveryHp),
-                    recoveryHp.ToString("N0"));
+                int value = Buff.GetRecoveryHp(Level);
+                bool activated = value != 0;
+                uiTextRecoveryHp.SetGameObjectActive(activated);
+                if (activated)
+                {
+                    uiTextRecoveryHp.text = ZString.Format(
+                        LanguageManager.GetText(formatKeyRecoveryHp),
+                        value.ToString("N0"));
+                }
             }
 
             if (uiTextRecoveryMp != null)
             {
-                int recoveryMp = Buff.GetRecoveryMp(Level);
-                uiTextRecoveryMp.SetGameObjectActive(recoveryMp != 0);
-                uiTextRecoveryMp.text = ZString.Format(
-                    LanguageManager.GetText(formatKeyRecoveryMp),
-                    recoveryMp.ToString("N0"));
+                int value = Buff.GetRecoveryMp(Level);
+                bool activated = value != 0;
+                uiTextRecoveryMp.SetGameObjectActive(activated);
+                if (activated)
+                {
+                    uiTextRecoveryMp.text = ZString.Format(
+                        LanguageManager.GetText(formatKeyRecoveryMp),
+                        value.ToString("N0"));
+                }
             }
 
             if (uiTextRecoveryStamina != null)
             {
-                int recoveryStamina = Buff.GetRecoveryStamina(Level);
-                uiTextRecoveryStamina.SetGameObjectActive(recoveryStamina != 0);
-                uiTextRecoveryStamina.text = ZString.Format(
-                    LanguageManager.GetText(formatKeyRecoveryStamina),
-                    recoveryStamina.ToString("N0"));
+                int value = Buff.GetRecoveryStamina(Level);
+                bool activated = value != 0;
+                uiTextRecoveryStamina.SetGameObjectActive(activated);
+                if (activated)
+                {
+                    uiTextRecoveryStamina.text = ZString.Format(
+                        LanguageManager.GetText(formatKeyRecoveryStamina),
+                        value.ToString("N0"));
+                }
             }
 
             if (uiTextRecoveryFood != null)
             {
-                int recoveryFood = Buff.GetRecoveryFood(Level);
-                uiTextRecoveryFood.SetGameObjectActive(recoveryFood != 0);
-                uiTextRecoveryFood.text = ZString.Format(
-                    LanguageManager.GetText(formatKeyRecoveryFood),
-                    recoveryFood.ToString("N0"));
+                int value = Buff.GetRecoveryFood(Level);
+                bool activated = value != 0;
+                uiTextRecoveryFood.SetGameObjectActive(activated);
+                if (activated)
+                {
+                    uiTextRecoveryFood.text = ZString.Format(
+                        LanguageManager.GetText(formatKeyRecoveryFood),
+                        value.ToString("N0"));
+                }
             }
 
             if (uiTextRecoveryWater != null)
             {
-                int recoveryWater = Buff.GetRecoveryWater(Level);
-                uiTextRecoveryWater.SetGameObjectActive(recoveryWater != 0);
-                uiTextRecoveryWater.text = ZString.Format(
-                    LanguageManager.GetText(formatKeyRecoveryWater),
-                    recoveryWater.ToString("N0"));
+                int value = Buff.GetRecoveryWater(Level);
+                bool activated = value != 0;
+                uiTextRecoveryWater.SetGameObjectActive(activated);
+                if (activated)
+                {
+                    uiTextRecoveryWater.text = ZString.Format(
+                        LanguageManager.GetText(formatKeyRecoveryWater),
+                        value.ToString("N0"));
+                }
+            }
+
+            if (uiTextMaxStack != null)
+            {
+                int value = Buff.GetMaxStack(Level);
+                bool activated = value != 0;
+                uiTextMaxStack.SetGameObjectActive(activated);
+                if (activated)
+                {
+                    uiTextMaxStack.text = ZString.Format(
+                        LanguageManager.GetText(formatKeyMaxStack),
+                        value.ToString("N0"));
+                }
+            }
+
+            if (uiTextRemoveBuffWhenAttackChance != null)
+            {
+                float value = Buff.GetRemoveBuffWhenAttackChance(Level);
+                bool activated = value != 0;
+                uiTextRemoveBuffWhenAttackChance.SetGameObjectActive(activated);
+                if (activated)
+                {
+                    uiTextRemoveBuffWhenAttackChance.text = ZString.Format(
+                        LanguageManager.GetText(formatKeyRemoveBuffWhenAttackChance),
+                        (value * 100).ToString("N2"));
+                }
+            }
+
+            if (uiTextRemoveBuffWhenAttackedChance != null)
+            {
+                float value = Buff.GetRemoveBuffWhenAttackedChance(Level);
+                bool activated = value != 0;
+                uiTextRemoveBuffWhenAttackedChance.SetGameObjectActive(activated);
+                if (activated)
+                {
+                    uiTextRemoveBuffWhenAttackedChance.text = ZString.Format(
+                        LanguageManager.GetText(formatKeyRemoveBuffWhenAttackedChance),
+                        (value * 100).ToString("N2"));
+                }
+            }
+
+            if (uiTextRemoveBuffWhenUseSkillChance != null)
+            {
+                float value = Buff.GetRemoveBuffWhenUseSkillChance(Level);
+                bool activated = value != 0;
+                uiTextRemoveBuffWhenUseSkillChance.SetGameObjectActive(activated);
+                if (activated)
+                {
+                    uiTextRemoveBuffWhenUseSkillChance.text = ZString.Format(
+                        LanguageManager.GetText(formatKeyRemoveBuffWhenUseSkillChance),
+                        (value * 100).ToString("N2"));
+                }
+            }
+
+            if (uiTextRemoveBuffWhenUseItemChance != null)
+            {
+                float value = Buff.GetRemoveBuffWhenUseItemChance(Level);
+                bool activated = value != 0;
+                uiTextRemoveBuffWhenUseItemChance.SetGameObjectActive(activated);
+                if (activated)
+                {
+                    uiTextRemoveBuffWhenUseItemChance.text = ZString.Format(
+                        LanguageManager.GetText(formatKeyRemoveBuffWhenUseItemChance),
+                        (value * 100).ToString("N2"));
+                }
+            }
+
+            if (uiTextRemoveBuffWhenPickupItemChance != null)
+            {
+                float value = Buff.GetRemoveBuffWhenPickupItemChance(Level);
+                bool activated = value != 0;
+                uiTextRemoveBuffWhenPickupItemChance.SetGameObjectActive(activated);
+                if (activated)
+                {
+                    uiTextRemoveBuffWhenPickupItemChance.text = ZString.Format(
+                        LanguageManager.GetText(formatKeyRemoveBuffWhenPickupItemChance),
+                        (value * 100).ToString("N2"));
+                }
+            }
+
+            if (uiTextAilments != null)
+            {
+                bool activated = Data.buff.disallowMove ||
+                    Data.buff.disallowSprint ||
+                    Data.buff.disallowWalk ||
+                    Data.buff.disallowJump ||
+                    Data.buff.disallowCrouch ||
+                    Data.buff.disallowCrawl ||
+                    Data.buff.disallowAttack ||
+                    Data.buff.disallowUseSkill ||
+                    Data.buff.disallowUseItem ||
+                    Data.buff.freezeAnimation ||
+                    Data.buff.isHide ||
+                    Data.buff.isRevealsHide ||
+                    Data.buff.isBlind ||
+                    Data.buff.doNotRemoveOnDead ||
+                    Data.buff.muteFootstepSound ||
+                    Data.buff.isExtendDuration;
+                uiTextAilments.SetGameObjectActive(activated);
+                if (activated)
+                {
+                    List<string> ailments = new List<string>();
+                    if (Data.buff.disallowMove)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_DISALLOW_MOVE.ToString(), "Disallow Move"));
+                    if (Data.buff.disallowSprint)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_DISALLOW_SPRINT.ToString(), "Disallow Sprint"));
+                    if (Data.buff.disallowWalk)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_DISALLOW_WALK.ToString(), "Disallow Walk"));
+                    if (Data.buff.disallowJump)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_DISALLOW_JUMP.ToString(), "Disallow Jump"));
+                    if (Data.buff.disallowCrouch)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_DISALLOW_CROUCH.ToString(), "Disallow Crouch"));
+                    if (Data.buff.disallowCrawl)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_DISALLOW_CRAWL.ToString(), "Disallow Crawl"));
+                    if (Data.buff.disallowAttack)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_DISALLOW_ATTACK.ToString(), "Disallow Attack"));
+                    if (Data.buff.disallowUseSkill)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_DISALLOW_USE_SKILL.ToString(), "Disallow Use Skill"));
+                    if (Data.buff.disallowUseItem)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_DISALLOW_USE_ITEM.ToString(), "Disallow Use Item"));
+                    if (Data.buff.freezeAnimation)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_FREEZE_ANIMATION.ToString(), "Freeze Animation"));
+                    if (Data.buff.isHide)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_IS_HIDE.ToString(), "Hide"));
+                    if (Data.buff.isRevealsHide)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_IS_REVEALS_HIDE.ToString(), "Reveals Hide"));
+                    if (Data.buff.isBlind)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_IS_BLIND.ToString(), "Blind"));
+                    if (Data.buff.doNotRemoveOnDead)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_DO_NOT_REMOVE_ON_DEAD.ToString(), "Won't Be Removed On Dead"));
+                    if (Data.buff.muteFootstepSound)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_MUTE_FOOTSTEP_SOUND.ToString(), "Mute Footstep Sound"));
+                    if (Data.buff.isExtendDuration)
+                        ailments.Add(LanguageManager.GetText(UITextKeys.UI_LABEL_BUFF_IS_EXTEND_DURATION.ToString(), "Extend Duration"));
+                    uiTextAilments.text = string.Join(ailmentsSeparator, ailments);
+                }
             }
 
             if (uiBuffStats != null)
@@ -347,6 +543,27 @@ namespace MultiplayerARPG
 
             if (disallowUseItemObject != null)
                 disallowUseItemObject.SetActive(Data.buff.disallowUseItem);
+
+            if (freezeAnimationObject != null)
+                freezeAnimationObject.SetActive(Data.buff.freezeAnimation);
+
+            if (isHideObject != null)
+                isHideObject.SetActive(Data.buff.isHide);
+
+            if (isRevealsHideObject != null)
+                isRevealsHideObject.SetActive(Data.buff.isRevealsHide);
+
+            if (isBlindObject != null)
+                isBlindObject.SetActive(Data.buff.isBlind);
+
+            if (doNotRemoveOnDeadObject != null)
+                doNotRemoveOnDeadObject.SetActive(Data.buff.doNotRemoveOnDead);
+
+            if (muteFootstepSoundObject != null)
+                muteFootstepSoundObject.SetActive(Data.buff.muteFootstepSound);
+
+            if (isExtendDurationObject != null)
+                isExtendDurationObject.SetActive(Data.buff.isExtendDuration);
         }
     }
 }
