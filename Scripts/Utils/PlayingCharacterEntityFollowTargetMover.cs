7cc44b544b78009d3e19f6508e982bb5393b6805
insthync <ittipon.bay@gmail.com>
2024-02-13 11:05:09 +0700

    feat: add simple component to move playing character entity follow transform

---
+++ Scripts/Utils/PlayingCharacterEntityFollowTargetMover.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public class PlayingCharacterEntityFollowTargetMover : MonoBehaviour
+    {
+        public Transform targetTransform;
+
+        private void LateUpdate()
+        {
+            if (GameInstance.PlayingCharacterEntity == null)
+                return;
+            GameInstance.PlayingCharacterEntity.transform.position = targetTransform.position;
+            GameInstance.PlayingCharacterEntity.transform.rotation = targetTransform.rotation;
+        }
+    }
+}
