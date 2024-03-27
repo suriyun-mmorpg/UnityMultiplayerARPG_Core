0bffe541d90d94e4c05b9ffb9ab5864e601d6bd1
insthync <ittipon.bay@gmail.com>
2024-03-27 14:27:37 +0700

    feat: make it able to set value to base character entity ref

--- Scripts/AddressableAssets/AssetReferenceBaseMonsterCharacterEntity.cs
+++ Scripts/AddressableAssets/AssetReferenceBaseMonsterCharacterEntity.cs
@@ -1,9 +1,12 @@
 using LiteNetLibManager;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace MultiplayerARPG
 {
     [System.Serializable]
-    public class AssetReferenceBaseMonsterCharacterEntity : AssetReferenceLiteNetLibBehaviour<BaseMonsterCharacterEntity>
+    public class AssetReferenceBaseMonsterCharacterEntity : AssetReferenceBaseCharacterEntity
     {
 #if UNITY_EDITOR
 
@@ -11,5 +14,36 @@ namespace MultiplayerARPG
         {
         }
 #endif
+
+        public new AsyncOperationHandle<BaseMonsterCharacterEntity> InstantiateAsync(Vector3 position, Quaternion rotation, Transform parent = null)
+        {
+            return Addressables.ResourceManager.CreateChainOperation(Addressables.InstantiateAsync(RuntimeKey, position, rotation, parent, false), GameObjectReady);
+        }
+
+        public new AsyncOperationHandle<BaseMonsterCharacterEntity> InstantiateAsync(Transform parent = null, bool instantiateInWorldSpace = false)
+        {
+            return Addressables.ResourceManager.CreateChainOperation(Addressables.InstantiateAsync(RuntimeKey, parent, instantiateInWorldSpace, false), GameObjectReady);
+        }
+
+        public new AsyncOperationHandle<BaseMonsterCharacterEntity> LoadAssetAsync()
+        {
+            return Addressables.ResourceManager.CreateChainOperation(base.LoadAssetAsync<GameObject>(), GameObjectReady);
+        }
+
+        static AsyncOperationHandle<BaseMonsterCharacterEntity> GameObjectReady(AsyncOperationHandle<GameObject> arg)
+        {
+            var comp = arg.Result.GetComponent<BaseMonsterCharacterEntity>();
+            return Addressables.ResourceManager.CreateCompletedOperation(comp, string.Empty);
+        }
+
+        public override bool ValidateAsset(Object obj)
+        {
+            return ValidateAsset<BaseMonsterCharacterEntity>(obj);
+        }
+
+        public override bool ValidateAsset(string path)
+        {
+            return ValidateAsset<BaseMonsterCharacterEntity>(path);
+        }
     }
 }
