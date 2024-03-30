99082f7e849eb921923c099a3c5691b2a2db3bcc
insthync <ittipon.bay@gmail.com>
2024-03-30 10:21:14 +0700

    improve: avoid leaked managed shell

---
+++ Scripts/Gameplay/VehicleSystems/VehicleEntity_Cleanup.cs
@@ -0,0 +1,19 @@
+namespace MultiplayerARPG
+{
+    public partial class VehicleEntity
+    {
+        public override void Clean()
+        {
+            base.Clean();
+            vehicleType = null;
+            seats?.Clear();
+            onVehicleDestroy?.RemoveAllListeners();
+            onVehicleDestroy = null;
+            Resistances?.Clear();
+            Armors?.Clear();
+            _passengers?.Clear();
+            _spawnEvents?.Clear();
+            _cacheBuff = null;
+        }
+    }
+}
