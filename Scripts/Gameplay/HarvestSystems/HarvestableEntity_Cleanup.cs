db6dee264fa3aa114caa2bbfdd3f85b9efaedb6a
insthync <ittipon.bay@gmail.com>
2024-03-30 10:22:50 +0700

    improve: avoid leaked managed shell

---
+++ Scripts/Gameplay/HarvestSystems/HarvestableEntity_Cleanup.cs
@@ -0,0 +1,16 @@
+namespace MultiplayerARPG
+{
+    public partial class HarvestableEntity
+    {
+        public override void Clean()
+        {
+            base.Clean();
+            harvestable = null;
+            onHarvestableDestroy?.RemoveAllListeners();
+            onHarvestableDestroy = null;
+            SpawnArea = null;
+            SpawnPrefab = null;
+            SpawnAddressablePrefab = null;
+        }
+    }
+}
