1a316238268d9a3fc53cb434eeb8498c1bedd4b6
insthync <ittipon.bay@gmail.com>
2024-04-01 10:08:13 +0700

    improve: exclude editor only codes

--- Scripts/GameData/Utils/ComponentWithPrefabRef.cs
+++ Scripts/GameData/Utils/ComponentWithPrefabRef.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace MultiplayerARPG
 {
@@ -12,10 +15,23 @@ namespace MultiplayerARPG
         where T : MonoBehaviour
     {
         public T refToPrefab;
+#if UNITY_EDITOR
+        [InspectorButton(nameof(ReplacePrefab))]
+        public bool btnRepacePrefab;
+#endif
 
         public void SetupRefToPrefab(GameObject prefab)
         {
             refToPrefab = prefab.GetComponent<T>();
         }
+
+#if UNITY_EDITOR
+        public void ReplacePrefab()
+        {
+            string path = PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(refToPrefab);
+            PrefabUtility.SaveAsPrefabAsset(gameObject, path, out bool success);
+            Debug.Log($"Replaced {gameObject} to {path} success?: {success}");
+        }
+#endif
     }
 }
