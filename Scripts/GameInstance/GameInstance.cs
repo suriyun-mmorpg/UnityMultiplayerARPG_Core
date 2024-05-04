87ba0e0fc82c7546ca7d58120518dae88bd5e762
insthync <ittipon.bay@gmail.com>
2024-05-05 00:58:33 +0700

    chore: add missing namespaces

--- Scripts/GameInstance/GameInstance.cs
+++ Scripts/GameInstance/GameInstance.cs
@@ -1,7 +1,9 @@
 ﻿using LiteNetLibManager;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 #if ENABLE_PURCHASING && (UNITY_IOS || UNITY_ANDROID)
 using UnityEngine.Purchasing;
