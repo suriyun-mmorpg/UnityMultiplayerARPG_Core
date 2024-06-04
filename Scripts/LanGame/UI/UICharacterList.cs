d8707a3f23ff6ed7734d97c03c16faeb10038dc8
insthync <ittipon.bay@gmail.com>
2024-06-04 15:15:01 +0700

    feat: update equipment immediately when setup character model's items by list UI

--- Scripts/LanGame/UI/UICharacterList.cs
+++ Scripts/LanGame/UI/UICharacterList.cs
@@ -155,7 +155,9 @@ namespace MultiplayerARPG
                             comps[i].SetupCharacterModelEvents(characterModel);
                             comps[i].ApplyModelAndColorBySavedData(characterData.PublicInts);
                         }
+                        characterModel.UpdateEquipmentImmediately = true;
                         characterModel.SetEquipItems(characterData.EquipItems, characterData.SelectableWeaponSets, characterData.EquipWeaponSet, false);
+                        characterModel.UpdateEquipmentImmediately = false;
                         characterModel.gameObject.SetActive(false);
                         CharacterSelectionManager.Add(uiCharacter);
                     }
