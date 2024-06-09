b75778bfbe4ce10c62a13f0ed6e51d31b9a18b9b
insthync <ittipon.bay@gmail.com>
2024-06-09 12:04:38 +0700

    feat: add component to show signs if character from UI is playing character

---
+++ Scripts/UI/Character/UIPlayingCharacterSigns.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public class UIPlayingCharacterSigns : MonoBehaviour
+    {
+        public UICharacter uiCharacter;
+        public GameObject[] isPlayingCharacterObjects = new GameObject[0];
+        public GameObject[] isNotPlayingCharacterObjects = new GameObject[0];
+
+        private void Update()
+        {
+            bool isPlayingCharacter = uiCharacter != null && GameInstance.PlayingCharacter != null &&
+                uiCharacter.Data != null && string.Equals(uiCharacter.Data.Id, GameInstance.PlayingCharacter.Id);
+
+            for (int i = 0; i < isPlayingCharacterObjects.Length; ++i)
+            {
+                isPlayingCharacterObjects[i].SetActive(isPlayingCharacter);
+            }
+
+            for (int i = 0; i < isNotPlayingCharacterObjects.Length; ++i)
+            {
+                isPlayingCharacterObjects[i].SetActive(!isPlayingCharacter);
+            }
+        }
+    }
+}
