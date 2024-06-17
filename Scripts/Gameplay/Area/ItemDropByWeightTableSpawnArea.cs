39edcd1e411b9d48eb161dbe2f4cd18fc3fab020
insthync <ittipon.bay@gmail.com>
2024-06-17 23:18:53 +0700

    feat: add spawn area to drop item by weight table

---
+++ Scripts/Gameplay/Area/ItemDropByWeightTableSpawnArea.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiplayerARPG
+{
+    internal class ItemDropByWeightTableSpawnArea : GameSpawnArea
+    {
+        public ItemRandomByWeightTable weightTable;
+        public int amount;
+
+        public override void SpawnAll()
+        {
+            // TODO: Implement this
+        }
+    }
+}
