da7f0783bd4c30ef8fbad8b464198bcabc4896ff
insthync <ittipon.bay@gmail.com>
2024-06-17 23:04:36 +0700

    feat: avoid null data

--- Scripts/UI/Utils/ButtonActivators/ActivateButtonActivator.cs
+++ Scripts/UI/Utils/ButtonActivators/ActivateButtonActivator.cs
@@ -4,7 +4,7 @@ namespace MultiplayerARPG
 {
     public class ActivateButtonActivator : MonoBehaviour
     {
-        public GameObject[] activateObjects;
+        public GameObject[] activateObjects = new GameObject[0];
 
         private void LateUpdate()
         {
