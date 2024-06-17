2d72d1796a3b96aeb8d107b458025a604dd9ed2f
insthync <ittipon.bay@gmail.com>
2024-06-17 23:16:58 +0700

    feat: add buttons to change slider's value

--- Scripts/GameplaySettings/CameraRotationSpeedScaleSetting.cs
+++ Scripts/GameplaySettings/CameraRotationSpeedScaleSetting.cs
@@ -11,9 +11,12 @@ namespace MultiplayerARPG
         public string valueFormat = "{0}%";
         public float valueTextMultiplicator = 100f;
         public float minValue = 0.01f;
-        public float maxValue = 1f;
+        public float maxValue = 4f;
         public float defaultValue = 1f;
-        public string cameraRotationSpeedScaleSaveKey = "DEFAULT_CAMERA_ROTATION_SPEED_SCALE";
+        public float valueChangeStepOnClick = 0.01f;
+        public Button buttonIncrease;
+        public Button buttonDecrease;
+        public string cameraRotationSpeedScaleSaveKey = "3RD_PERSON_CAMERA_SCALE";
         private readonly static Dictionary<string, float> s_cameraRotationSpeedScales = new Dictionary<string, float>();
         public float CameraRotationSpeedScale
         {
@@ -40,17 +43,27 @@ namespace MultiplayerARPG
             return s_cameraRotationSpeedScales[key];
         }
 
-        private void Start()
+        private void Awake()
         {
             slider.minValue = minValue;
             slider.maxValue = maxValue;
             slider.SetValueWithoutNotify(CameraRotationSpeedScale);
             slider.onValueChanged.AddListener(OnValueChanged);
+            if (buttonIncrease != null)
+                buttonIncrease.onClick.AddListener(OnClickIncrease);
+            if (buttonDecrease != null)
+                buttonDecrease.onClick.AddListener(OnClickDecrease);
+            if (textScaleValue != null)
+                textScaleValue.text = string.Format(valueFormat, (slider.value * valueTextMultiplicator).ToString("N2"));
         }
 
         private void OnDestroy()
         {
             slider.onValueChanged.RemoveListener(OnValueChanged);
+            if (buttonIncrease != null)
+                buttonIncrease.onClick.RemoveListener(OnClickIncrease);
+            if (buttonDecrease != null)
+                buttonDecrease.onClick.RemoveListener(OnClickDecrease);
         }
 
         public void OnValueChanged(float value)
@@ -58,6 +71,16 @@ namespace MultiplayerARPG
             CameraRotationSpeedScale = value;
             if (textScaleValue != null)
                 textScaleValue.text = string.Format(valueFormat, (value * valueTextMultiplicator).ToString("N2"));
+        }
+
+        public void OnClickIncrease()
+        {
+            slider.value += valueChangeStepOnClick;
+        }
+
+        public void OnClickDecrease()
+        {
+            slider.value -= valueChangeStepOnClick;
         }
     }
 }
