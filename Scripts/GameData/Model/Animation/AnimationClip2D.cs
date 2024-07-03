dc3243413de829b42e54b1ac50d38a493ccad1d7
insthync <ittipon.bay@gmail.com>
2024-07-03 19:23:24 +0700

    chore: delete unused codes, set namespace for `AnimationClip2D`

--- Scripts/GameData/Model/Animation/AnimationClip2D.cs
+++ Scripts/GameData/Model/Animation/AnimationClip2D.cs
@@ -1,19 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-[CreateAssetMenu(fileName = "Animation Clip 2D", menuName = "Animation Clip 2D")]
-public class AnimationClip2D : ScriptableObject
+namespace MultiplayerARPG
 {
-    public Sprite[] frames;
-    public float framesPerSec = 5;
-    public bool loop = true;
-    public bool flipX;
-    public bool flipY;
-
-    public float length
+    [CreateAssetMenu(fileName = "Animation Clip 2D", menuName = "Animation Clip 2D")]
+    public class AnimationClip2D : ScriptableObject
     {
-        get { return frames.Length / framesPerSec; }
-        set { framesPerSec = frames.Length / value; }
+        public Sprite[] frames;
+        public float framesPerSec = 5;
+        public bool loop = true;
+        public bool flipX;
+        public bool flipY;
+
+        public float length
+        {
+            get { return frames.Length / framesPerSec; }
+            set { framesPerSec = frames.Length / value; }
+        }
     }
 }
