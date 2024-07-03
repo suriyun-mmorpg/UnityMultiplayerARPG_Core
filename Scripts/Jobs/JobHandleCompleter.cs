64d43b7f39c7fda4694d03f42732852551613903
insthync <ittipon.bay@gmail.com>
2024-07-03 19:28:03 +0700

    feat: add job handler completer

---
+++ Scripts/Jobs/JobHandleCompleter.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    [DefaultExecutionOrder(int.MaxValue)]
+    public class JobHandleCompleter : MonoBehaviour
+    {
+        private static JobHandleCompleter _singleton;
+        public static JobHandleCompleter Singleton
+        {
+            get
+            {
+                if (_singleton == null)
+                {
+                    _singleton = new GameObject("_JobHandleCompleter_Instance").AddComponent<JobHandleCompleter>();
+                    DontDestroyOnLoad(_singleton.gameObject);
+                }
+                return _singleton;
+            }
+        }
+
+        private readonly static List<JobHandle> s_jobHandles = new List<JobHandle>();
+        public static System.Action OnCompletedEvent = null;
+
+        public void AddJobHandle(JobHandle jobHandle)
+        {
+            s_jobHandles.Add(jobHandle);
+        }
+
+        private void LateUpdate()
+        {
+            NativeArray<JobHandle> jobHandles = new NativeArray<JobHandle>(s_jobHandles.ToArray(), Allocator.TempJob);
+            s_jobHandles.Clear();
+            JobHandle.CompleteAll(jobHandles);
+            OnCompletedEvent?.Invoke();
+            jobHandles.Dispose();
+        }
+    }
+}
