0b30c8a5d5d0bdefa692401803c6bd81122e8d00
insthync <ittipon.bay@gmail.com>
2024-07-11 03:05:10 +0700

    feat: WIP data exporter

--- Scripts/ExternalData/ExternalDataUtils.cs
+++ Scripts/ExternalData/ExternalDataUtils.cs
@@ -36,6 +36,8 @@ namespace MultiplayerARPG
 
         public static Dictionary<string, object> GetExportData(this object target)
         {
+            if (target == null)
+                return null;
             Dictionary<string, object> result = new Dictionary<string, object>();
             List<FieldInfo> exportingFields = new List<FieldInfo>();
             FieldInfo[] publicFields = target.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
@@ -46,16 +48,89 @@ namespace MultiplayerARPG
                 if (nonPublicFields[i].HasAttribute<SerializeField>())
                 {
                     exportingFields.Add(nonPublicFields[i]);
-                    break;
                 }
             }
             foreach (FieldInfo field in exportingFields)
             {
                 Type fieldType = field.FieldType;
-                if (field.FieldType.IsArray)
-                    fieldType = field.FieldType.GetElementType();
-                if (fieldType.IsClass)
+                if (fieldType.IsArray)
                 {
+                    Type elementType = field.FieldType.GetElementType();
+                    if (elementType.IsClass)
+                    {
+                        if (elementType == typeof(string))
+                        {
+                            result[field.Name] = field.GetValue(target);
+                            continue;
+                        }
+                        Array arr = field.GetValue(target) as Array;
+                        List<Dictionary<string, object>> dicts = new List<Dictionary<string, object>>();
+                        if (elementType.HasInterface<IGameData>())
+                        {
+                            for (int i = 0; i < arr.Length; ++i)
+                            {
+                                IGameData gameData = arr.GetValue(i) as IGameData;
+                                if (gameData != null && !string.IsNullOrEmpty(gameData.Id))
+                                {
+                                    dicts.Add(new Dictionary<string, object>()
+                                    {
+                                        { "Type", elementType.FullName },
+                                        { "Id",  gameData.Id },
+                                    });
+                                }
+                            }
+                            result[field.Name] = dicts;
+                            continue;
+                        }
+                        if (elementType.IsSubclassOf(typeof(AssetReference)))
+                        {
+                            for (int i = 0; i < arr.Length; ++i)
+                            {
+                                AssetReference aa = arr.GetValue(i) as AssetReference;
+                                if (aa.IsDataValid())
+                                {
+                                    dicts.Add(new Dictionary<string, object>()
+                                    {
+                                        { "Type", elementType.FullName },
+                                        { "Key", aa.RuntimeKey },
+                                    });
+                                }
+                            }
+                            result[field.Name] = dicts;
+                            continue;
+                        }
+                        if (elementType.IsSubclassOf(typeof(UnityEngine.Object)))
+                        {
+                            continue;
+                        }
+                        for (int i = 0; i < arr.Length; ++i)
+                        {
+                            dicts.Add(GetExportData(arr.GetValue(i)));
+                        }
+                        result[field.Name] = dicts;
+                    }
+                    else if (fieldType.IsValueType && !fieldType.IsPrimitive && !fieldType.IsEnum)
+                    {
+                        Array arr = field.GetValue(target) as Array;
+                        List<Dictionary<string, object>> dicts = new List<Dictionary<string, object>>();
+                        for (int i = 0; i < arr.Length; ++i)
+                        {
+                            dicts.Add(GetExportData(arr.GetValue(i)));
+                        }
+                        result[field.Name] = dicts;
+                    }
+                    else
+                    {
+                        result[field.Name] = field.GetValue(target);
+                    }
+                }
+                else if (fieldType.IsClass)
+                {
+                    if (fieldType == typeof(string))
+                    {
+                        result[field.Name] = field.GetValue(target);
+                        continue;
+                    }
                     if (fieldType.HasInterface<IGameData>())
                     {
                         IGameData gameData = field.GetValue(target) as IGameData;
@@ -74,7 +149,11 @@ namespace MultiplayerARPG
                         AssetReference aa = field.GetValue(target) as AssetReference;
                         if (aa.IsDataValid())
                         {
-                            result[field.Name] = aa.RuntimeKey;
+                            result[field.Name] = new Dictionary<string, object>()
+                            {
+                                { "Type", fieldType.FullName },
+                                { "Key", aa.RuntimeKey },
+                            };
                         }
                         continue;
                     }
