aa52744c4ed733904fc15c70e51f64ef5795f3e9
insthync <ittipon.bay@gmail.com>
2024-07-15 00:08:08 +0700

    feat: move mount function to be part of game entity not character entity

--- Scripts/Gameplay/BaseGameEntity_MountFunctions.cs
+++ Scripts/Gameplay/BaseGameEntity_MountFunctions.cs
@@ -1,11 +1,11 @@
-﻿using LiteNetLibManager;
+﻿using Insthync.AddressableAssetTools;
+using LiteNetLibManager;
 using UnityEngine;
 
 namespace MultiplayerARPG
 {
     public partial class BaseGameEntity
     {
-
         public byte PassengingVehicleSeatIndex { get; private set; }
 
         private IVehicleEntity _passengingVehicleEntity;
@@ -51,6 +51,53 @@ namespace MultiplayerARPG
                     return PassengingVehicleEntity.Entity.Model;
                 return null;
             }
+        }
+
+        protected float _lastMountTime;
+
+        public virtual void Mount(VehicleEntity prefab, AssetReferenceVehicleEntity addressablePrefab)
+        {
+            if (!IsServer || (prefab == null && !addressablePrefab.IsDataValid()) || Time.unscaledTime - _lastMountTime < CurrentGameInstance.mountDelay)
+                return;
+
+            _lastMountTime = Time.unscaledTime;
+
+            Vector3 enterPosition = EntityTransform.position;
+            if (PassengingVehicleEntity != null)
+            {
+                enterPosition = PassengingVehicleEntity.Entity.EntityTransform.position;
+                ExitVehicle();
+            }
+
+            // Instantiate new mount entity
+            LiteNetLibIdentity spawnObj;
+            if (prefab != null)
+            {
+                spawnObj = BaseGameNetworkManager.Singleton.Assets.GetObjectInstance(
+                    prefab.Identity.HashAssetId, enterPosition,
+                    Quaternion.Euler(0, EntityTransform.eulerAngles.y, 0));
+            }
+            else if (addressablePrefab.IsDataValid())
+            {
+                spawnObj = BaseGameNetworkManager.Singleton.Assets.GetObjectInstance(
+                    addressablePrefab.HashAssetId, enterPosition,
+                    Quaternion.Euler(0, EntityTransform.eulerAngles.y, 0));
+            }
+            else
+            {
+                return;
+            }
+
+            if (spawnObj == null)
+            {
+                return;
+            }
+
+            VehicleEntity vehicle = spawnObj.GetComponent<VehicleEntity>();
+            BaseGameNetworkManager.Singleton.Assets.NetworkSpawn(spawnObj, 0, ConnectionId);
+
+            // Seat index for mount entity always 0
+            EnterVehicle(vehicle, 0);
         }
 
         protected virtual bool EnterVehicle(IVehicleEntity vehicle, byte seatIndex)
--- Scripts/Gameplay/CharacterEntity/BaseCharacterEntity_MountFunctions.cs
+++ Scripts/Gameplay/CharacterEntity/BaseCharacterEntity_MountFunctions.cs
@@ -1,58 +1,7 @@
-﻿using Insthync.AddressableAssetTools;
-using LiteNetLibManager;
-using UnityEngine;
-
-namespace MultiplayerARPG
+﻿namespace MultiplayerARPG
 {
     public partial class BaseCharacterEntity
     {
-        protected float _lastMountTime;
-
-        public virtual void Mount(VehicleEntity prefab, AssetReferenceVehicleEntity addressablePrefab)
-        {
-            if (!IsServer || (prefab == null && !addressablePrefab.IsDataValid()) || Time.unscaledTime - _lastMountTime < CurrentGameInstance.mountDelay)
-                return;
-
-            _lastMountTime = Time.unscaledTime;
-
-            Vector3 enterPosition = EntityTransform.position;
-            if (PassengingVehicleEntity != null)
-            {
-                enterPosition = PassengingVehicleEntity.Entity.EntityTransform.position;
-                ExitVehicle();
-            }
-
-            // Instantiate new mount entity
-            LiteNetLibIdentity spawnObj;
-            if (prefab != null)
-            {
-                spawnObj = BaseGameNetworkManager.Singleton.Assets.GetObjectInstance(
-                    prefab.Identity.HashAssetId, enterPosition,
-                    Quaternion.Euler(0, EntityTransform.eulerAngles.y, 0));
-            }
-            else if (addressablePrefab.IsDataValid())
-            {
-                spawnObj = BaseGameNetworkManager.Singleton.Assets.GetObjectInstance(
-                    addressablePrefab.HashAssetId, enterPosition,
-                    Quaternion.Euler(0, EntityTransform.eulerAngles.y, 0));
-            }
-            else
-            {
-                return;
-            }
-
-            if (spawnObj == null)
-            {
-                return;
-            }
-
-            VehicleEntity vehicle = spawnObj.GetComponent<VehicleEntity>();
-            BaseGameNetworkManager.Singleton.Assets.NetworkSpawn(spawnObj, 0, ConnectionId);
-
-            // Seat index for mount entity always 0
-            EnterVehicle(vehicle, 0);
-        }
-
         public override void SetPassengingVehicle(byte seatIndex, IVehicleEntity vehicleEntity)
         {
             base.SetPassengingVehicle(seatIndex, vehicleEntity);
