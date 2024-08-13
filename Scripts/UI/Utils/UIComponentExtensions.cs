23a020ad24ff94e435cc58b486d0f94c69c66a26
insthync <ittipon.bay@gmail.com>
2024-08-13 14:00:51 +0700

    feat: add function to load texture by URL and set to raw image component

--- Scripts/UI/Utils/UIComponentExtensions.cs
+++ Scripts/UI/Utils/UIComponentExtensions.cs
@@ -5,36 +5,57 @@ namespace MultiplayerARPG
 {
     public static class UIComponentExtensions
     {
-        public static async void SetImageGameDataIcon(this Image image, BaseGameData gameData, bool deactivateIfNotSprite = true)
+        public static async void SetImageGameDataIcon(this Image image, BaseGameData gameData, bool deactivateIfNoContent = true)
         {
 #if UNITY_EDITOR || !UNITY_SERVER
             Sprite sprite = null;
             if (gameData)
                 sprite = await gameData.GetIcon();
-            image.SetImageSprite(sprite, deactivateIfNotSprite);
+            image.SetImageSprite(sprite, deactivateIfNoContent);
 #endif
         }
 
-        public static async void SetImageNpcDialogIcon(this Image image, BaseNpcDialog npcDialog, bool deactivateIfNotSprite = true)
+        public static async void SetRawImageExternalTexture(this RawImage rawImage, string url, Texture2D defaultTexture = null, bool deactivateIfNoContent = true)
+        {
+#if UNITY_EDITOR || !UNITY_SERVER
+            if (!rawImage)
+                return;
+            Texture2D texture = null;
+            // TODO: May improve it by change URL format before load
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                texture = await ExternalTextureManager.Load(url);
+                if (texture == null)
+                    texture = defaultTexture;
+            }
+            rawImage.gameObject.SetActive(!deactivateIfNoContent || texture != null);
+            rawImage.texture = texture;
+#endif
+        }
+
+        public static async void SetImageNpcDialogIcon(this Image image, BaseNpcDialog npcDialog, bool deactivateIfNoContent = true)
         {
 #if UNITY_EDITOR || !UNITY_SERVER
             Sprite sprite = null;
             if (npcDialog)
                 sprite = await npcDialog.GetIcon();
-            image.SetImageSprite(sprite, deactivateIfNotSprite);
+            image.SetImageSprite(sprite, deactivateIfNoContent);
 #endif
         }
 
-        public static void SetImageSprite(this Image image, Sprite sprite, bool deactivateIfNotSprite = true)
+        public static void SetImageSprite(this Image image, Sprite sprite, bool deactivateIfNoContent = true)
         {
+#if UNITY_EDITOR || !UNITY_SERVER
             if (!image)
                 return;
-            image.gameObject.SetActive(!deactivateIfNotSprite || sprite != null);
+            image.gameObject.SetActive(!deactivateIfNoContent || sprite != null);
             image.sprite = sprite;
+#endif
         }
 
         public static async void PlayNpcDialogVoice(this AudioSource source, MonoBehaviour uiRoot, BaseNpcDialog npcDialog)
         {
+#if UNITY_EDITOR || !UNITY_SERVER
             if (!source)
                 return;
             source.Stop();
@@ -42,6 +63,7 @@ namespace MultiplayerARPG
             source.clip = clip;
             if (clip != null && uiRoot.enabled)
                 source.Play();
+#endif
         }
     }
 }
