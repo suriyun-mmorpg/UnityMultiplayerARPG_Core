bace7036f1c8e63f94db6597c8d7a6b50d510f8e
insthync <ittipon.bay@gmail.com>
2024-08-16 20:45:07 +0700

    feat: make UI scene loading codes / component reusable

--- Scripts/UI/Scenes/UISceneLoading.cs
+++ Scripts/UI/Scenes/UISceneLoading.cs
@@ -1,22 +1,10 @@
-﻿using Insthync.AddressableAssetTools;
-using System.Collections;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-
-namespace MultiplayerARPG
+﻿namespace MultiplayerARPG
 {
-    public partial class UISceneLoading : MonoBehaviour
+    public partial class UISceneLoading : UIInstancedSceneLoading
     {
         public static UISceneLoading Singleton { get; private set; }
-        public GameObject rootObject;
-        public TextWrapper uiTextProgress;
-        public Image imageGage;
-        public Slider sliderGage;
-        [Tooltip("Delay before deactivate `rootObject`")]
-        public float finishedDelay = 0.25f;
 
-        protected virtual void Awake()
+        protected override void Awake()
         {
             if (Singleton != null)
             {
@@ -25,93 +13,7 @@ namespace MultiplayerARPG
             }
 
             DontDestroyOnLoad(gameObject);
-            Singleton = this;
-
-            if (rootObject != null)
-                rootObject.SetActive(false);
-        }
-
-        public virtual Coroutine LoadScene(string sceneName)
-        {
-            return StartCoroutine(LoadSceneRoutine(sceneName));
-        }
-
-        protected virtual IEnumerator LoadSceneRoutine(string sceneName)
-        {
-            if (SceneManager.GetActiveScene().name.Equals(sceneName))
-                yield break;
-            if (rootObject != null)
-                rootObject.SetActive(true);
-            if (uiTextProgress != null)
-                uiTextProgress.text = "0.00%";
-            if (imageGage != null)
-                imageGage.fillAmount = 0;
-            if (sliderGage != null)
-                sliderGage.value = 0;
-            yield return null;
-            AsyncOperation asyncOp = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
-            while (!asyncOp.isDone)
-            {
-                if (uiTextProgress != null)
-                    uiTextProgress.text = (asyncOp.progress * 100f).ToString("N2") + "%";
-                if (imageGage != null)
-                    imageGage.fillAmount = asyncOp.progress;
-                if (sliderGage != null)
-                    sliderGage.value = asyncOp.progress;
-                yield return null;
-            }
-            yield return null;
-            if (uiTextProgress != null)
-                uiTextProgress.text = "100.00%";
-            if (imageGage != null)
-                imageGage.fillAmount = 1;
-            if (sliderGage != null)
-                sliderGage.value = 1;
-            yield return new WaitForSecondsRealtime(finishedDelay);
-            if (rootObject != null)
-                rootObject.SetActive(false);
-        }
-
-        public virtual Coroutine LoadScene(AssetReferenceScene sceneRef)
-        {
-            return StartCoroutine(LoadSceneRoutine(sceneRef));
-        }
-
-        protected virtual IEnumerator LoadSceneRoutine(AssetReferenceScene sceneRef)
-        {
-            
-            if (SceneManager.GetActiveScene().name.Equals(sceneRef.SceneName))
-                yield break;
-            if (rootObject != null)
-                rootObject.SetActive(true);
-            if (uiTextProgress != null)
-                uiTextProgress.text = "0.00%";
-            if (imageGage != null)
-                imageGage.fillAmount = 0;
-            if (sliderGage != null)
-                sliderGage.value = 0;
-            yield return null;
-            var asyncOp = sceneRef.LoadSceneAsync(LoadSceneMode.Single);
-            while (!asyncOp.IsDone)
-            {
-                if (uiTextProgress != null)
-                    uiTextProgress.text = (asyncOp.PercentComplete * 100f).ToString("N2") + "%";
-                if (imageGage != null)
-                    imageGage.fillAmount = asyncOp.PercentComplete;
-                if (sliderGage != null)
-                    sliderGage.value = asyncOp.PercentComplete;
-                yield return null;
-            }
-            yield return null;
-            if (uiTextProgress != null)
-                uiTextProgress.text = "100.00%";
-            if (imageGage != null)
-                imageGage.fillAmount = 1;
-            if (sliderGage != null)
-                sliderGage.value = 1;
-            yield return new WaitForSecondsRealtime(finishedDelay);
-            if (rootObject != null)
-                rootObject.SetActive(false);
+            base.Awake();
         }
     }
 }
