ea03956c4a98e190e46f940575e9dcf4a7ed0002
insthync <ittipon.bay@gmail.com>
2024-09-05 01:01:42 +0700

    feat: add set scale on awake component

---
+++ Scripts/Utils/SetScaleOnAwake.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace UtilsComponents
+{
+    public class SetScaleOnAwake : MonoBehaviour
+    {
+        public Vector3 scale = Vector3.one;
+
+        private void Awake()
+        {
+            transform.localScale = scale;
+        }
+    }
+}
