46d23afe90b55ff68c2bbb0055fcf6da0fafb82a
insthync <ittipon.bay@gmail.com>
2024-09-28 02:53:14 +0700

    feat: add set layer component

---
+++ Scripts/Utils/SetLayerTo.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace UtilsComponents
+{
+    public class SetLayerTo : MonoBehaviour
+    {
+        public UnityLayer layer;
+        public bool setChildrenLayersRecursively = true;
+        public bool includeInactiveLayers = false;
+
+        private void LateUpdate()
+        {
+            if (setChildrenLayersRecursively)
+                gameObject.SetLayerRecursively(layer, includeInactiveLayers);
+            else
+                gameObject.layer = layer;
+        }
+    }
+}
