fb73fb3372209405dc43eec309aef75985a55114
insthync <ittipon.bay@gmail.com>
2024-09-28 03:03:16 +0700

    feat: add functions to UI component extensions

--- Scripts/UI/Utils/UIComponentExtensions.cs
+++ Scripts/UI/Utils/UIComponentExtensions.cs
@@ -5,13 +5,18 @@ namespace MultiplayerARPG
 {
     public static class UIComponentExtensions
     {
-        public static async void SetImageGameDataIcon(this Image image, BaseGameData gameData, bool deactivateIfNoContent = true)
+        public static void SetImageGameDataIcon(this Image image, BaseGameData gameData)
+        {
+            SetImageGameDataIcon(image, gameData, true);
+        }
+
+        public static async void SetImageGameDataIcon(this Image image, BaseGameData gameData, bool deactivateIfNoContent, params GameObject[] placeHolders)
         {
 #if UNITY_EDITOR || !UNITY_SERVER
             Sprite sprite = null;
             if (gameData)
                 sprite = await gameData.GetIcon();
-            image.SetImageSprite(sprite, deactivateIfNoContent);
+            image.SetImageSprite(sprite, deactivateIfNoContent, placeHolders);
 #endif
         }
 
@@ -22,7 +27,7 @@ namespace MultiplayerARPG
                 return;
             Texture2D texture = null;
             // TODO: May improve it by change URL format before load
-            if (string.IsNullOrWhiteSpace(url))
+            if (!string.IsNullOrWhiteSpace(url))
             {
                 texture = await ExternalTextureManager.Load(url);
                 if (texture == null)
@@ -33,23 +38,36 @@ namespace MultiplayerARPG
 #endif
         }
 
-        public static async void SetImageNpcDialogIcon(this Image image, BaseNpcDialog npcDialog, bool deactivateIfNoContent = true)
+        public static void SetImageNpcDialogIcon(this Image image, BaseNpcDialog npcDialog)
+        {
+            SetImageNpcDialogIcon(image, npcDialog, true);
+        }
+
+        public static async void SetImageNpcDialogIcon(this Image image, BaseNpcDialog npcDialog, bool deactivateIfNoContent, params GameObject[] placeHolders)
         {
 #if UNITY_EDITOR || !UNITY_SERVER
             Sprite sprite = null;
             if (npcDialog)
                 sprite = await npcDialog.GetIcon();
-            image.SetImageSprite(sprite, deactivateIfNoContent);
+            image.SetImageSprite(sprite, deactivateIfNoContent, placeHolders);
 #endif
         }
 
-        public static void SetImageSprite(this Image image, Sprite sprite, bool deactivateIfNoContent = true)
+        public static void SetImageSprite(this Image image, Sprite sprite, bool deactivateIfNoContent, params GameObject[] placeHolders)
         {
 #if UNITY_EDITOR || !UNITY_SERVER
             if (!image)
                 return;
-            image.gameObject.SetActive(!deactivateIfNoContent || sprite != null);
+            bool isNull = sprite == null;
+            image.gameObject.SetActive(!deactivateIfNoContent || !isNull);
             image.sprite = sprite;
+            if (placeHolders != null && placeHolders.Length > 0)
+            {
+                for (int i = 0; i < placeHolders.Length; ++i)
+                {
+                    placeHolders[i].SetActive(isNull);
+                }
+            }
 #endif
         }
 
