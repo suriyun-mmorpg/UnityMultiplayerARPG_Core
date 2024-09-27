8f6a2657f9ce3d3b160fa838f2d63e488a611352
insthync <ittipon.bay@gmail.com>
2024-09-28 03:35:45 +0700

    feat: add day night time counting UI

---
+++ Scripts/UI/DayNight/UIDayNightTime.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public class UIDayNightTime : MonoBehaviour
+    {
+        public UITimer uiTimerDayNightTime;
+
+        private void Update()
+        {
+            float timeOfDay = GameInstance.Singleton != null && GameInstance.Singleton.DayNightTimeUpdater != null ? GameInstance.Singleton.DayNightTimeUpdater.TimeOfDay : 0f;
+            float timeOfDayInSeconds = timeOfDay / 24f * (60f * 60f * 24f);
+            if (uiTimerDayNightTime != null)
+                uiTimerDayNightTime.UpdateTime(timeOfDayInSeconds);
+        }
+    }
+}
