a26a5f05071cc5bdfa7fa66fbeb690536a7e2836
insthync <ittipon.bay@gmail.com>
2024-12-22 16:10:07 +0700

    feat: avoid null ref error

--- Scripts/Gameplay/CharacterEntity/PlayerCharacterEntity/BasePlayerCharacterEntity_MoveFunctions.cs
+++ Scripts/Gameplay/CharacterEntity/PlayerCharacterEntity/BasePlayerCharacterEntity_MoveFunctions.cs
@@ -4,56 +4,56 @@ namespace MultiplayerARPG
     {
         protected override bool CanMove_Implementation()
         {
-            if (Vending.Data.isStarted)
+            if (Vending != null && Vending.Data.isStarted)
                 return false;
             return base.CanMove_Implementation();
         }
 
         protected override bool CanSprint_Implementation()
         {
-            if (Vending.Data.isStarted)
+            if (Vending != null && Vending.Data.isStarted)
                 return false;
             return base.CanSprint_Implementation();
         }
 
         protected override bool CanWalk_Implementation()
         {
-            if (Vending.Data.isStarted)
+            if (Vending != null && Vending.Data.isStarted)
                 return false;
             return base.CanWalk_Implementation();
         }
 
         protected override bool CanCrouch_Implementation()
         {
-            if (Vending.Data.isStarted)
+            if (Vending != null && Vending.Data.isStarted)
                 return false;
             return base.CanCrouch_Implementation();
         }
 
         protected override bool CanCrawl_Implementation()
         {
-            if (Vending.Data.isStarted)
+            if (Vending != null && Vending.Data.isStarted)
                 return false;
             return base.CanCrawl_Implementation();
         }
 
         protected override bool CanJump_Implementation()
         {
-            if (Vending.Data.isStarted)
+            if (Vending != null && Vending.Data.isStarted)
                 return false;
             return base.CanJump_Implementation();
         }
 
         protected override bool CanDash_Implementation()
         {
-            if (Vending.Data.isStarted)
+            if (Vending != null && Vending.Data.isStarted)
                 return false;
             return base.CanDash_Implementation();
         }
 
         protected override bool CanTurn_Implementation()
         {
-            if (Vending.Data.isStarted)
+            if (Vending != null && Vending.Data.isStarted)
                 return false;
             return base.CanTurn_Implementation();
         }
