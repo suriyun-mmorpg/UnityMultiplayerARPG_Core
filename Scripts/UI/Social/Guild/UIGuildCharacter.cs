57320e224a6de7391b9be2da5249d05a5d2f170c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2025-02-26 02:07:00 +0700

    feat: add more data to UI guild character

--- Scripts/UI/Social/Guild/UIGuildCharacter.cs
+++ Scripts/UI/Social/Guild/UIGuildCharacter.cs
@@ -1,5 +1,6 @@
 ﻿using Cysharp.Text;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace MultiplayerARPG
 {
@@ -8,19 +9,43 @@ namespace MultiplayerARPG
         [Header("String Formats")]
         [Tooltip("Format => {0} = {Role Name}")]
         public UILocaleKeySetting formatKeyGuildRole = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_SIMPLE);
-        
+        [Tooltip("Format => {0} = {Share Exp Percentage}")]
+        public UILocaleKeySetting formatKeyShareExpPercentage = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_SHARE_EXP_PERCENTAGE);
+
         [Header("UI Elements")]
-        public TextWrapper uiGuildRole;
+        [FormerlySerializedAs("uiGuildRole")]
+        public TextWrapper uiTextGuildRole;
+        public TextWrapper uiTextShareExpPercentage;
 
         public byte GuildRole { get; private set; }
+        public GuildRoleData GuildRoleData { get; private set; }
+
+        public override void CloneTo(UISelectionEntry<SocialCharacterData> target)
+        {
+            base.CloneTo(target);
+            if (target is UIGuildCharacter uiGuildCharacter)
+                uiGuildCharacter.Setup(Data, GuildRole, GuildRoleData);
+        }
 
         public void Setup(SocialCharacterData data, byte guildRole, GuildRoleData guildRoleData)
         {
-            Data = data;
+            GuildRoleData = guildRoleData;
             GuildRole = guildRole;
+            Data = data;
 
-            if (uiGuildRole != null)
-                uiGuildRole.text = ZString.Format(LanguageManager.GetText(formatKeyGuildRole), guildRoleData.roleName);
+            if (uiTextGuildRole != null)
+            {
+                uiTextGuildRole.text = ZString.Format(
+                    LanguageManager.GetText(formatKeyGuildRole),
+                    guildRoleData.roleName);
+            }
+
+            if (uiTextShareExpPercentage != null)
+            {
+                uiTextShareExpPercentage.text = ZString.Format(
+                    LanguageManager.GetText(formatKeyShareExpPercentage),
+                    guildRoleData.shareExpPercentage.ToString("N0"));
+            }
         }
     }
 }
