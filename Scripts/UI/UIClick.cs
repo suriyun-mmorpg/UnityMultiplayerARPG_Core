f8ac4d86852413fe574ef4865021d718bae0e946
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2025-03-04 12:30:21 +0700

    Revert "feat: change namespace (MultiplayerARPG) (Unity force me to do it to upload to asset store)"
    This reverts commit 5b46114e4e9f522bf084bd4b91c6cf692b7066f5.

--- Scripts/UI/UIClick.cs
+++ Scripts/UI/UIClick.cs
@@ -2,22 +2,19 @@
 using UnityEngine.EventSystems;
 using UnityEngine.Events;
 
-namespace MultiplayerARPG
+public class UIClick : MonoBehaviour, IPointerClickHandler
 {
-    public class UIClick : MonoBehaviour, IPointerClickHandler
-    {
-        public UnityEvent onLeftClick;
-        public UnityEvent onMiddleClick;
-        public UnityEvent onRightClick;
+    public UnityEvent onLeftClick;
+    public UnityEvent onMiddleClick;
+    public UnityEvent onRightClick;
 
-        public void OnPointerClick(PointerEventData eventData)
-        {
-            if (eventData.button == PointerEventData.InputButton.Left)
-                onLeftClick.Invoke();
-            else if (eventData.button == PointerEventData.InputButton.Middle)
-                onMiddleClick.Invoke();
-            else if (eventData.button == PointerEventData.InputButton.Right)
-                onRightClick.Invoke();
-        }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+            onLeftClick.Invoke();
+        else if (eventData.button == PointerEventData.InputButton.Middle)
+            onMiddleClick.Invoke();
+        else if (eventData.button == PointerEventData.InputButton.Right)
+            onRightClick.Invoke();
     }
 }
