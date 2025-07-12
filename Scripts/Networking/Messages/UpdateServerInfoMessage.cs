ec1549dc660c393e996183a26029ea166850885a
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2025-07-12 12:22:52 +0700

    feat: add update server info message

---
+++ Scripts/Networking/Messages/UpdateServerInfoMessage.cs
@@ -0,0 +1,25 @@
+using LiteNetLib.Utils;
+
+namespace MultiplayerARPG
+{
+    public struct UpdateServerInfoMessage : INetSerializable
+    {
+        public string channelId;
+        public string channelTitle;
+        public string channelDescription;
+
+        public void Deserialize(NetDataReader reader)
+        {
+            channelId = reader.GetString();
+            channelTitle = reader.GetString();
+            channelDescription = reader.GetString();
+        }
+
+        public void Serialize(NetDataWriter writer)
+        {
+            writer.Put(channelId);
+            writer.Put(channelTitle);
+            writer.Put(channelDescription);
+        }
+    }
+}
