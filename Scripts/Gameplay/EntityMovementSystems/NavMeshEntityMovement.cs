24d2ff549a9e6c07232b8ca89ac95014f8a8a554
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2025-08-11 08:46:27 +0700

    fix: invalid condition to set input buffers

--- Scripts/Gameplay/EntityMovementSystems/NavMeshEntityMovement.cs
+++ Scripts/Gameplay/EntityMovementSystems/NavMeshEntityMovement.cs
@@ -225,14 +225,20 @@ namespace MultiplayerARPG
 
         public bool CanSimulateMovement()
         {
-            return Entity.IsOwnerClient || (Entity.IsOwnerClientOrOwnedByServer && movementSecure == MovementSecure.NotSecure) || (Entity.IsServer && movementSecure == MovementSecure.ServerAuthoritative);
+            switch (movementSecure)
+            {
+                case MovementSecure.ServerAuthoritative:
+                    return Entity.IsServer || Entity.IsOwnerClientOrOwnedByServer;
+                default:
+                    return Entity.IsOwnerClientOrOwnedByServer;
+            }
         }
 
         protected void SetupSimulationTick(uint simTick)
         {
-            if (_simTick > simTick && _simTick - simTick > 1)
+            if (_simTick > simTick && _simTick - simTick > 2)
                 _simTick = simTick;
-            if (simTick > _simTick && simTick - _simTick > 1)
+            if (simTick > _simTick && simTick - _simTick > 2)
                 _simTick = simTick;
         }
 
@@ -243,9 +249,9 @@ namespace MultiplayerARPG
 
         protected void SetupInterpolationTick(uint interpTick)
         {
-            if (_interpTick > interpTick && _interpTick - interpTick > 1)
+            if (_interpTick > interpTick && _interpTick - interpTick > 2)
                 _interpTick = interpTick;
-            if (interpTick > _interpTick && interpTick - _interpTick > 1)
+            if (interpTick > _interpTick && interpTick - _interpTick > 2)
                 _interpTick = interpTick;
         }
 
@@ -339,7 +345,7 @@ namespace MultiplayerARPG
         {
             if (!Entity.CanMove())
                 return;
-            if (!CanSimulateMovement())
+            if (!Entity.IsOwnerClientOrOwnedByServer)
                 return;
             uint tick = Manager.LocalTick;
             if (_inputBuffers.Count > 0)
@@ -369,7 +375,7 @@ namespace MultiplayerARPG
         {
             if (!Entity.CanMove())
                 return;
-            if (!CanSimulateMovement())
+            if (!Entity.IsOwnerClientOrOwnedByServer)
                 return;
             uint tick = Manager.LocalTick;
             _inputBuffers.Remove(tick);
@@ -385,7 +391,7 @@ namespace MultiplayerARPG
         {
             if (!Entity.CanMove())
                 return;
-            if (!CanSimulateMovement())
+            if (!Entity.IsOwnerClientOrOwnedByServer)
                 return;
             uint tick = Manager.LocalTick;
             if (!_inputBuffers.TryGetValue(tick, out MovementInputData3D inputData))
@@ -401,7 +407,7 @@ namespace MultiplayerARPG
 
             if (!Entity.CanMove() || !Entity.CanTurn())
                 return;
-            if (!CanSimulateMovement())
+            if (!Entity.IsOwnerClientOrOwnedByServer)
                 return;
             uint tick = Manager.LocalTick;
             if (!_inputBuffers.TryGetValue(tick, out MovementInputData3D inputData))
@@ -420,7 +426,7 @@ namespace MultiplayerARPG
 
         public void StopMove()
         {
-            if (movementSecure == MovementSecure.ServerAuthoritative)
+            if (Entity.IsOwnerClientOrOwnedByServer)
             {
                 // Send movement input to server, then server will apply movement and sync transform to clients
                 uint tick = Manager.LocalTick;
@@ -526,7 +532,7 @@ namespace MultiplayerARPG
                 return;
             }
 
-            if (!_inputBuffers.TryGetValue(_simTick, out MovementInputData3D inputData))
+            if (!TryGetInputBuffer(out MovementInputData3D inputData))
             {
                 // No inputs
                 return;
@@ -588,6 +594,22 @@ namespace MultiplayerARPG
             EntityTransform.rotation = Quaternion.Euler(0f, rotation, 0f);
             MovementState = t < 0.75f ? _interpFromData.MovementState : _interpToData.MovementState;
             ExtraMovementState = t < 0.75f ? _interpFromData.ExtraMovementState : _interpToData.ExtraMovementState;
+        }
+
+        protected bool TryGetInputBuffer(out MovementInputData3D inputData, byte maxLookback = 2)
+        {
+            for (byte i = 0; i <= maxLookback; ++i)
+            {
+                if (_simTick > 0 && _simTick <= i)
+                {
+                    // Not able to look back
+                    break;
+                }
+                if (_inputBuffers.TryGetValue(_simTick - i, out inputData))
+                    return true;
+            }
+            inputData = default;
+            return false;
         }
 
         protected void StoreInputBuffer(MovementInputData3D entry, int maxBuffers = 3)
