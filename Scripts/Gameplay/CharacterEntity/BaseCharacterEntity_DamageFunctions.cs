bd79be4dfe03a0951d311047572239a9b5ef70e8
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2026-08-26 16:29:17 +0700

    feat: add on killed event

--- Scripts/Gameplay/CharacterEntity/BaseCharacterEntity_DamageFunctions.cs
+++ Scripts/Gameplay/CharacterEntity/BaseCharacterEntity_DamageFunctions.cs
@@ -69,6 +69,7 @@ namespace MultiplayerARPG
             }
             CallRpcOnDead();
             _receivedDamageRecords.Clear();
+            onKilled?.Invoke(this, lastAttacker);
         }
 
         public virtual void OnRespawn()
